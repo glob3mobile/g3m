8f92f20fadd651af3071e0aad458bfe3c80e7b02
José Miguel S N <josemiguelsn@MacBook-de-Jose-Miguel-S-N.local>
2012-07-17 18:40:51 +0100

    TilePetitions listener

--- iOS/G3MiOSSDK/Commons/Basic/SimpleTileTexturizer.hpp
+++ iOS/G3MiOSSDK/Commons/Basic/SimpleTileTexturizer.hpp
@@ -21,73 +21,28 @@
 #include "Tile.hpp"
 #include "IFactory.hpp"
 
-class Petition{
-  
-  double _minLat, _minLon, _maxLat, _maxLon; //Degrees
-  std::string _url;
-  ByteBuffer* _bb;
-  
-public:
-  
-  Petition(Sector s, std::string url): _url(url), 
-                                      _minLat(s.lower().latitude().degrees()),
-  _minLon(s.lower().longitude().degrees()),
-  _maxLat(s.upper().latitude().degrees()),
-  _maxLon(s.upper().longitude().degrees()),
-  _bb(NULL)
-  {}
-  
-  ~Petition(){ if (_bb != NULL) delete _bb;}
-  
-  std::string getURL() const { return _url;}
-  
-  bool isArrived() const{ return _bb != NULL;}
-  void setByteBuffer(ByteBuffer* bb) { _bb = bb;}
-  const ByteBuffer* getByteBuffer() const { return _bb;}
-  
-};
-class TilePetitions{
-  const Tile * _tile;
-  std::vector<Petition> _petitions;
-  
-public:
-  
-  TilePetitions(): _tile(NULL){}
-  TilePetitions(const Tile* t): _tile(t){}
-  void add(const std::string& url, const Sector& s){
-    Petition p(s, url);
-    _petitions.push_back(p);
-  }
-  
-  const Tile * getTile() const { return _tile;}
-  Petition& getPetition(int i) { return _petitions[i];}
-  int getNumPetitions() { return _petitions.size();}
-  
-  bool allFinished() const{ 
-    for (int i = 0; i < _petitions.size(); i++) {
-      if (!_petitions[i].isArrived()){
-        return false;
-      }
-    }
-    return true;
-  }
-  
+class TilePetitions;
+
+struct FinishedTile{
+  int   _level;
+  int   _row;
+  int   _column;
+  int   _texID;
 };
 
-
-class SimpleTileTexturizer : public TileTexturizer, IDownloadListener {
+class SimpleTileTexturizer : public TileTexturizer{
 private:
   
-  std::vector<TilePetitions> _tilePetitions;
+  const RenderContext* _rc;
   
-  TilePetitions registerTilePetitions(const Tile* tile);
+  std::vector<FinishedTile> _finishedTiles;
+  
+  TilePetitions* getTilePetitions(const Tile* tile);
   
   std::vector<MutableVector2D> createTextureCoordinates() const;
   
   
-  Mesh* getMesh(const RenderContext* rc,
-                Tile* tile,
-                Mesh* mesh);
+  Mesh* getMesh(Tile* tile, Mesh* mesh);
   
   const int _resolution;
   
@@ -100,8 +55,8 @@ public:
                           Mesh* mesh,
                           Mesh* previousMesh);
   
-  void onDownload(const Response &response); 
-  void onError(const Response& e);
+  void onTilePetitionsFinished(TilePetitions * tp);
+  
 };
 
 #endif
