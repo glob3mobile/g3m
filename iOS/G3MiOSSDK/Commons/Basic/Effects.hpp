57bcc7d63cdb03d4ceec7348d7f92d2bef91c7bf
Agustín Trujillo Pino <gus@imac-de-agustin-trujillo-pino.local>
2012-08-06 13:44:10 +0100

    deleted method Camera::zoom

--- iOS/G3MiOSSDK/Commons/Basic/Effects.hpp
+++ iOS/G3MiOSSDK/Commons/Basic/Effects.hpp
@@ -188,7 +188,7 @@ public:
                       const TimeInterval& now) {
     const double percent = pace( percentDone(now) );
     
-    rc->getCamera()->zoom(1 - (percent / 25));
+    //rc->getCamera()->zoom(1 - (percent / 25));
   }
   
   virtual void stop(const RenderContext *rc,
