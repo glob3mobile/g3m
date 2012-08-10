0fc901f9ccaf4fe044a12bb332e43acdc7b81d88
José Miguel S N <josemiguelsn@MacBook-de-Jose-Miguel-S-N.local>
2012-08-10 15:50:28 +0100

    Widget Android initialization

---
+++ Android/G3MAndroidSDK/src/org/glob3/mobile/specific/Downloader_Android.java
@@ -0,0 +1,27 @@
+package org.glob3.mobile.specific;
+
+import org.glob3.mobile.generated.IDownloadListener;
+import org.glob3.mobile.generated.IDownloader;
+import org.glob3.mobile.generated.Url;
+
+public class Downloader_Android extends IDownloader {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int request(Url url, int priority, IDownloadListener listener) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void cancelRequest(int requestId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
