2d622e5d4e8b5e4310f3b2a26a01e69b67b5d200
José Miguel S N <josemiguelsn@MacBook-de-Jose-Miguel-S-N.local>
2012-08-27 15:14:50 +0100

    merge master

---
+++ Android/G3MAndroidSDK/src/org/glob3/mobile/specific/StringUtils_Android.java
@@ -0,0 +1,59 @@
+package org.glob3.mobile.specific;
+
+import java.util.ArrayList;
+
+import org.glob3.mobile.generated.IStringUtils;
+
+public class StringUtils_Android extends IStringUtils {
+
+	@Override
+	public String createString(byte[] data, int length) {
+		return new String(data, "UTF8");
+	}
+
+	@Override
+	public ArrayList<String> splitLines(String String) {
+		String lines[] = String.split("\\r?\\n");
+		ArrayList<String> l = new ArrayList<String>();
+		for(int i = 0; i < lines.length; i++) {
+			l.add(lines[i]);
+		}
+		
+		return l;
+	}
+
+	@Override
+	public boolean beginsWith(String String, String prefix) {
+		return String.startsWith(prefix);
+	}
+
+	@Override
+	public int indexOf(String String, String search) {
+		return String.indexOf(search);
+	}
+
+	@Override
+	public String substring(String String, int beginIndex, int endIndex) {
+		// TODO Auto-generated method stub
+		return String.substring(beginIndex, endIndex);
+	}
+
+	@Override
+	public String rtrim(String String) {
+		int index = String.length()-1;
+		while(index > 0 && String.charAt(index) == ' ') {
+			index--;
+		}
+		return String.substring(0, index);
+	}
+
+	@Override
+	public String ltrim(String String) {
+		int index = 0;
+		while(index < String.length() && String.charAt(index) == ' ') {
+			index++;
+		}
+		return String.substring(index, String.length());
+	}
+
+}
