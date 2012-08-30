6ddaa987b26cc1152330d10253c19a337418add9
José Miguel S N <josemiguelsn@MacBook-de-Jose-Miguel-S-N.local>
2012-08-30 12:40:54 +0100

    Code converted

--- Android/G3MAndroidSDK/src_/org/glob3/mobile/generated/InitializationContext.java
+++
@@ -1,8 +0,0 @@
-package org.glob3.mobile.generated; 
-public class InitializationContext extends Context
-{
-  public InitializationContext(IFactory factory, ILogger logger, Planet planet)
-  {
-	  super(factory, logger, planet);
-  }
-}
