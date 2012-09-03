024f188ce7beb6ed529309c412fbc9a07d2627f1
aldeanueva <aldeanueva@igosoftware.es>
2012-09-03 21:28:48 +0200

    Generated

--- WebGL/G3MWebGLSDK/org/glob3/mobile/client/generated/GlobalMembersGL.java
+++
@@ -1,6 +0,0 @@
-package org.glob3.mobile.generated; 
-public class GlobalMembersGL
-{
-	public static UniformsStruct Uniforms = new UniformsStruct();
-	public static AttributesStruct Attributes = new AttributesStruct();
-}
