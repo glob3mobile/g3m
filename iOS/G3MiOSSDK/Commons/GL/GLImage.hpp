b607a404a0418d9c154a01e825d755e59bee2cac
José Miguel S N <josemiguelsn@MacBook-de-Jose-Miguel-S-N.local>
2012-09-10 13:24:45 +0100

    ...

---
+++ iOS/G3MiOSSDK/Commons/GL/GLImage.hpp
@@ -0,0 +1,16 @@
+//
+//  GLImage.hpp
+//  G3MiOSSDK
+//
+//  Created by José Miguel S N on 10/09/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef G3MiOSSDK_GLImage_hpp
+#define G3MiOSSDK_GLImage_hpp
+
+class GLImage{
+  
+};
+
+#endif
