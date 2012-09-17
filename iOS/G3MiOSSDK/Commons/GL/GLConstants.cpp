23b79b3d9dc49ae51ad9273b2e63979ee0e07dc9
José Miguel S N <josemiguelsn@MacBook-de-Jose-Miguel-S-N.local>
2012-09-17 13:05:20 +0100

    GLConstants

---
+++ iOS/G3MiOSSDK/Commons/GL/GLConstants.cpp
@@ -0,0 +1,13 @@
+//
+//  GLConstants.cpp
+//  G3MiOSSDK
+//
+//  Created by José Miguel S N on 17/09/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include "GLConstants.hpp"
+
+int GLCullFace::_front = 0;
+int GLCullFace::_back = 0;
+int GLCullFace::_frontAndBack = 0;
