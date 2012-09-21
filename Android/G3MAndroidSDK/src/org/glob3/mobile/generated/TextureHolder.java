7747a9808dc0aa542efa81d35da6a3dbf13f93e0
aldeanueva <aldeanueva@igosoftware.es>
2012-09-21 11:26:02 +0200

    Generated code

--- Android/G3MAndroidSDK/src/org/glob3/mobile/generated/TextureHolder.java
+++ Android/G3MAndroidSDK/src/org/glob3/mobile/generated/TextureHolder.java
@@ -2,7 +2,7 @@ package org.glob3.mobile.generated;
 public class TextureHolder
 {
   public final TextureSpec _textureSpec;
-  private IGLTextureId _glTextureId;
+  public IGLTextureId _glTextureId;
 
   public int _referenceCounter;
 
