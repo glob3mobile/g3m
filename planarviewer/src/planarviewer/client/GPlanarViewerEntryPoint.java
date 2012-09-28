707705cec0ee8b71cee8539babac519dc3e094f9
fpulido <fpulido@becario.(none)>
2012-09-28 10:28:16 +0200

    create project planarviewer

---
+++ planarviewer/src/planarviewer/client/GPlanarViewerEntryPoint.java
@@ -0,0 +1,18 @@
+
+
+package planarviewer.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+public class GPlanarViewerEntryPoint
+         implements
+            EntryPoint {
+
+   @Override
+   public void onModuleLoad() {
+      // TODO Auto-generated method stub
+      System.out.println("> Starting up..");
+   }
+
+}
