5b4fafcd28396f7db648825ff858410cf8daabf3
aldeanueva <aldeanueva@igosoftware.es>
2012-10-23 13:58:12 +0200

    Generated

---
+++ WebGL/G3MWebGLSDK/src/org/glob3/mobile/generated/PeriodicalTask.java
@@ -0,0 +1,41 @@
+package org.glob3.mobile.generated; 
+//
+//  PeriodicalTask.hpp
+//  G3MiOSSDK
+//
+//  Created by José Miguel S N on 23/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+
+//Storing Scheduled Tasks
+public class PeriodicalTask
+{
+  public PeriodicalTask(TimeInterval interval, GTask task)
+  {
+	  _interval = interval.milliseconds();
+	  _task = task;
+	  _lastExecution = 0;
+  }
+
+  public final void executeIfNecessary()
+  {
+	ITimer t = IFactory.instance().createTimer();
+	long now = t.now().milliseconds();
+	IFactory.instance().deleteTimer(t);
+
+	long interval = now - _lastExecution;
+
+	if (interval >= _interval)
+	{
+	  _task.run();
+	  _lastExecution = now;
+	}
+  }
+
+  //Milliseconds
+  private long _interval;
+  private long _lastExecution;
+  private GTask _task;
+}
