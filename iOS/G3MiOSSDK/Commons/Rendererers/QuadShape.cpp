2e7b013574932e993cb7f17e2aed5396fc33fac4
Diego Gomez Deck <dgd@igosoftware.es>
2012-10-28 14:40:33 -0700

    -

---
+++ iOS/G3MiOSSDK/Commons/Rendererers/QuadShape.cpp
@@ -0,0 +1,14 @@
+//
+//  QuadShape.cpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 10/28/12.
+//
+//
+
+#include "QuadShape.hpp"
+
+
+void QuadShape::render(const RenderContext* rc) {
+  int __diego_at_work;
+}
---
+++ iOS/G3MiOSSDK/Commons/Rendererers/Shape.cpp
@@ -0,0 +1,9 @@
+//
+//  Shape.cpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 10/28/12.
+//
+//
+
+#include "Shape.hpp"
