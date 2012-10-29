73f24c74c69f5617bb2cc37af8793da88a6f93b7
Diego Gomez Deck <dgd@igosoftware.es>
2012-10-29 12:02:22 -0700

    -

--- iOS/G3MiOSDemo/Glob3iOSDemo/ViewController.mm
+++ iOS/G3MiOSDemo/Glob3iOSDemo/ViewController.mm
@@ -234,7 +234,7 @@
     marksRenderer->addMark(m2);
 
     if (false) {
-      for (int i = 0; i < 500; i++) {
+      for (int i = 0; i < 2000; i++) {
         const Angle latitude = Angle::fromDegrees( (int) (arc4random() % 180) - 90 );
         const Angle longitude = Angle::fromDegrees( (int) (arc4random() % 360) - 180 );
 
