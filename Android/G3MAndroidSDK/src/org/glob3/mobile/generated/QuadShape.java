a365bf528b08c272641c954952bd5a896cccf4d5
aldeanueva <aldeanueva@igosoftware.es>
2012-10-29 18:40:44 +0100

    Generated

--- Android/G3MAndroidSDK/src/org/glob3/mobile/generated/QuadShape.java
+++ Android/G3MAndroidSDK/src/org/glob3/mobile/generated/QuadShape.java
@@ -8,7 +8,7 @@ package org.glob3.mobile.generated;
 //
 
 //
-//  QuadShape.h
+//  QuadShape.hpp
 //  G3MiOSSDK
 //
 //  Created by Diego Gomez Deck on 10/28/12.
@@ -16,23 +16,112 @@ package org.glob3.mobile.generated;
 //
 
 
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class Mesh;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class IImage;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class IGLTextureId;
 
 public class QuadShape extends Shape
 {
-  public QuadShape(Geodetic3D position)
+  private Mesh createMesh(RenderContext rc)
+  {
+  
+	final float halfWidth = (float) _width / 2.0f;
+	final float halfHeight = (float) _height / 2.0f;
+	FloatBufferBuilderFromCartesian3D vertices = new FloatBufferBuilderFromCartesian3D(CenterStrategy.noCenter(), Vector3D.zero());
+	vertices.add(-halfWidth, +halfHeight, 0);
+	vertices.add(-halfWidth, -halfHeight, 0);
+	vertices.add(+halfWidth, +halfHeight, 0);
+	vertices.add(+halfWidth, -halfHeight, 0);
+  
+	IntBufferBuilder indices = new IntBufferBuilder();
+	indices.add(0);
+	indices.add(1);
+	indices.add(2);
+	indices.add(3);
+  
+	IndexedMesh im = new IndexedMesh(GLPrimitive.triangleStrip(), true, Vector3D.zero(), vertices.create(), indices.create(), 2, new Color(Color.white()));
+  
+	final IGLTextureId texId = getTextureId(rc);
+	if (texId == null)
+	{
+	  return im;
+	}
+  
+	FloatBufferBuilderFromCartesian2D texCoords = new FloatBufferBuilderFromCartesian2D();
+	texCoords.add(0, 0);
+	texCoords.add(0, 1);
+	texCoords.add(1, 0);
+	texCoords.add(1, 1);
+  
+	TextureMapping texMap = new SimpleTextureMapping(texId, texCoords.create(), true);
+  
+	return new TexturedMesh(im, true, texMap, true);
+  }
+  private Mesh getMesh(RenderContext rc)
+  {
+	if (_mesh == null)
+	{
+	  _mesh = createMesh(rc);
+	}
+	return _mesh;
+  }
+
+  private Mesh _mesh;
+
+  private final String _textureFilename;
+  private IImage _textureImage;
+  private final boolean _autoDeleteTextureImage;
+
+  private final int _width;
+  private final int _height;
+
+
+  IGLTextureId
+//C++ TO JAVA CONVERTER TODO TASK: The following statement was not recognized, possibly due to an unrecognized macro:
+  getTextureId(const RenderContext* rc);
+
+
+  public QuadShape(Geodetic3D position, IImage textureImage, boolean autoDeleteTextureImage, String textureFilename, int width, int height)
   {
 	  super(position);
+	  _mesh = null;
+	  _textureFilename = textureFilename;
+	  _textureImage = textureImage;
+	  _autoDeleteTextureImage = autoDeleteTextureImage;
+	  _width = width;
+	  _height = height;
 
   }
 
   public void dispose()
   {
-
+	if (_mesh != null)
+		_mesh.dispose();
   }
 
   public final void render(RenderContext rc)
   {
 	int __diego_at_work;
+	Mesh mesh = getMesh(rc);
+	if (mesh != null)
+	{
+	  GL gl = rc.getGL();
+  
+	  gl.pushMatrix();
+	  gl.loadMatrixf(MutableMatrix44D.identity());
+  
+	  final Vector3D cartesianPosition = rc.getPlanet().toCartesian(_position);
+	  MutableMatrix44D translationMatrix = MutableMatrix44D.createTranslationMatrix(cartesianPosition);
+  
+	  gl.multMatrixf(translationMatrix);
+  
+	  mesh.render(rc);
+  
+	  gl.popMatrix();
+	}
   }
 
 }
