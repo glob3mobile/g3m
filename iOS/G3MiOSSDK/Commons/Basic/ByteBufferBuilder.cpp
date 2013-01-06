da0c7204b896fc2681e646b6577d043c4b4d1a5d
Diego Gomez Deck <dgd@igosoftware.es>
2013-01-06 12:20:40 -0500

    -

--- iOS/G3MiOSSDK/Commons/Basic/ByteBufferBuilder.cpp
+++ iOS/G3MiOSSDK/Commons/Basic/ByteBufferBuilder.cpp
@@ -25,12 +25,29 @@ IByteBuffer* ByteBufferBuilder::create() const {
 }
 
 void ByteBufferBuilder::addStringZeroTerminated(const std::string& str) {
+#ifdef C_CODE
   const int size = str.size();
   for (int i = 0; i < size; i++) {
     const char c = str.at(i);
     _values.push_back(c);
   }
   _values.push_back((unsigned char) 0);
+#endif
+#if JAVA_CODE
+  try {
+    final byte[] bytes = str.getBytes("UTF8");
+
+    final int size = bytes.length;
+    for (int i = 0; i < size; i++) {
+      final byte c = bytes[i];
+      _values.add(c);
+    }
+    _values.add((byte) 0);
+  }
+  catch (final UnsupportedEncodingException e) {
+    e.printStackTrace();
+  }
+#endif
 }
 
 void ByteBufferBuilder::addDouble(double value) {
