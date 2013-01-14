00ac99e77dd92770df57a8604b12bd21303d546c
Diego Gomez Deck <dgd@igosoftware.es>
2013-01-14 13:16:16 -0500

    Mark with icon/label or both

---
+++ WebGL/G3MWebGLSDK/src/org/glob3/mobile/specific/TextUtils_WebGL.java
@@ -0,0 +1,22 @@
+
+
+package org.glob3.mobile.specific;
+
+import org.glob3.mobile.generated.Color;
+import org.glob3.mobile.generated.IImage;
+import org.glob3.mobile.generated.ITextUtils;
+
+
+public class TextUtils_WebGL
+         extends
+            ITextUtils {
+
+   @Override
+   public IImage createLabelBitmap(final String label,
+                                   final float fontSize,
+                                   final Color color,
+                                   final Color shadowColor) {
+
+   }
+
+}
