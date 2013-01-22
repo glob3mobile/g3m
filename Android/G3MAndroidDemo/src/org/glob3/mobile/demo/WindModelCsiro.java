5889b21172c38c620fccf485f23c13b520ffe435
Vidal Toboso <vidaltoboso@igosoftware.es>
2013-01-22 18:14:12 +0100

    Tirando para adelante en JavaDesktop

--- Android/G3MAndroidDemo/src/org/glob3/mobile/demo/WindModelCsiro.java
+++ Android/G3MAndroidDemo/src/org/glob3/mobile/demo/WindModelCsiro.java
@@ -7,97 +7,98 @@ import java.util.ArrayList;
 
 
 public class WindModelCsiro
-         implements
-            Serializable {
+    implements
+      Serializable {
 
-   public WindModelCsiro(final float latitude,
-                         final float longitude,
-                         final ArrayList<Float> levels,
-                         final ArrayList<Float> meridWind,
-                         final ArrayList<Float> zonalWind) {
-      super();
-      _latitude = latitude;
-      _longitude = longitude;
-      _levels = levels;
-      _meridWind = meridWind;
-      _zonalWind = zonalWind;
-   }
+  public WindModelCsiro(final float latitude,
+                        final float longitude,
+                        final ArrayList<Float> levels,
+                        final ArrayList<Float> meridWind,
+                        final ArrayList<Float> zonalWind) {
+    super();
+    _latitude = latitude;
+    _longitude = longitude;
+    _levels = levels;
+    _meridWind = meridWind;
+    _zonalWind = zonalWind;
+  }
 
 
-   public WindModelCsiro() {
+  public WindModelCsiro() {
 
-   }
+  }
 
-   /**
+  /**
     * 
     */
-   private static final long serialVersionUID = 1L;
-   float                     _latitude;
-   float                     _longitude;
-   ArrayList<Float>          _levels;
-   ArrayList<Float>          _meridWind;
-   ArrayList<Float>          _zonalWind;
+  private static final long serialVersionUID = 1L;
+  float _latitude;
+  float _longitude;
+  ArrayList<Float> _levels;
+  ArrayList<Float> _meridWind;
+  ArrayList<Float> _zonalWind;
 
 
-   public static long getSerialversionuid() {
-      return serialVersionUID;
-   }
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
 
 
-   public float getLatitude() {
-      return _latitude;
-   }
+  public float getLatitude() {
+    return _latitude;
+  }
 
 
-   public float getLongitude() {
-      return _longitude;
-   }
+  public float getLongitude() {
+    return _longitude;
+  }
 
 
-   public ArrayList<Float> getLevels() {
-      return _levels;
-   }
+  public ArrayList<Float> getLevels() {
+    return _levels;
+  }
 
 
-   public ArrayList<Float> getMeridWind() {
-      return _meridWind;
-   }
+  public ArrayList<Float> getMeridWind() {
+    return _meridWind;
+  }
 
 
-   public ArrayList<Float> getZonalWind() {
-      return _zonalWind;
-   }
+  public ArrayList<Float> getZonalWind() {
+    return _zonalWind;
+  }
 
 
-   public void setLatitude(final float latitude) {
-      _latitude = latitude;
-   }
+  public void setLatitude(final float latitude) {
+    _latitude = latitude;
+  }
 
 
-   public void setLongitude(final float longitude) {
-      _longitude = longitude;
-   }
+  public void setLongitude(final float longitude) {
+    _longitude = longitude;
+  }
 
 
-   public void setLevels(final ArrayList<Float> levels) {
-      _levels = levels;
-   }
+  public void setLevels(final ArrayList<Float> levels) {
+    _levels = levels;
+  }
 
 
-   public void setMeridWind(final ArrayList<Float> meridWind) {
-      _meridWind = meridWind;
-   }
+  public void setMeridWind(final ArrayList<Float> meridWind) {
+    _meridWind = meridWind;
+  }
 
 
-   public void setZonalWind(final ArrayList<Float> zonalWind) {
-      _zonalWind = zonalWind;
-   }
+  public void setZonalWind(final ArrayList<Float> zonalWind) {
+    _zonalWind = zonalWind;
+  }
 
 
-   @Override
-   public String toString() {
-      return "lat:" + _latitude + ",lon:" + _longitude + ",Num. levels:" + _levels.size() + ",Num zonal winds:"
-             + _zonalWind.size() + ",Num merid winds:" + _meridWind.size();
+  @Override
+  public String toString() {
+    return "lat:" + _latitude + ",lon:" + _longitude + ",Num. levels:"
+           + _levels.size() + ",Num zonal winds:" + _zonalWind.size()
+           + ",Num merid winds:" + _meridWind.size();
 
-   }
+  }
 }
--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/MultiLayerTileTexturizer.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/MultiLayerTileTexturizer.java
@@ -1,250 +1,277 @@
-package org.glob3.mobile.generated; 
+
+
+package org.glob3.mobile.generated;
+
 //
-//  MultiLayerTileTexturizer.cpp
-//  G3MiOSSDK
+// MultiLayerTileTexturizer.cpp
+// G3MiOSSDK
 //
-//  Created by Diego Gomez Deck on 08/08/12.
+// Created by Diego Gomez Deck on 08/08/12.
 //
 //
 
 //
-//  MultiLayerTileTexturizer.hpp
-//  G3MiOSSDK
+// MultiLayerTileTexturizer.hpp
+// G3MiOSSDK
 //
-//  Created by Diego Gomez Deck on 08/08/12.
+// Created by Diego Gomez Deck on 08/08/12.
 //
 //
 
 
+// C++ TO JAVA CONVERTER NOTE: Java has no need of forward class
+// declarations:
+// class IGLTextureId;
+// C++ TO JAVA CONVERTER NOTE: Java has no need of forward class
+// declarations:
+// class TileTextureBuilder;
+// C++ TO JAVA CONVERTER NOTE: Java has no need of forward class
+// declarations:
+// class LayerSet;
+// C++ TO JAVA CONVERTER NOTE: Java has no need of forward class
+// declarations:
+// class IDownloader;
+// C++ TO JAVA CONVERTER NOTE: Java has no need of forward class
+// declarations:
+// class LeveledTexturedMesh;
+// C++ TO JAVA CONVERTER NOTE: Java has no need of forward class
+// declarations:
+// class IFloatBuffer;
 
-//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
-//class IGLTextureId;
-//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
-//class TileTextureBuilder;
-//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
-//class LayerSet;
-//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
-//class IDownloader;
-//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
-//class LeveledTexturedMesh;
-//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
-//class IFloatBuffer;
 
-
-public class MultiLayerTileTexturizer extends TileTexturizer
-{
+public class MultiLayerTileTexturizer
+    extends
+      TileTexturizer {
   private TilesRenderParameters _parameters;
 
   private IFloatBuffer _texCoordsCache;
 
-//C++ TO JAVA CONVERTER WARNING: 'const' methods are not available in Java:
-//ORIGINAL LINE: IFloatBuffer* getTextureCoordinates(const TileRenderContext* trc) const
-  private IFloatBuffer getTextureCoordinates(TileRenderContext trc)
-  {
-	if (_texCoordsCache == null)
-	{
-	  _texCoordsCache = trc.getTessellator().createUnitTextCoords();
-	}
-	return _texCoordsCache;
+
+  // C++ TO JAVA CONVERTER WARNING: 'const' methods are not available in
+  // Java:
+  // ORIGINAL LINE: IFloatBuffer* getTextureCoordinates(const
+  // TileRenderContext* trc) const
+  private IFloatBuffer getTextureCoordinates(final TileRenderContext trc) {
+    if (_texCoordsCache == null) {
+      _texCoordsCache = trc.getTessellator().createUnitTextCoords();
+    }
+    return _texCoordsCache;
   }
 
-//  long _pendingTopTileRequests;
+  // long _pendingTopTileRequests;
 
   private TexturesHandler _texturesHandler;
 
-//C++ TO JAVA CONVERTER WARNING: 'const' methods are not available in Java:
-//ORIGINAL LINE: LeveledTexturedMesh* getMesh(Tile* tile) const
-  private LeveledTexturedMesh getMesh(Tile tile)
-  {
-	TileTextureBuilderHolder tileBuilderHolder = (TileTextureBuilderHolder) tile.getTexturizerData();
-	return (tileBuilderHolder == null) ? null : tileBuilderHolder.get().getMesh();
+
+  // C++ TO JAVA CONVERTER WARNING: 'const' methods are not available in
+  // Java:
+  // ORIGINAL LINE: LeveledTexturedMesh* getMesh(Tile* tile) const
+  private LeveledTexturedMesh getMesh(final Tile tile) {
+    final TileTextureBuilderHolder tileBuilderHolder = (TileTextureBuilderHolder) tile.getTexturizerData();
+    return (tileBuilderHolder == null) ? null
+                                      : tileBuilderHolder.get().getMesh();
   }
+
 
   public MultiLayerTileTexturizer()
-  //_pendingTopTileRequests(0),
+  // _pendingTopTileRequests(0),
   {
-	  _parameters = null;
-	  _texCoordsCache = null;
-	  _texturesHandler = null;
-  
+    _parameters = null;
+    _texCoordsCache = null;
+    _texturesHandler = null;
+
   }
 
-//  void countTopTileRequest() {
-//    _pendingTopTileRequests--;
-//  }
 
-  public void dispose()
-  {
-	if (_texCoordsCache != null)
-		_texCoordsCache.dispose();
-	_texCoordsCache = null;
+  // void countTopTileRequest() {
+  // _pendingTopTileRequests--;
+  // }
+
+  @Override
+  public void dispose() {
+    if (_texCoordsCache != null) {
+      _texCoordsCache.dispose();
+    }
+    _texCoordsCache = null;
   }
 
-  public final boolean isReady(G3MRenderContext rc, LayerSet layerSet)
-  {
-	if (layerSet != null)
-	{
-	  return layerSet.isReady();
-	}
-	return true;
+
+  @Override
+  public final boolean isReady(final G3MRenderContext rc,
+                               final LayerSet layerSet) {
+    if (layerSet != null) {
+      return layerSet.isReady();
+    }
+    return true;
   }
 
-  public final void initialize(G3MContext context, TilesRenderParameters parameters)
-  {
-	_parameters = parameters;
-	//  _layerSet->initialize(ic);
+
+  @Override
+  public final void initialize(final G3MContext context,
+                               final TilesRenderParameters parameters) {
+    _parameters = parameters;
+    // _layerSet->initialize(ic);
   }
 
-  public final Mesh texturize(G3MRenderContext rc, TileRenderContext trc, Tile tile, Mesh tessellatorMesh, Mesh previousMesh)
-  {
-	_texturesHandler = rc.getTexturesHandler();
-  
-  
-	TileTextureBuilderHolder builderHolder = (TileTextureBuilderHolder) tile.getTexturizerData();
-  
-	if (builderHolder == null)
-	{
-	  builderHolder = new TileTextureBuilderHolder(new TileTextureBuilder(this, rc, trc.getLayerSet(), _parameters, rc.getDownloader(), tile, tessellatorMesh, getTextureCoordinates(trc)));
-	  tile.setTexturizerData(builderHolder);
-	}
-  
-	if (trc.isForcedFullRender())
-	{
-	  builderHolder.get().start();
-	}
-	else
-	{
-//C++ TO JAVA CONVERTER TODO TASK: Java does not allow declaring types within methods:
-//	  class BuilderStartTask : public FrameTask
-//	  {
-//	  private:
-//		TileTextureBuilder* _builder;
-//  
-//	  public:
-//		BuilderStartTask(TileTextureBuilder* builder) : _builder(builder)
-//		{
-//		  _builder->_retain();
-//		}
-//  
-//		virtual ~BuilderStartTask()
-//		{
-//		  _builder->_release();
-//		}
-//  
-//		void execute(const G3MRenderContext* rc)
-//		{
-//		  _builder->start();
-//		}
-//  
-//		boolean isCanceled(const G3MRenderContext *rc)
-//		{
-//		  return _builder->isCanceled();
-//		}
-//	  };
-	  rc.getFrameTasksExecutor().addPreRenderTask(new BuilderStartTask(builderHolder.get()));
-	}
-  
-	tile.setTexturizerDirty(false);
-	return builderHolder.get().getMesh();
+
+  @Override
+  public final Mesh texturize(final G3MRenderContext rc,
+                              final TileRenderContext trc,
+                              final Tile tile,
+                              final Mesh tessellatorMesh,
+                              final Mesh previousMesh) {
+    _texturesHandler = rc.getTexturesHandler();
+
+
+    TileTextureBuilderHolder builderHolder = (TileTextureBuilderHolder) tile.getTexturizerData();
+
+    if (builderHolder == null) {
+      builderHolder = new TileTextureBuilderHolder(new TileTextureBuilder(
+          this, rc, trc.getLayerSet(), _parameters, rc.getDownloader(), tile,
+          tessellatorMesh, getTextureCoordinates(trc)));
+      tile.setTexturizerData(builderHolder);
+    }
+
+    if (trc.isForcedFullRender()) {
+      builderHolder.get().start();
+    }
+    else {
+      // C++ TO JAVA CONVERTER TODO TASK: Java does not allow declaring
+      // types within methods:
+      // class BuilderStartTask : public FrameTask
+      // {
+      // private:
+      // TileTextureBuilder* _builder;
+      //
+      // public:
+      // BuilderStartTask(TileTextureBuilder* builder) : _builder(builder)
+      // {
+      // _builder->_retain();
+      // }
+      //
+      // virtual ~BuilderStartTask()
+      // {
+      // _builder->_release();
+      // }
+      //
+      // void execute(const G3MRenderContext* rc)
+      // {
+      // _builder->start();
+      // }
+      //
+      // boolean isCanceled(const G3MRenderContext *rc)
+      // {
+      // return _builder->isCanceled();
+      // }
+      // };
+      rc.getFrameTasksExecutor().addPreRenderTask(
+          new BuilderStartTask(builderHolder.get()));
+    }
+
+    tile.setTexturizerDirty(false);
+    return builderHolder.get().getMesh();
   }
 
-  public final void tileToBeDeleted(Tile tile, Mesh mesh)
-  {
-  
-	TileTextureBuilderHolder builderHolder = (TileTextureBuilderHolder) tile.getTexturizerData();
-  
-	if (builderHolder != null)
-	{
-	  builderHolder.get().cancel();
-	  builderHolder.get().cleanTile();
-	  builderHolder.get().cleanMesh();
-	}
-	else
-	{
-	  if (mesh != null)
-	  {
-		ILogger.instance().logInfo("break (point) on me 4\n");
-	  }
-	}
+
+  @Override
+  public final void tileToBeDeleted(final Tile tile,
+                                    final Mesh mesh) {
+
+    final TileTextureBuilderHolder builderHolder = (TileTextureBuilderHolder) tile.getTexturizerData();
+
+    if (builderHolder != null) {
+      builderHolder.get().cancel();
+      builderHolder.get().cleanTile();
+      builderHolder.get().cleanMesh();
+    }
+    else {
+      if (mesh != null) {
+        ILogger.instance().logInfo("break (point) on me 4\n");
+      }
+    }
   }
 
-  public final boolean tileMeetsRenderCriteria(Tile tile)
-  {
-	return false;
+
+  @Override
+  public final boolean tileMeetsRenderCriteria(final Tile tile) {
+    return false;
   }
 
-  public final void justCreatedTopTile(G3MRenderContext rc, Tile tile, LayerSet layerSet)
-  {
+
+  @Override
+  public final void justCreatedTopTile(final G3MRenderContext rc,
+                                       final Tile tile,
+                                       final LayerSet layerSet) {
   }
 
-  public final void ancestorTexturedSolvedChanged(Tile tile, Tile ancestorTile, boolean textureSolved)
-  {
-	if (!textureSolved)
-	{
-	  return;
-	}
-  
-	if (tile.isTextureSolved())
-	{
-	  return;
-	}
-  
-	LeveledTexturedMesh ancestorMesh = getMesh(ancestorTile);
-	if (ancestorMesh == null)
-	{
-	  return;
-	}
-  
-	final IGLTextureId glTextureId = ancestorMesh.getTopLevelGLTextureId();
-	if (glTextureId == null)
-	{
-	  return;
-	}
-  
-	LeveledTexturedMesh tileMesh = getMesh(tile);
-	if (tileMesh == null)
-	{
-	  return;
-	}
-  
-	final int level = tile.getLevel() - ancestorTile.getLevel() - _parameters._topLevel;
-	_texturesHandler.retainGLTextureId(glTextureId);
-	if (!tileMesh.setGLTextureIdForLevel(level, glTextureId))
-	{
-	  _texturesHandler.releaseGLTextureId(glTextureId);
-	}
+
+  @Override
+  public final void ancestorTexturedSolvedChanged(final Tile tile,
+                                                  final Tile ancestorTile,
+                                                  final boolean textureSolved) {
+    if (!textureSolved) {
+      return;
+    }
+
+    if (tile.isTextureSolved()) {
+      return;
+    }
+
+    final LeveledTexturedMesh ancestorMesh = getMesh(ancestorTile);
+    if (ancestorMesh == null) {
+      return;
+    }
+
+    final IGLTextureId glTextureId = ancestorMesh.getTopLevelGLTextureId();
+    if (glTextureId == null) {
+      return;
+    }
+
+    final LeveledTexturedMesh tileMesh = getMesh(tile);
+    if (tileMesh == null) {
+      return;
+    }
+
+    final int level = tile.getLevel() - ancestorTile.getLevel()
+                      - _parameters._topLevel;
+    _texturesHandler.retainGLTextureId(glTextureId);
+    if (!tileMesh.setGLTextureIdForLevel(level, glTextureId)) {
+      _texturesHandler.releaseGLTextureId(glTextureId);
+    }
   }
 
-  public final IGLTextureId getTopLevelGLTextureIdForTile(Tile tile)
-  {
-	LeveledTexturedMesh mesh = (LeveledTexturedMesh) tile.getTexturizedMesh();
-  
-	return (mesh == null) ? null : mesh.getTopLevelGLTextureId();
+
+  public final IGLTextureId getTopLevelGLTextureIdForTile(final Tile tile) {
+    final LeveledTexturedMesh mesh = (LeveledTexturedMesh) tile.getTexturizedMesh();
+
+    return (mesh == null) ? null : mesh.getTopLevelGLTextureId();
   }
 
-  public final void onTerrainTouchEvent(G3MEventContext ec, Geodetic3D position, Tile tile, LayerSet layerSet)
-  {
-	if (layerSet != null)
-	{
-	  layerSet.onTerrainTouchEvent(ec, position, tile);
-	}
+
+  @Override
+  public final void onTerrainTouchEvent(final G3MEventContext ec,
+                                        final Geodetic3D position,
+                                        final Tile tile,
+                                        final LayerSet layerSet) {
+    if (layerSet != null) {
+      layerSet.onTerrainTouchEvent(ec, position, tile);
+    }
   }
 
-  public final void tileMeshToBeDeleted(Tile tile, Mesh mesh)
-  {
-	TileTextureBuilderHolder builderHolder = (TileTextureBuilderHolder) tile.getTexturizerData();
-	if (builderHolder != null)
-	{
-	  builderHolder.get().cancel();
-	  builderHolder.get().cleanMesh();
-	}
-	else
-	{
-	  if (mesh != null)
-	  {
-		ILogger.instance().logInfo("break (point) on me 5\n");
-	  }
-	}
+
+  @Override
+  public final void tileMeshToBeDeleted(final Tile tile,
+                                        final Mesh mesh) {
+    final TileTextureBuilderHolder builderHolder = (TileTextureBuilderHolder) tile.getTexturizerData();
+    if (builderHolder != null) {
+      builderHolder.get().cancel();
+      builderHolder.get().cleanMesh();
+    }
+    else {
+      if (mesh != null) {
+        ILogger.instance().logInfo("break (point) on me 5\n");
+      }
+    }
   }
 
 }
