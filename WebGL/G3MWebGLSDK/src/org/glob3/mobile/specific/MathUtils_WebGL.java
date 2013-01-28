cfca5fe795d8a242da4ed11a21c5a091eec99a33
MariLuz Mateo <mlmateo@igosoftware.es>
2013-01-28 12:15:17 +0100

    WebGL: implemented IMathUtils doubleToRawLongBits and rawLongBitsToDouble. Fixed ByteBuffer_WebGL

--- WebGL/G3MWebGLSDK/src/org/glob3/mobile/specific/MathUtils_WebGL.java
+++ WebGL/G3MWebGLSDK/src/org/glob3/mobile/specific/MathUtils_WebGL.java
@@ -2,6 +2,7 @@
 
 package org.glob3.mobile.specific;
 
+import org.glob3.mobile.generated.ILogger;
 import org.glob3.mobile.generated.IMathUtils;
 
 
@@ -303,18 +304,96 @@ public final class MathUtils_WebGL
 
    @Override
    public long doubleToRawLongBits(final double value) {
-      //return Double.doubleToRawLongBits(value);
-      final int ______TODO;
-      throw new RuntimeException("doubleToRawLongBits() not implemented!");
+      final byte[] byteArray = doubleToByteArray(value);
+      // LittleEndian
+
+      final int b1 = byteArray[0] & 0xFF;
+      final int b2 = byteArray[1] & 0xFF;
+      final int b3 = byteArray[2] & 0xFF;
+      final int b4 = byteArray[3] & 0xFF;
+      final int b5 = byteArray[4] & 0xFF;
+      final int b6 = byteArray[5] & 0xFF;
+      final int b7 = byteArray[6] & 0xFF;
+      final int b8 = byteArray[7] & 0xFF;
+
+      ILogger.instance().logInfo(
+               "doubleToRawLongBits " + "b[0]=" + byteArray[0] + " b[1]=" + byteArray[1] + " b[2]=" + byteArray[2] + " b[3]="
+                        + byteArray[3] + " b[4]=" + byteArray[4] + " b[5]=" + byteArray[5] + " b[6]=" + byteArray[6] + " b[7]="
+                        + byteArray[7]);
+
+      return (b1) | ((long) b2 << 8) | ((long) b3 << 16) | ((long) b4 << 24) | ((long) b5 << 32) | ((long) b6 << 40)
+             | ((long) b7 << 48) | ((long) b8 << 56);
    }
+
+
+   private native byte[] doubleToByteArray(final double value) /*-{
+		debugger;
+		var buffer = new ArrayBuffer(8);
+		var doubleView = new Float64Array(buffer);
+		doubleView[0] = value;
+
+		return (new Uint8Array(buffer));
+   }-*/;
 
 
    @Override
    public double rawLongBitsToDouble(final long value) {
-      //return Double.longBitsToDouble(value);
-      final int ______TODO;
-      throw new RuntimeException("doubleToRawLongBits() not implemented!");
+      final int[] byteArray = new int[8];
+
+      byteArray[0] = (int) (value & 0xFF);
+      byteArray[1] = (int) ((value >> 8) & 0xFF);
+      byteArray[2] = (int) ((value >> 16) & 0xFF);
+      byteArray[3] = (int) ((value >> 24) & 0xFF);
+      byteArray[4] = (int) ((value >> 32) & 0xFF);
+      byteArray[5] = (int) ((value >> 40) & 0xFF);
+      byteArray[6] = (int) ((value >> 48) & 0xFF);
+      byteArray[7] = (int) ((value >> 56) & 0xFF);
+
+      ILogger.instance().logInfo(
+               "rawLongBitsToDouble " + "b[0]=" + byteArray[0] + " b[1]=" + byteArray[1] + " b[2]=" + byteArray[2] + " b[3]="
+                        + byteArray[3] + " b[4]=" + byteArray[4] + " b[5]=" + byteArray[5] + " b[6]=" + byteArray[6] + " b[7]="
+                        + byteArray[7]);
+
+      return byteArrayToDouble(byteArray);
    }
 
 
+   private native double byteArrayToDouble(final int[] byteArray) /*-{
+		debugger;
+		var uint8Array = new Uint8Array(byteArray);
+		var doubleView = new Float64Array(uint8Array.buffer);
+
+		return doubleView[0];
+   }-*/;
+
+
+   //   @Override
+   //   public double rawLongBitsToDouble(final long value) {
+   //            final byte[] byteArray = new byte[8];
+   //            
+   //            byteArray[0] = (byte) (value & 0xFF);
+   //            byteArray[1] = (byte) ((value >> 8) & 0xFF);
+   //            byteArray[2] = (byte) ((value >> 16) & 0xFF);
+   //            byteArray[3] = (byte) ((value >> 24) & 0xFF);
+   //            byteArray[4] = (byte) ((value >> 32) & 0xFF);
+   //            byteArray[5] = (byte) ((value >> 40) & 0xFF);
+   //            byteArray[6] = (byte) ((value >> 48) & 0xFF);
+   //            byteArray[7] = (byte) ((value >> 56) & 0xFF);
+   //
+   //      ILogger.instance().logInfo(
+   //               "rawLongBitsToDouble " + "b[0]=" + byteArray[0] + " b[1]=" + byteArray[1] + " b[2]=" + byteArray[2] + " b[3]="
+   //                        + byteArray[3] + " b[4]=" + byteArray[4] + " b[5]=" + byteArray[5] + " b[6]=" + byteArray[6] + " b[7]="
+   //                        + byteArray[7]);
+   //
+   //            return byteArrayToDouble(byteArray);
+   //   }
+   //
+   //
+   //   private native double byteArrayToDouble(final byte[] byteArray) /*-{
+   //		debugger;
+   //		var uint8Array = new Uint8Array(byteArray);
+   //		var doubleView = new Float64Array(uint8Array.buffer);
+   //
+   //		return doubleView[0];
+   //   }-*/;
 }
