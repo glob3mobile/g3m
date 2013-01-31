2909dfcfe0dc3203581784eea55197ee54afc881
aldeanueva <aldeanueva@igosoftware.es>
2013-01-31 19:01:14 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/SimpleCameraConstrainer.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/SimpleCameraConstrainer.java
@@ -1,16 +1,36 @@
 package org.glob3.mobile.generated; 
+//
+//  SimpleCameraConstrainer.cpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 1/30/13.
+//
+//
+
+//
+//  SimpleCameraConstrainer.hpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 1/30/13.
+//
+//
+
+
+
+
 public class SimpleCameraConstrainer implements ICameraConstrainer
 {
+
 //C++ TO JAVA CONVERTER WARNING: 'const' methods are not available in Java:
-//ORIGINAL LINE: void onCameraChange(const Planet *planet, const Camera* previousCamera, Camera* nextCamera) const
-  public final void onCameraChange(Planet planet, Camera previousCamera, Camera nextCamera)
+//ORIGINAL LINE: virtual void onCameraChange(const Planet *planet, const Camera* previousCamera, Camera* nextCamera) const
+  public void onCameraChange(Planet planet, Camera previousCamera, Camera nextCamera)
   {
-
+  
 	final double radii = planet.getRadii().maxAxis();
-
+  
 	final Geodetic3D cameraPosition3D = planet.toGeodetic3D(nextCamera.getCartesianPosition());
 	final double cameraHeight = cameraPosition3D.height();
-
+  
 	if (cameraHeight > radii *9)
 	{
 	  nextCamera.resetPosition();
@@ -18,16 +38,4 @@ public class SimpleCameraConstrainer implements ICameraConstrainer
 	}
   }
 
-
-  //  bool acceptsCamera(const Camera* camera,
-  //                     const Planet *planet) const {
-  //    const double distance = camera->getPosition().length();
-  //    const double radii    = planet->getRadii().maxAxis();
-  //    if (distance > radii*10) {
-  //      // printf ("--- camera constraint!\n");
-  //      return false;
-  //    }
-  //
-  //    return true;
-  //  }
 }
