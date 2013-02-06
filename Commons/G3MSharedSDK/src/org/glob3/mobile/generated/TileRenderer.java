a022303ddabca9ff0815c955286bbc73fea19857
aldeanueva <aldeanueva@igosoftware.es>
2013-02-06 19:41:04 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/TileRenderer.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/TileRenderer.java
@@ -77,26 +77,8 @@ public class TileRenderer extends LeafRenderer implements LayerSetChangedListene
 
   private java.util.ArrayList<VisibleSectorListenerEntry> _visibleSectorListeners = new java.util.ArrayList<VisibleSectorListenerEntry>();
 
-  private void visitSubTilesTouchesWith(G3MRenderContext rc, Tile tile, Sector sectorToVisit, int topLevel, int maxLevel)
-  {
-	  if (tile.getLevel() < maxLevel)
-	  {
-		  final int subTilesCount = tile.getSubTiles().size();
-		  for (int i = 0; i < subTilesCount; i++)
-		  {
-			  Tile tl = tile.getSubTiles().get(i);
-			  if (tl.getSector().touchesWith(sectorToVisit))
-			  {
-				  if ((tile.getLevel() >= topLevel))
-				  {
-					  rc.getLogger().logInfo("Level: %s. Col: %s; Row: %s", tl.getLevel(), tl.getColumn(), tl.getRow());
-					  _tileVisitor.visitTile(tl);
-				  }
-				  visitSubTilesTouchesWith(rc, tl, sectorToVisit, topLevel, maxLevel);
-			  }
-		  }
-	  }
-  }
+//C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
+//  void visitSubTilesTouchesWith(G3MRenderContext rc, Tile tile, Sector sectorToVisit, int topLevel, int maxLevel);
 
   public TileRenderer(TileTessellator tessellator, TileTexturizer texturizer, LayerSet layerSet, TilesRenderParameters parameters, boolean showStatistics)
   {
@@ -341,27 +323,8 @@ public class TileRenderer extends LeafRenderer implements LayerSetChangedListene
 	_tileVisitor = tileVisitor;
   }
 
-  public final void visitTilesTouchesWith(G3MRenderContext rc, Sector sector, int topLevel, int maxLevel)
-  {
-  
-	  if (_tileVisitor != null)
-	  {
-		  final int topLevelCache = (topLevel < _parameters._topLevel) ? _parameters._topLevel : topLevel;
-  
-		  final int maxLevelCache = (maxLevel > _parameters._maxLevel) ? _parameters._maxLevel : maxLevel;
-		  // Get Tiles to Cache
-		  final int topLevelTilesCount = _topLevelTiles.size();
-		  for (int i = 0; i < topLevelTilesCount; i++)
-		  {
-			  Tile tile = _topLevelTiles.get(i);
-			  if (tile.getSector().touchesWith(sector))
-			  {
-				  _tileVisitor.visitTile(tile);
-				  visitSubTilesTouchesWith(rc, tile, sector, topLevelCache, maxLevelCache);
-			  }
-		  }
-	  }
-  }
+//C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
+//  void visitTilesTouchesWith(G3MRenderContext rc, Sector sector, int topLevel, int maxLevel);
 
   public final void start()
   {
--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/TileRendererBuilder.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/TileRendererBuilder.java
@@ -1,25 +1,25 @@
-
-
-package org.glob3.mobile.generated;
-
+package org.glob3.mobile.generated; 
 //
-// TileRendererBuilder.cpp
-// G3MiOSSDK
+//  TileRendererBuilder.cpp
+//  G3MiOSSDK
 //
-// Created by Mari Luz Mateo on 22/11/12.
+//  Created by Mari Luz Mateo on 22/11/12.
 //
 //
 
 //
-// TileRendererBuilder.hpp
-// G3MiOSSDK
+//  TileRendererBuilder.hpp
+//  G3MiOSSDK
 //
-// Created by Mari Luz Mateo on 22/11/12.
+//  Created by Mari Luz Mateo on 22/11/12.
 //
 //
 
 
-public class TileRendererBuilder {
+
+
+public class TileRendererBuilder
+{
 
   private TileTessellator _tileTessellator;
   private TileTexturizer _texturizer;
@@ -30,155 +30,124 @@ public class TileRendererBuilder {
   private boolean _useTilesSplitBudget;
   private boolean _forceTopLevelTilesRenderOnStart;
   private boolean _incrementalTileQuality;
-  private final java.util.ArrayList<VisibleSectorListener> _visibleSectorListeners = new java.util.ArrayList<VisibleSectorListener>();
-  private final java.util.ArrayList<Long> _stabilizationMilliSeconds = new java.util.ArrayList<Long>();
+  private java.util.ArrayList<VisibleSectorListener> _visibleSectorListeners = new java.util.ArrayList<VisibleSectorListener>();
+  private java.util.ArrayList<Long> _stabilizationMilliSeconds = new java.util.ArrayList<Long>();
 
-
-  private LayerSet createLayerSet() {
-    final LayerSet layerSet = new LayerSet();
-
-    final WMSLayer bing = LayerBuilder.createBingLayer(true);
-    layerSet.addLayer(bing);
-
-    return layerSet;
+  private LayerSet createLayerSet()
+  {
+	LayerSet layerSet = new LayerSet();
+  
+	WMSLayer bing = LayerBuilder.createBingLayer(true);
+	layerSet.addLayer(bing);
+  
+	return layerSet;
+  }
+  private TilesRenderParameters createTileRendererParameters()
+  {
+	return TilesRenderParameters.createDefault(_renderDebug, _useTilesSplitBudget, _forceTopLevelTilesRenderOnStart, _incrementalTileQuality);
+  }
+  private TileTessellator createTileTessellator()
+  {
+	TileTessellator tileTessellator = new EllipsoidalTileTessellator(_parameters._tileResolution, true);
+  
+	return tileTessellator;
   }
 
-
-  private TilesRenderParameters createTileRendererParameters() {
-    return TilesRenderParameters.createDefault(_renderDebug,
-        _useTilesSplitBudget, _forceTopLevelTilesRenderOnStart,
-        _incrementalTileQuality);
+  public TileRendererBuilder()
+  {
+	_showStatistics = false;
+	_renderDebug = false;
+	_useTilesSplitBudget = true;
+	_forceTopLevelTilesRenderOnStart = true;
+	_incrementalTileQuality = false;
+  
+	_parameters = createTileRendererParameters();
+	_layerSet = createLayerSet();
+	_texturizer = new MultiLayerTileTexturizer();
+	_tileTessellator = createTileTessellator();
   }
-
-
-  private TileTessellator createTileTessellator() {
-    final TileTessellator tileTessellator = new EllipsoidalTileTessellator(
-        _parameters._tileResolution, true);
-
-    return tileTessellator;
+  public void dispose()
+  {
+	//    delete _tileTessellator;
+	//    delete _texturizer;
+	//    delete _layerSet;
+	///#ifdef C_CODE
+	//    delete _parameters;
+	///#endif
   }
-
-
-  public TileRendererBuilder() {
-    _showStatistics = false;
-    _renderDebug = false;
-    _useTilesSplitBudget = true;
-    _forceTopLevelTilesRenderOnStart = true;
-    _incrementalTileQuality = false;
-
-    _parameters = createTileRendererParameters();
-    _layerSet = createLayerSet();
-    _texturizer = new MultiLayerTileTexturizer();
-    _tileTessellator = createTileTessellator();
+  public final TileRenderer create()
+  {
+	TileRenderer tileRenderer = new TileRenderer(_tileTessellator, _texturizer, _layerSet, _parameters, _showStatistics);
+  
+	for (int i = 0; i < _visibleSectorListeners.size(); i++)
+	{
+	  tileRenderer.addVisibleSectorListener(_visibleSectorListeners.get(i), TimeInterval.fromMilliseconds(_stabilizationMilliSeconds.get(i)));
+	}
+  
+	return tileRenderer;
   }
-
-
-  public void dispose() {
-    // delete _tileTessellator;
-    // delete _texturizer;
-    // delete _layerSet;
-    // /#ifdef C_CODE
-    // delete _parameters;
-    // /#endif
+  public final void setTileTessellator(TileTessellator tileTessellator)
+  {
+	if (_tileTessellator != tileTessellator)
+	{
+	  if (_tileTessellator != null)
+		  _tileTessellator.dispose();
+	  _tileTessellator = tileTessellator;
+	}
   }
-
-
-  public final TileRenderer create() {
-    final TileRenderer tileRenderer = new TileRenderer(_tileTessellator,
-        _texturizer, _layerSet, _parameters, _showStatistics);
-
-    for (int i = 0; i < _visibleSectorListeners.size(); i++) {
-      tileRenderer.addVisibleSectorListener(_visibleSectorListeners.get(i),
-          TimeInterval.fromMilliseconds(_stabilizationMilliSeconds.get(i)));
-    }
-
-    return tileRenderer;
+  public final void setTileTexturizer(TileTexturizer tileTexturizer)
+  {
+	if (_texturizer != tileTexturizer)
+	{
+	  if (_texturizer != null)
+		  _texturizer.dispose();
+	  _texturizer = tileTexturizer;
+	}
   }
-
-
-  public final void setTileTessellator(final TileTessellator tileTessellator) {
-    if (_tileTessellator != tileTessellator) {
-      if (_tileTessellator != null) {
-        _tileTessellator.dispose();
-      }
-      _tileTessellator = tileTessellator;
-    }
+  public final void setLayerSet(LayerSet layerSet)
+  {
+	if (_layerSet != layerSet)
+	{
+	  if (_layerSet != null)
+		  _layerSet.dispose();
+	  _layerSet = layerSet;
+	}
   }
-
-
-  public final void setTileTexturizer(final TileTexturizer tileTexturizer) {
-    if (_texturizer != tileTexturizer) {
-      if (_texturizer != null) {
-        _texturizer.dispose();
-      }
-      _texturizer = tileTexturizer;
-    }
+  public final void setTileRendererParameters(TilesRenderParameters parameters)
+  {
+	if (_parameters != parameters)
+	{
+	  _parameters = parameters;
+	}
   }
-
-
-  public final void setLayerSet(final LayerSet layerSet) {
-    if (_layerSet != layerSet) {
-      if (_layerSet != null) {
-        _layerSet.dispose();
-      }
-      _layerSet = layerSet;
-    }
+  public final void setShowStatistics(boolean showStatistics)
+  {
+	_showStatistics = showStatistics;
   }
-
-
-  public final void setTileRendererParameters(final TilesRenderParameters parameters) {
-    if (_parameters != parameters) {
-      _parameters = parameters;
-    }
+  public final void setRenderDebug(boolean renderDebug)
+  {
+	_renderDebug = renderDebug;
   }
-
-
-  public final void setShowStatistics(final boolean showStatistics) {
-    _showStatistics = showStatistics;
+  public final void setUseTilesSplitBuget(boolean useTilesSplitBudget)
+  {
+	_useTilesSplitBudget = useTilesSplitBudget;
   }
-
-
-  public final void setRenderDebug(final boolean renderDebug) {
-    _renderDebug = renderDebug;
+  public final void setForceTopLevelTilesRenderOnStart(boolean forceTopLevelTilesRenderOnStart)
+  {
+	_forceTopLevelTilesRenderOnStart = forceTopLevelTilesRenderOnStart;
   }
-
-
-  public final void setUseTilesSplitBuget(final boolean useTilesSplitBudget) {
-    _useTilesSplitBudget = useTilesSplitBudget;
+  public final void setIncrementalTileQuality(boolean incrementalTileQuality)
+  {
+	_incrementalTileQuality = incrementalTileQuality;
   }
-
-
-  public final void setForceTopLevelTilesRenderOnStart(final boolean forceTopLevelTilesRenderOnStart) {
-    _forceTopLevelTilesRenderOnStart = forceTopLevelTilesRenderOnStart;
+  public final void addVisibleSectorListener(VisibleSectorListener listener, TimeInterval stabilizationInterval)
+  {
+	_visibleSectorListeners.add(listener);
+	_stabilizationMilliSeconds.add(stabilizationInterval.milliseconds());
   }
-
-
-  public final void setIncrementalTileQuality(final boolean incrementalTileQuality) {
-    _incrementalTileQuality = incrementalTileQuality;
-  }
-
-
-  public final void addVisibleSectorListener(final VisibleSectorListener listener,
-                                             final TimeInterval stabilizationInterval) {
-    _visibleSectorListeners.add(listener);
-    _stabilizationMilliSeconds.add(stabilizationInterval.milliseconds());
-  }
-
-
-  public final void addVisibleSectorListener(final VisibleSectorListener listener) {
-    addVisibleSectorListener(listener, TimeInterval.zero());
-  }
-
-
-  // TODO: IN CORE
-  public TilesRenderParameters getTileRenderParameters() {
-    return _parameters;
-  }
-
-
-  // TODO: IN CORE
-  public TileTexturizer getTileTexturizer() {
-    return _texturizer;
+  public final void addVisibleSectorListener(VisibleSectorListener listener)
+  {
+	addVisibleSectorListener(listener, TimeInterval.zero());
   }
 
 }
