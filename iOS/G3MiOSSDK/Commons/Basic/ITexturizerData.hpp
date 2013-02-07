a9ad2face05636878c5ef33f2cfa0397d71045de
Diego Gomez Deck <dgd@igosoftware.es>
2013-02-07 11:09:59 -0500

    -

--- iOS/G3MiOSSDK/Commons/Basic/ITexturizerData.hpp
+++ iOS/G3MiOSSDK/Commons/Basic/ITexturizerData.hpp
@@ -9,28 +9,27 @@
 #ifndef __G3MiOSSDK__ITexturizerData__
 #define __G3MiOSSDK__ITexturizerData__
 
-//class ITexturizerData {
-//public:
-////  virtual bool isTexturizerData() const = 0; //Java needs to know that this is an interface
-//#ifdef C_CODE
-//  virtual ~ITexturizerData() { }
-//#endif
-//#ifdef JAVA_CODE
-//  public void dispose();
-//#endif
-//};
-
-#ifdef C_CODE
 class ITexturizerData {
 public:
+#ifdef C_CODE
   virtual ~ITexturizerData() { }
-};
 #endif
-
 #ifdef JAVA_CODE
-public interface ITexturizerData {
   public void dispose();
-}
 #endif
+};
+
+//#ifdef C_CODE
+//class ITexturizerData {
+//public:
+//  virtual ~ITexturizerData() { }
+//};
+//#endif
+//
+//#ifdef JAVA_CODE
+//public interface ITexturizerData {
+//  public void dispose();
+//}
+//#endif
 
 #endif
