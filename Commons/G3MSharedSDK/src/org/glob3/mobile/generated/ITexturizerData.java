c5808a3ee9c7c326f5cbe4f7b1efdab868002fea
aldeanueva <aldeanueva@igosoftware.es>
2013-02-07 17:12:42 +0100

    Generated

---
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/ITexturizerData.java
@@ -0,0 +1,36 @@
+package org.glob3.mobile.generated; 
+//
+//  ITexturizerData.cpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 2/7/13.
+//
+//
+
+//
+//  ITexturizerData.hpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 2/7/13.
+//
+//
+
+
+public class ITexturizerData
+{
+  public void dispose();
+}
+///#ifdef C_CODE
+//class ITexturizerData {
+//public:
+//  virtual ~ITexturizerData() { }
+//};
+///#endif
+//
+///#ifdef JAVA_CODE
+//public interface ITexturizerData {
+//  public void dispose();
+//}
+///#endif
+
+
