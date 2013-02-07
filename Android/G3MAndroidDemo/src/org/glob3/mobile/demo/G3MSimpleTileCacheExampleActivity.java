2c7fffe16aca44aed86842b3f8b48e34f34f9998
Vidal Toboso <vidaltoboso@igosoftware.es>
2013-02-07 12:36:45 +0100

    Añadimos un Activity para cachear

--- Android/G3MAndroidDemo/src/org/glob3/mobile/demo/G3MSimpleTileCacheExampleActivity.java
+++ Android/G3MAndroidDemo/src/org/glob3/mobile/demo/G3MSimpleTileCacheExampleActivity.java
@@ -14,15 +14,14 @@ import org.glob3.mobile.generated.CameraRotationHandler;
 import org.glob3.mobile.generated.CameraSingleDragHandler;
 import org.glob3.mobile.generated.Color;
 import org.glob3.mobile.generated.CompositeRenderer;
+import org.glob3.mobile.generated.G3MContext;
 import org.glob3.mobile.generated.GInitializationTask;
-import org.glob3.mobile.generated.Geodetic3D;
+import org.glob3.mobile.generated.Geodetic2D;
 import org.glob3.mobile.generated.ICameraConstrainer;
 import org.glob3.mobile.generated.IDownloader;
 import org.glob3.mobile.generated.IStorage;
 import org.glob3.mobile.generated.IThreadUtils;
 import org.glob3.mobile.generated.LayerSet;
-import org.glob3.mobile.generated.Mark;
-import org.glob3.mobile.generated.MarksRenderer;
 import org.glob3.mobile.generated.PeriodicalTask;
 import org.glob3.mobile.generated.Planet;
 import org.glob3.mobile.generated.Sector;
@@ -39,11 +38,12 @@ import org.glob3.mobile.specific.G3MBaseActivity;
 import org.glob3.mobile.specific.G3MWidget_Android;
 import org.glob3.mobile.specific.SQLiteStorage_Android;
 import org.glob3.mobile.specific.ThreadUtils_Android;
+import org.glob3.mobile.specific.TileVisitorCache_Android;
 
 import android.os.Bundle;
 
 
-public class G3MSimplestGlob3Activity
+public class G3MSimpleTileCacheExampleActivity
     extends
       G3MBaseActivity {
 
@@ -53,23 +53,13 @@ public class G3MSimplestGlob3Activity
   @Override
   public void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    // initialize a default widget by using a builder
-    // final G3MBuilder_Android g3mBuilder = new G3MBuilder_Android(this);
-    //
-    // _widgetAndroid = g3mBuilder.createWidget();
-    // setContentView(_widgetAndroid);
-
-
-    // initialize a customized widget without using any builder
-    // initialize a customized widget without using any builder
     _widgetAndroid = new G3MWidget_Android(this);
-
     final IStorage storage = new SQLiteStorage_Android("g3m.cache", this);
 
     final TimeInterval connectTimeout = TimeInterval.fromSeconds(20);
     final TimeInterval readTimeout = TimeInterval.fromSeconds(30);
     final boolean saveInBackground = true;
+
     final IDownloader downloader = new CachedDownloader( //
         new Downloader_Android(8, connectTimeout, readTimeout, this), //
         storage, //
@@ -93,6 +83,8 @@ public class G3MSimplestGlob3Activity
     cameraRenderer.addHandler(new CameraDoubleTapHandler());
 
     final CompositeRenderer mainRenderer = new CompositeRenderer();
+
+    // Añadir todas las capas que se quieren cachear
     final LayerSet layerSet = new LayerSet();
     final WMSLayer osm = new WMSLayer( //
         "osm_auto:all", //
@@ -106,28 +98,11 @@ public class G3MSimplestGlob3Activity
         null, //
         TimeInterval.fromDays(30));
     layerSet.addLayer(osm);
+
     final TileRendererBuilder tlBuilder = new TileRendererBuilder();
     tlBuilder.setLayerSet(layerSet);
     final TileRenderer tileRenderer = tlBuilder.create();
     mainRenderer.addRenderer(tileRenderer);
-
-
-    final MarksRenderer marksRenderer = new MarksRenderer(false);
-    final Mark m1 = new Mark(
-        "Fuerteventura", //
-        new URL("http://glob3m.glob3mobile.com/icons/markers/g3m.png", false), //
-        new Geodetic3D(Angle.fromDegrees(28.05), Angle.fromDegrees(-14.36), 0), //
-        false);
-    marksRenderer.addMark(m1);
-
-    final Mark m3 = new Mark("Washington, DC", //
-        new Geodetic3D(Angle.fromDegreesMinutesSeconds(38, 53, 42.24),
-            Angle.fromDegreesMinutesSeconds(-77, 2, 10.92), 100), //
-        0);
-    marksRenderer.addMark(m3);
-    mainRenderer.addRenderer(marksRenderer);
-
-    final GInitializationTask initializationTask = null;
 
     final org.glob3.mobile.generated.Renderer busyRenderer = new BusyMeshRenderer();
 
@@ -137,6 +112,36 @@ public class G3MSimplestGlob3Activity
     final boolean logFPS = true;
 
     final boolean logDownloaderStatistics = false;
+
+    // PRECACHING
+    final GInitializationTask initializationTask = new GInitializationTask() {
+      @Override
+      public void run(final G3MContext ctx) {
+        // Especificamos el tipo de cacheo que se quiere hacer, indicando el
+        // ancho y alto de los tiles
+        tileRenderer.acceptTileVisitor(new TileVisitorCache_Android(ctx,
+            layerSet, 256, 256));
+
+        // Cacheamos los 2 primeros niveles de todas las capas
+        tileRenderer.visitTilesTouchesWith(Sector.fullSphere(), 0, 2);
+
+        // Cacheamos el sector que nos interese de todas las capas
+        tileRenderer.visitTilesTouchesWith(
+            new Sector(new Geodetic2D(Angle.fromDegrees(39.31),
+                Angle.fromDegrees(-6.72)), new Geodetic2D(
+                Angle.fromDegrees(39.38), Angle.fromDegrees(-6.64))), 2, 14);
+
+
+        ctx.getLogger().logInfo(
+            "SE HA TERMINADO DE CACHEAR LOS SECTORES ESPECIFICADOS.");
+      }
+
+
+      @Override
+      public boolean isDone(final G3MContext context1) {
+        return true;
+      }
+    };
 
     final boolean autoDeleteInitializationTask = true;
 
