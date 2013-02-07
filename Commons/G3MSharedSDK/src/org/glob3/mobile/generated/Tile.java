e535547e7c2a7395b3d33e07335a066fadbba37a
aldeanueva <aldeanueva@igosoftware.es>
2013-02-07 16:56:19 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Tile.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Tile.java
@@ -1,4 +1,61 @@
 package org.glob3.mobile.generated; 
+//
+//  Tile.cpp
+//  G3MiOSSDK
+//
+//  Created by Agustín Trujillo Pino on 12/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+//
+//  Tile.h
+//  G3MiOSSDK
+//
+//  Created by Agustín Trujillo Pino on 12/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class G3MRenderContext;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class Mesh;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class TileTessellator;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class TileTexturizer;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class TilesRenderParameters;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class ITimer;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class TilesStatistics;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class TileRenderContext;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class TileKey;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class Vector3D;
+//C++ TO JAVA CONVERTER NOTE: Java has no need of forward class declarations:
+//class GLState;
+
+//class ITexturizerData {
+//public:
+////  virtual bool isTexturizerData() const = 0; //Java needs to know that this is an interface
+///#ifdef C_CODE
+//  virtual ~ITexturizerData() { }
+///#endif
+///#ifdef JAVA_CODE
+//  public void dispose();
+///#endif
+//};
+
+
+public interface ITexturizerData {
+  public void dispose();
+}
+
 public class Tile
 {
   private TileTexturizer _texturizer;
@@ -252,8 +309,7 @@ public class Tile
 		  _texturizedMesh.dispose();
 	  _texturizedMesh = null;
   
-	  if (_texturizerData != null)
-		  _texturizerData.dispose();
+	  _texturizerData = null;
 	  _texturizerData = null;
   
 	  setTexturizerDirty(true);
@@ -302,8 +358,7 @@ public class Tile
 	if (_tessellatorMesh != null)
 		_tessellatorMesh.dispose();
   
-	if (_texturizerData != null)
-		_texturizerData.dispose();
+	_texturizerData = null;
 	_texturizerData = null;
   
 	if (_texturizedMesh != null)
