fa558a7c5e8bfa207b889d42edb60975a945ef0f
Diego Gomez Deck <dgd@igosoftware.es>
2013-02-08 10:03:25 -0500

    -

--- iOS/G3MiOSSDK/Commons/Geometry/MutableMatrix44D.cpp
+++ iOS/G3MiOSSDK/Commons/Geometry/MutableMatrix44D.cpp
@@ -282,13 +282,13 @@ Vector2D MutableMatrix44D::project(const Vector3D& point,
   const double in0 = point._x;
   const double in1 = point._y;
   const double in2 = point._z;
-  const double in3 = 1.0;
+  //  const double in3 = 1.0;
 
   //Transformating point
-  double out0 = _m00 * in0 + _m01 * in1 + _m02 * in2 + _m03 * in3;
-  double out1 = _m10 * in0 + _m11 * in1 + _m12 * in2 + _m13 * in3;
+  double out0 = _m00 * in0 + _m01 * in1 + _m02 * in2 + _m03 /* * in3 */;
+  double out1 = _m10 * in0 + _m11 * in1 + _m12 * in2 + _m13 /* * in3 */;
   //double out2 = _m20 * in0 + _m21 * in1 + _m22 * in2 + _m23 * in3;
-  const double out3 = _m30 * in0 + _m31 * in1 + _m32 * in2 + _m33 * in3;
+  const double out3 = _m30 * in0 + _m31 * in1 + _m32 * in2 + _m33 /* * in3 */;
 
   if (out3 == 0.0) {
     return Vector2D::nan();
