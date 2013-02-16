37887e2717551f33362d459c2a0d73b98153d956
aldeanueva <aldeanueva@igosoftware.es>
2013-02-16 21:02:13 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/DefineConstants.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/DefineConstants.java
@@ -4,4 +4,5 @@ final class DefineConstants
    public static final int TILE_DOWNLOAD_PRIORITY = 1000000000;
    public static final double THRESHOLD = 1e-5;
    public static final double ISBETWEEN_THRESHOLD = 1e-2;
+   public static final int TEXTURES_DOWNLOAD_PRIORITY = 1000000;
 }
--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/GlobalMembersSGLayerNode.java
+++
@@ -1,9 +0,0 @@
-package org.glob3.mobile.generated; 
-public class GlobalMembersSGLayerNode
-{
-
-
-
-
-      public static final int TEXTURES_DOWNLOAD_PRIORITY = 1000000;
-}
