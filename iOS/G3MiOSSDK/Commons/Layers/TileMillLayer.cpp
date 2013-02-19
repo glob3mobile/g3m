f29cf96315b555b66351a157e407c8140fee04f2
Vidal Toboso <vidalete@macigo.local>
2013-02-19 09:42:06 +0100

    TileMillLayer

---
+++ iOS/G3MiOSSDK/Commons/Layers/TileMillLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  TileMillLayer.cpp
+//  G3MiOSSDK
+//
+//  Created by Vidal Toboso on 19/02/13.
+//
+//
+
+#include "TileMillLayer.h"
---
+++ iOS/G3MiOSSDK/Commons/Layers/TileMillLayer.hpp
@@ -0,0 +1,14 @@
+//
+//  TileMillLayer.hpp
+//  G3MiOSSDK
+//
+//  Created by Vidal Toboso on 19/02/13.
+//
+//
+
+#ifndef G3MiOSSDK_TileMillLayer_hpp
+#define G3MiOSSDK_TileMillLayer_hpp
+
+
+
+#endif
