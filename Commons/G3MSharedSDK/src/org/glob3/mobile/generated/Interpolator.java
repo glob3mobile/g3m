5efea2012cfc6b342ba43b9b677c4a921135e945
aldeanueva <aldeanueva@igosoftware.es>
2013-02-21 15:00:48 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Interpolator.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Interpolator.java
@@ -24,6 +24,11 @@ public class Interpolator
 //C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
 //  Interpolator();
 
+  public static double interpolate(Geodetic2D sw, Geodetic2D ne, double valueSW, double valueSE, double valueNE, double valueNW, Geodetic2D position)
+  {
+    return interpolate(sw, ne, valueSW, valueSE, valueNE, valueNW, position.latitude(), position.longitude());
+  }
+
   public static double interpolate(Geodetic2D sw, Geodetic2D ne, double valueSW, double valueSE, double valueNE, double valueNW, Angle latitude, Angle longitude)
   {
   
@@ -38,18 +43,17 @@ public class Interpolator
     final double u = (longitude.radians() - swLonRadians) / deltaLonRadians;
     final double v = (neLatRadians - latitude.radians()) / deltaLatRadians;
   
+    return interpolate(sw, ne, valueSW, valueSE, valueNE, valueNW, u, v);
+  }
+
+  public static double interpolate(Geodetic2D sw, Geodetic2D ne, double valueSW, double valueSE, double valueNE, double valueNW, double u, double v)
+  {
     final double alphaSW = (1.0 - u) * v;
     final double alphaSE = u * v;
     final double alphaNE = u * (1.0 - v);
     final double alphaNW = (1.0 - u) * (1.0 - v);
   
     return (alphaSW * valueSW) + (alphaSE * valueSE) + (alphaNE * valueNE) + (alphaNW * valueNW);
-  }
-
-
-  public static double interpolate(Geodetic2D sw, Geodetic2D ne, double valueSW, double valueSE, double valueNE, double valueNW, Geodetic2D position)
-  {
-    return interpolate(sw, ne, valueSW, valueSE, valueNE, valueNW, position.latitude(), position.longitude());
   }
 
 }
