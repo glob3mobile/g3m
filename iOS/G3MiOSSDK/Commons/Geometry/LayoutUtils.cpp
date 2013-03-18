4c16d6b4f92417f2a4aeedd5d6e6231cb9314e18
Diego Gomez Deck <dgd@igosoftware.es>
2013-03-18 12:47:17 -0400

    -

--- iOS/G3MiOSSDK/Commons/Geometry/LayoutUtils.cpp
+++ iOS/G3MiOSSDK/Commons/Geometry/LayoutUtils.cpp
@@ -6,29 +6,33 @@
 //
 //
 
-#include "LayoutUtils.h"
+#include "LayoutUtils.hpp"
 #include "Geodetic3D.hpp"
 #include "Vector3D.hpp"
 #include "Ellipsoid.hpp"
+#include "IMathUtils.hpp"
 
-
-std::vector<Geodetic3D> LayoutUtils::splitOverCircle(const Ellipsoid* ellipsoid, Geodetic3D center,
-                                                     double radiusInMeters, int splits, const Angle& startAngle)
-{
-  std::vector<Geodetic3D> positionList;
-  Angle angle = startAngle;
-  Angle delta = Angle::pi().times(2).div(splits);
+std::vector<Geodetic3D*> LayoutUtils::splitOverCircle(const Ellipsoid* ellipsoid,
+                                                     const Geodetic3D& center,
+                                                     double radiusInMeters,
+                                                     int splits,
+                                                     const Angle& startAngle) {
+  std::vector<Geodetic3D*> result;
+  const double deltaInRadians = (IMathUtils::instance()->pi() * 2.0) / splits;
 
   const Vector3D cartesianCenter  = ellipsoid->toCartesian(center);
   const Vector3D normal           = ellipsoid->geodeticSurfaceNormal(center);
   const Vector3D northInPlane     = Vector3D::upZ().projectionInPlane(normal).normalized().times(radiusInMeters);
-  
-  for (unsigned int i=0; i<splits; i++) {
-    const Vector3D cartesianPosition = northInPlane.rotateAroundAxis(normal, angle);
-    const Geodetic3D geodeticPosition = ellipsoid->toGeodetic3D(cartesianPosition);
-    //positionList.push_back(ellipsoid->toGeodetic3D(cartesianPosition));
-    //angle = angle.add(delta);
+
+  const double startAngleInRadians = startAngle._radians;
+  for (int i = 0; i < splits; i++) {
+    const double angleInRadians = startAngleInRadians + (deltaInRadians * i);
+
+    const Vector3D cartesianPosition = northInPlane.rotateAroundAxis(normal,
+                                                                     Angle::fromRadians(angleInRadians));
+
+    result.push_back( new Geodetic3D(ellipsoid->toGeodetic3D(cartesianPosition)) );
   }
   
-  return positionList;
+  return result;
 }
