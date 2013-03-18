042cf90693f2f4d1c639b07f8a2d80301f8e93c2
Diego Gomez Deck <dgd@igosoftware.es>
2013-03-18 12:51:11 -0400

    -

--- iOS/G3MiOSSDK/Commons/Geometry/LayoutUtils.cpp
+++ iOS/G3MiOSSDK/Commons/Geometry/LayoutUtils.cpp
@@ -13,26 +13,26 @@
 #include "IMathUtils.hpp"
 
 std::vector<Geodetic3D*> LayoutUtils::splitOverCircle(const Ellipsoid* ellipsoid,
-                                                     const Geodetic3D& center,
-                                                     double radiusInMeters,
-                                                     int splits,
-                                                     const Angle& startAngle) {
+                                                      const Geodetic3D& center,
+                                                      double radiusInMeters,
+                                                      int splits,
+                                                      const Angle& startAngle) {
   std::vector<Geodetic3D*> result;
-  const double deltaInRadians = (IMathUtils::instance()->pi() * 2.0) / splits;
-
-  const Vector3D cartesianCenter  = ellipsoid->toCartesian(center);
-  const Vector3D normal           = ellipsoid->geodeticSurfaceNormal(center);
-  const Vector3D northInPlane     = Vector3D::upZ().projectionInPlane(normal).normalized().times(radiusInMeters);
 
   const double startAngleInRadians = startAngle._radians;
+  const double deltaInRadians      = (IMathUtils::instance()->pi() * 2.0) / splits;
+  const Vector3D cartesianCenter   = ellipsoid->toCartesian(center);
+  const Vector3D normal            = ellipsoid->geodeticSurfaceNormal(center);
+  const Vector3D northInPlane      = Vector3D::upZ().projectionInPlane(normal).normalized().times(radiusInMeters);
+
   for (int i = 0; i < splits; i++) {
     const double angleInRadians = startAngleInRadians + (deltaInRadians * i);
 
     const Vector3D cartesianPosition = northInPlane.rotateAroundAxis(normal,
                                                                      Angle::fromRadians(angleInRadians));
 
-    result.push_back( new Geodetic3D(ellipsoid->toGeodetic3D(cartesianPosition)) );
+    result.push_back( new Geodetic3D( ellipsoid->toGeodetic3D(cartesianPosition) ) );
   }
-  
+
   return result;
 }
