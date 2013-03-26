d3a905b0f8b82de1d9f9d16a50ebc2c99bb751ad
Diego Gomez Deck <dgd@consultar.com>
2013-03-26 12:39:09 -0400

    New symbolization in GEORendering

--- iOS/G3MiOSSDK/Commons/GEO/GEOSymbolizer.hpp
+++ iOS/G3MiOSSDK/Commons/GEO/GEOSymbolizer.hpp
@@ -9,8 +9,6 @@
 #ifndef __G3MiOSSDK__GEOSymbolizer__
 #define __G3MiOSSDK__GEOSymbolizer__
 
-//class Mesh;
-//class G3MRenderContext;
 class GEOSymbol;
 class GEO2DMultiLineStringGeometry;
 class GEO2DLineStringGeometry;
@@ -18,12 +16,13 @@ class GEO2DLineStringGeometry;
 
 class GEOSymbolizer {
 public:
-#ifdef C_CODE
+//#ifdef C_CODE
+//  virtual ~GEOSymbolizer() { }
+//#endif
+//#ifdef JAVA_CODE
+//  public void dispose();
+//#endif
   virtual ~GEOSymbolizer() { }
-#endif
-#ifdef JAVA_CODE
-  public void dispose();
-#endif
 
   virtual std::vector<GEOSymbol*>* createSymbols(const GEO2DMultiLineStringGeometry* geometry) const = 0;
   virtual std::vector<GEOSymbol*>* createSymbols(const GEO2DLineStringGeometry*      geometry) const = 0;
