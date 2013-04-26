0b915501494e043c78288a8b34d01fe49a4a1f07
aldeanueva <aldeanueva@igosoftware.es>
2013-04-26 18:28:11 +0200

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Camera.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Camera.java
@@ -189,17 +189,22 @@ public class Camera
   {
     final Vector2D p = getModelViewMatrix().project(point, 0, 0, _width, _height);
   
-    final IMathUtils mu = IMathUtils.instance();
-  
-    return new Vector2I(mu.round((float) p._x), mu.round((float)((double) _height - p._y)));
+  //  const IMathUtils* mu = IMathUtils::instance();
+  //
+  //  return Vector2I(mu->round( (float) p._x ),
+  //                  mu->round( (float) ((double) _height - p._y) ) );
+  //
+    return new Vector2I((int) p._x, (int)(_height - p._y));
   }
   public final Vector2I point2Pixel(Vector3F point)
   {
     final Vector2F p = getModelViewMatrix().project(point, 0, 0, _width, _height);
   
-    final IMathUtils mu = IMathUtils.instance();
-  
-    return new Vector2I(mu.round(p._x), mu.round((float) _height - p._y));
+  //  const IMathUtils* mu = IMathUtils::instance();
+  //
+  //  return Vector2I(mu->round( p._x ),
+  //                  mu->round( (float) _height - p._y ) );
+    return new Vector2I((int) p._x, (int)(_height - p._y));
   }
 
   public final int getWidth()
