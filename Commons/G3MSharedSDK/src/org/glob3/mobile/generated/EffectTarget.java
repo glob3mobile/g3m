b712448c074cba08fe09b97f212f57eab4624de1
aldeanueva <aldeanueva@igosoftware.es>
2013-05-29 15:35:03 +0200

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/EffectTarget.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/EffectTarget.java
@@ -28,6 +28,6 @@ package org.glob3.mobile.generated;
 
 public interface EffectTarget extends GLClient
 {
-  public void isEffectTarget() { return true;} //CONVERSOR GENERATE ABSTRACT CLASS INSTEAD OF INTERFACE
+  public bool isEffectTarget() { return true;} //CONVERSOR GENERATE ABSTRACT CLASS INSTEAD OF INTERFACE
   public void dispose(){ }
 }
