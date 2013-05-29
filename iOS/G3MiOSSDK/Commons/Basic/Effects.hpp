17e29253ce85e977d12fb18792f00963c8a6929c
Jose Miguel SN <josemiguelsn@MacBook-Pro-de-Jose-2.local>
2013-05-29 13:50:02 +0100

    EffectTarget is abstract class

--- iOS/G3MiOSSDK/Commons/Basic/Effects.hpp
+++ iOS/G3MiOSSDK/Commons/Basic/Effects.hpp
@@ -30,6 +30,7 @@ public:
   virtual ~EffectTarget() { }
 #endif
 #ifdef JAVA_CODE
+  public void isEffectTarget() { return true;} //CONVERSOR GENERATE ABSTRACT CLASS INSTEAD OF INTERFACE
   public void dispose(){ }
 #endif
 };
