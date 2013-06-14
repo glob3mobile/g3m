26ceb1500b70dab02e10134e0d9ffc2cd9a7d7be
Jose Miguel SN <josemiguelsn@MacBook-Pro-de-Jose-2.local>
2013-06-14 14:34:10 +0100

    Fixing Android marks

--- iOS/G3MiOSSDK/Commons/GL/GPUUniform.cpp
+++ iOS/G3MiOSSDK/Commons/GL/GPUUniform.cpp
@@ -15,21 +15,3 @@ void GPUUniformValue::setValueToLinkedUniform() const{
     _uniform->set((GPUUniformValue*)this);
   }
 }
-
-void GPUUniform::set(GPUUniformValue* v){
-  if (_type != v->getType()){ //type checking
-    //      delete v;
-    ILogger::instance()->logError("Attempting to set uniform " + _name + "with invalid value type.");
-    return;
-  }
-  if (_value == NULL || !_value->isEqualsTo(v)){
-    _dirty = true;
-    
-    if (_value != NULL){
-      v->setLastGPUUniformValue(_value); //Multiply matrix when needed
-      _value->copyFrom(v);
-    } else{
-      _value = v->deepCopy();
-    }
-  }
-}
--- iOS/G3MiOSSDK/Commons/GL/GPUUniform.hpp
+++ iOS/G3MiOSSDK/Commons/GL/GPUUniform.hpp
@@ -85,7 +85,23 @@ public:
     _dirty = false;
   }
   
-  void set(GPUUniformValue* v);
+  void set(GPUUniformValue* v){
+    if (_type != v->getType()){ //type checking
+      //      delete v;
+      ILogger::instance()->logError("Attempting to set uniform " + _name + "with invalid value type.");
+      return;
+    }
+    if (_value == NULL || !_value->isEqualsTo(v)){
+      _dirty = true;
+      
+      if (_value != NULL){
+        v->setLastGPUUniformValue(_value); //Multiply matrix when needed
+        _value->copyFrom(v);
+      } else{
+        _value = v->deepCopy();
+      }
+    }
+  }
   
   virtual void applyChanges(GL* gl){
     if (_dirty){
