fa784eaa898068c683a5e06ee685aea354d9c867
Jose Miguel SN <josemiguelsn@MacBook-Pro-de-Jose-2.local>
2013-06-26 12:34:35 +0100

    BusyMesh fixed

--- iOS/G3MiOSSDK/Commons/Rendererers/BusyMeshRenderer.cpp
+++ iOS/G3MiOSSDK/Commons/Rendererers/BusyMeshRenderer.cpp
@@ -117,21 +117,22 @@ void BusyMeshRenderer::createGLState(){
   
   GPUProgramState& progState = * _glState.getGPUProgramState();
   
-  progState.setUniformValue(GPUVariable::EnableTexture, false);
-  progState.setUniformValue(GPUVariable::POINT_SIZE, (float)1.0);
-  progState.setUniformValue(GPUVariable::SCALE_TEXTURE_COORDS, Vector2D(1.0,1.0));
-  progState.setUniformValue(GPUVariable::TRANSLATION_TEXTURE_COORDS, Vector2D(0.0,0.0));
+//  progState.setUniformValue(GPUVariable::EnableTexture, false);
+//  progState.setUniformValue(GPUVariable::POINT_SIZE, (float)1.0);
+//  progState.setUniformValue(GPUVariable::SCALE_TEXTURE_COORDS, Vector2D(1.0,1.0));
+//  progState.setUniformValue(GPUVariable::TRANSLATION_TEXTURE_COORDS, Vector2D(0.0,0.0));
   
-  progState.setUniformValue(GPUVariable::ColorPerVertexIntensity, (float)0.0);
-  progState.setUniformValue(GPUVariable::EnableFlatColor, false);
-  progState.setUniformValue(GPUVariable::FLAT_COLOR, (float)0.0, (float)0.0, (float)0.0, (float)0.0);
-  progState.setUniformValue(GPUVariable::FlatColorIntensity, (float)0.0);
+//  progState.setUniformValue(GPUVariable::ColorPerVertexIntensity, (float)0.0);
+//  progState.setUniformValue(GPUVariable::EnableFlatColor, false);
+//  progState.setUniformValue(GPUVariable::FLAT_COLOR, (float)0.0, (float)0.0, (float)0.0, (float)0.0);
+//  progState.setUniformValue(GPUVariable::FlatColorIntensity, (float)0.0);
   
-  progState.setAttributeEnabled(GPUVariable::TEXTURE_COORDS, false);
-  progState.setAttributeEnabled(GPUVariable::COLOR, false);
+//  progState.setAttributeEnabled(GPUVariable::TEXTURE_COORDS, false);
+//  progState.setAttributeEnabled(GPUVariable::COLOR, false);
   
   //Modelview and projection
   _modelviewMatrix = MutableMatrix44D::createRotationMatrix(Angle::fromDegrees(_degrees), Vector3D(0, 0, -1));
-  _glState.getGPUProgramState()->setUniformMatrixValue(GPUVariable::MODELVIEW, _projectionMatrix.multiply(_modelviewMatrix), false);
+  _glState.getGPUProgramState()->setUniformMatrixValue(GPUVariable::MODELVIEW,
+                                                       _projectionMatrix.multiply(_modelviewMatrix), false);
   
 }
