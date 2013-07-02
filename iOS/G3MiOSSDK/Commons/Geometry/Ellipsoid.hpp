fec9c52aac5fffbdaef57b5633d31b86e80f1520
Diego Gomez Deck <dgd@consultar.com>
2013-07-02 14:42:46 -0400

    -

--- iOS/G3MiOSSDK/Commons/Geometry/Ellipsoid.hpp
+++ iOS/G3MiOSSDK/Commons/Geometry/Ellipsoid.hpp
@@ -13,18 +13,19 @@
 
 #include "Vector3D.hpp"
 
-class Ellipsoid: public Geometry3D{
-  
+class Ellipsoid: public Geometry3D {
+private:
   const Vector3D _center;
   const Vector3D _radii;
-  
+
   const Vector3D _radiiSquared;
   const Vector3D _radiiToTheFourth;
   const Vector3D _oneOverRadiiSquared;
-  
+
 public:
-  
-  Ellipsoid(const Vector3D& center, const Vector3D& radii):
+
+  Ellipsoid(const Vector3D& center,
+            const Vector3D& radii):
   _center(center), _radii(radii),
   _radiiSquared(Vector3D(radii._x * radii._x ,
                          radii._y * radii._y,
@@ -35,16 +36,32 @@ public:
   _oneOverRadiiSquared(Vector3D(1.0 / (radii._x * radii._x ),
                                 1.0 / (radii._y * radii._y),
                                 1.0 / (radii._z * radii._z))){}
-  
-  Vector3D getCenter() const { return _center;}
-  Vector3D getRadii() const { return _radii;}
-  Vector3D getRadiiSquared() const { return _radiiSquared;}
-  Vector3D getRadiiToTheFourth() const { return _radiiToTheFourth;}
-  Vector3D getOneOverRadiiSquared() const { return _oneOverRadiiSquared;}
-  
-  double getMeanRadius() const { return (_radii._x + _radii._y + _radii._y) /3;}
+
+  Vector3D getCenter() const {
+    return _center;
+  }
+
+  Vector3D getRadii() const {
+    return _radii;
+  }
+
+  Vector3D getRadiiSquared() const {
+    return _radiiSquared;
+  }
+
+  Vector3D getRadiiToTheFourth() const {
+    return _radiiToTheFourth;
+  }
+
+  Vector3D getOneOverRadiiSquared() const {
+    return _oneOverRadiiSquared;
+  }
+
+  double getMeanRadius() const {
+    return (_radii._x + _radii._y + _radii._y) /3;
+  }
   
   
 };
 
-#endif /* defined(__G3MiOSSDK__Ellipsoid__) */
+#endif
