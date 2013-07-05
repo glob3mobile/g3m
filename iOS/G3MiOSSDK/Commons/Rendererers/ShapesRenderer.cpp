89a7e0263a09c4351e581a2ac377ae14fe7864e1
Jose Miguel SN <josemiguelsn@MacBook-Pro-de-Jose-2.local>
2013-07-05 15:28:30 +0100

    setColor in state

--- iOS/G3MiOSSDK/Commons/Rendererers/ShapesRenderer.cpp
+++ iOS/G3MiOSSDK/Commons/Rendererers/ShapesRenderer.cpp
@@ -62,11 +62,8 @@ void ShapesRenderer::render(const G3MRenderContext* rc) {
   
   //Setting camera matrixes
   MutableMatrix44D m = rc->getCurrentCamera()->getModelViewMatrix();
-  //_glState.getGPUProgramState()->setUniformMatrixValue(MODELVIEW, m, false);
-  _glState.setModelView(rc->getCurrentCamera()->getModelViewMatrix().asMatrix44D(), false);
-
-  //_glStateTransparent.getGPUProgramState()->setUniformMatrixValue(MODELVIEW, m, false);
-  _glStateTransparent.setModelView(rc->getCurrentCamera()->getModelViewMatrix().asMatrix44D(), false);
+  _glState.setModelView(m.asMatrix44D(), false);
+  _glStateTransparent.setModelView(m.asMatrix44D(), false);
   
   const int shapesCount = _shapes.size();
   for (int i = 0; i < shapesCount; i++) {
