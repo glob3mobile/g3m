b647e3c4e539e2cc4e1bada0fb6df21c9d323608
aldeanueva <aldeanueva@igosoftware.es>
2013-07-18 20:49:56 +0200

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Angle.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Angle.java
@@ -20,36 +20,49 @@ package org.glob3.mobile.generated;
 
 
 
+//#define TO_RADIANS(degrees) ((degrees) / 180.0 * 3.14159265358979323846264338327950288)
+//#define TO_DEGREES(radians) ((radians) * (180.0 / 3.14159265358979323846264338327950288))
+
 
 public class Angle
 {
-  private Angle(double degrees)
+//  Angle(const double degrees) :
+//  _degrees( degrees ),
+//  _radians( degrees / 180.0 * 3.14159265358979323846264338327950288 )
+//  {
+//  }
+
+  private Angle(double degrees, double radians)
   {
      _degrees = degrees;
-     _radians = degrees / 180.0 * 3.14159265358979323846264338327950288;
+     _radians = radians;
   }
+
 
   public final double _degrees;
   public final double _radians;
 
   public static Angle fromDegrees(double degrees)
   {
-    return new Angle(degrees);
+    return new Angle(degrees, ((degrees) / 180.0 * 3.14159265358979323846264338327950288));
   }
 
   public static Angle fromDegreesMinutes(double degrees, double minutes)
   {
-    return new Angle(degrees + (minutes / 60.0));
+    final double d = degrees + (minutes / 60.0);
+    return new Angle(d, ((d) / 180.0 * 3.14159265358979323846264338327950288));
   }
 
   public static Angle fromDegreesMinutesSeconds(double degrees, double minutes, double seconds)
   {
-    return new Angle(degrees + (minutes / 60.0) + (seconds / 3600.0));
+    final double d = degrees + (minutes / 60.0) + (seconds / 3600.0);
+    return new Angle(d, ((d) / 180.0 * 3.14159265358979323846264338327950288));
   }
 
   public static Angle fromRadians(double radians)
   {
-    return new Angle(radians / 3.14159265358979323846264338327950288 * 180.0);
+//    return Angle(radians / 3.14159265358979323846264338327950288 * 180.0);
+    return new Angle(((radians) * (180.0 / 3.14159265358979323846264338327950288)), radians);
   }
 
   public static Angle min(Angle a1, Angle a2)
@@ -133,37 +146,41 @@ public class Angle
 
   public final Angle add(Angle a)
   {
-    return new Angle(_degrees + a._degrees);
+    final double r = _radians + a._radians;
+    return new Angle(((r) * (180.0 / 3.14159265358979323846264338327950288)), r);
   }
 
   public final Angle sub(Angle a)
   {
-    return new Angle(_degrees - a._degrees);
+    final double r = _radians - a._radians;
+    return new Angle(((r) * (180.0 / 3.14159265358979323846264338327950288)), r);
   }
 
   public final Angle times(double k)
   {
-    return new Angle(k * _degrees);
+    final double r = k * _radians;
+    return new Angle(((r) * (180.0 / 3.14159265358979323846264338327950288)), r);
   }
 
   public final Angle div(double k)
   {
-    return new Angle(_degrees / k);
+    final double r = _radians / k;
+    return new Angle(((r) * (180.0 / 3.14159265358979323846264338327950288)), r);
   }
 
   public final double div(Angle k)
   {
-    return _degrees / k._degrees;
+    return _radians / k._radians;
   }
 
   public final boolean greaterThan(Angle a)
   {
-    return (_degrees > a._degrees);
+    return (_radians > a._radians);
   }
 
   public final boolean lowerThan(Angle a)
   {
-    return (_degrees < a._degrees);
+    return (_radians < a._radians);
   }
 
   public final Angle clampedTo(Angle min, Angle max)
@@ -219,7 +236,7 @@ public class Angle
     {
       degrees -= 360;
     }
-    return new Angle(degrees);
+    return new Angle(degrees, ((degrees) / 180.0 * 3.14159265358979323846264338327950288));
   }
 
   public final boolean isZero()
