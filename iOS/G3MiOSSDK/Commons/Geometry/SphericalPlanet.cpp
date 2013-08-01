df3ba132094ec3d570379fdd4d3535944fbc7157
Agustín Trujillo <agustin@MacBook-Pro-de-Agustin.local>
2013-08-01 21:28:28 +0100

    refactoring doubleDragHandler

--- iOS/G3MiOSSDK/Commons/Geometry/SphericalPlanet.cpp
+++ iOS/G3MiOSSDK/Commons/Geometry/SphericalPlanet.cpp
@@ -337,197 +337,117 @@ MutableMatrix44D SphericalPlanet::doubleDrag(const Vector3D& finalRay0,
   // init params
   const IMathUtils* mu = IMathUtils::instance();
   MutableVector3D positionCamera = _origin;
-
-  // compute camera translation using numerical iterations until convergence
-  double dAccum=0;
+  const double finalRaysAngle = finalRay0.angleBetween(finalRay1).degrees();
+  const double factor = finalRaysAngle / _angleBetweenInitialRays;
+  double dAccum=0, angle0, angle1;
+  double distance = _origin.sub(_centerPoint).length();
+  
+  // compute estimated camera translation: step 0
+  double d = distance*(factor-1)/factor;
+  MutableMatrix44D translation = MutableMatrix44D::createTranslationMatrix(_centerRay.asVector3D().normalized().times(d));
+  positionCamera = positionCamera.transformedBy(translation, 1.0);
+  dAccum += d;
   {
-    double finalRaysAngle = finalRay0.angleBetween(finalRay1).degrees();
-    double factor = finalRaysAngle / _angleBetweenInitialRays;
-    
-    // compute estimated camera translation
-    double distance = _origin.sub(_centerPoint).length();
-    double d = distance*(factor-1)/factor;
-    //tempCamera.moveForward(d);
-    MutableMatrix44D translation = MutableMatrix44D::createTranslationMatrix(_centerRay.asVector3D().normalized().times(d));
-    positionCamera = positionCamera.transformedBy(translation, 1.0);
-    dAccum += d;
-    //double angle0 = tempCamera.compute3DAngularDistance(pixel0, pixel1)._degrees;
-    double angle0;
-    {
-      const Vector3D point0 = closestIntersection(positionCamera.asVector3D(), finalRay0);
-      if (point0.isNan()) {
-        printf ("error: point0 nan!\n");
-      }
-      const Vector3D point1 = closestIntersection(positionCamera.asVector3D(), finalRay1);
-      if (point1.isNan()) {
-        printf ("error: point1 nan!\n");
-      }
-      angle0 = point0.angleBetween(point1)._degrees;
-    }
-    
-    
-    
-    
+    const Vector3D point0 = closestIntersection(positionCamera.asVector3D(), finalRay0);
+    const Vector3D point1 = closestIntersection(positionCamera.asVector3D(), finalRay1);
+    angle0 = point0.angleBetween(point1)._degrees;
     if (mu->isNan(angle0)) return MutableMatrix44D::invalid();
-    //printf("distancia angular original = %.4f     d=%.1f   angulo step0=%.4f\n", angle, d, angle0);
+  }
     
-    // step 1
-    d = mu->abs((distance-d)*0.3);
-    if (angle0 < _angleBetweenInitialPoints) d*=-1;
-    //tempCamera.moveForward(d);
-    translation = MutableMatrix44D::createTranslationMatrix(_centerRay.asVector3D().normalized().times(d));
-    positionCamera = positionCamera.transformedBy(translation, 1.0);
-    
-    dAccum += d;
-    //tempCamera.updateModelMatrix();
-    //double angle1 = tempCamera.compute3DAngularDistance(pixel0, pixel1)._degrees;
-    double angle1;
-    {
-      const Vector3D point0 = closestIntersection(positionCamera.asVector3D(), finalRay0);
-      if (point0.isNan()) {
-        printf ("error: point0 nan!\n");
-      }
-      const Vector3D point1 = closestIntersection(positionCamera.asVector3D(), finalRay1);
-      if (point1.isNan()) {
-        printf ("error: point1 nan!\n");
-      }
-      angle1 = point0.angleBetween(point1)._degrees;
-    }
-    
-    
-    
-    
-    
-    double angle_n1=angle0, angle_n=angle1;
-    
-    // iterations
-    //    int iter=0;
-    double precision = mu->pow(10, mu->log10(distance)-8.5);
-    while (mu->abs(angle_n-_angleBetweenInitialPoints) > precision) {
-      //      iter++;
-      if ((angle_n1-angle_n)/(angle_n-_angleBetweenInitialPoints) < 0) d*=-0.5;
-      //tempCamera.moveForward(d);
-      translation = MutableMatrix44D::createTranslationMatrix(_centerRay.asVector3D().normalized().times(d));
-      positionCamera = positionCamera.transformedBy(translation, 1.0);
-      
-      dAccum += d;
-      //tempCamera.updateModelMatrix();
-      angle_n1 = angle_n;
-      //angle_n = tempCamera.compute3DAngularDistance(pixel0, pixel1)._degrees;
-      {
-        const Vector3D point0 = closestIntersection(positionCamera.asVector3D(), finalRay0);
-        if (point0.isNan()) {
-          printf ("error: point0 nan!\n");
-        }
-        const Vector3D point1 = closestIntersection(positionCamera.asVector3D(), finalRay1);
-        if (point1.isNan()) {
-          printf ("error: point1 nan!\n");
-        }
-        angle_n = point0.angleBetween(point1)._degrees;
-      }
-      
-    }
-    //printf("-----------  iteraciones=%d  precision=%f angulo final=%.4f  distancia final=%.1f\n", iter, precision, angle_n, dAccum);
+  // compute estimated camera translation: step 1
+  d = mu->abs((distance-d)*0.3);
+  if (angle0 < _angleBetweenInitialPoints) d*=-1;
+  translation = MutableMatrix44D::createTranslationMatrix(_centerRay.asVector3D().normalized().times(d));
+  positionCamera = positionCamera.transformedBy(translation, 1.0);
+  dAccum += d;
+  {
+    const Vector3D point0 = closestIntersection(positionCamera.asVector3D(), finalRay0);
+    const Vector3D point1 = closestIntersection(positionCamera.asVector3D(), finalRay1);
+    angle1 = point0.angleBetween(point1)._degrees;
+    if (mu->isNan(angle1)) return MutableMatrix44D::invalid();
   }
   
-  
-  
-  //// *********** FUNCIONA HASTA AQUI
-  
-  // create temp camera to test gesture first
-  //Camera tempCamera(_camera0);
+  // compute estimated camera translation: steps 2..n until convergence
+  //int iter=0;
+  double precision = mu->pow(10, mu->log10(distance)-7.0);
+  double angle_n1=angle0, angle_n=angle1;
+  while (mu->abs(angle_n-_angleBetweenInitialPoints) > precision) {
+    // iter++;
+    if ((angle_n1-angle_n)/(angle_n-_angleBetweenInitialPoints) < 0) d*=-0.5;
+    translation = MutableMatrix44D::createTranslationMatrix(_centerRay.asVector3D().normalized().times(d));
+    positionCamera = positionCamera.transformedBy(translation, 1.0);
+    dAccum += d;
+    angle_n1 = angle_n;
+    {
+      const Vector3D point0 = closestIntersection(positionCamera.asVector3D(), finalRay0);
+      const Vector3D point1 = closestIntersection(positionCamera.asVector3D(), finalRay1);
+      angle_n = point0.angleBetween(point1)._degrees;
+      if (mu->isNan(angle_n)) return MutableMatrix44D::invalid();
+    }
+  }
+  //if (iter>2) printf("-----------  iteraciones=%d  precision=%f angulo final=%.4f  distancia final=%.1f\n", iter, precision, angle_n, dAccum);
+
+  // start to compound matrix
+  MutableMatrix44D matrix = MutableMatrix44D::identity();
   positionCamera = _origin;
   MutableVector3D viewDirection = _centerRay;
   MutableVector3D ray0 = finalRay0.asMutableVector3D();
   MutableVector3D ray1 = finalRay1.asMutableVector3D();
   
   // drag from initialPoint to centerPoint
-  MutableMatrix44D matrix = MutableMatrix44D::identity();
   {
     Vector3D initialPoint = _initialPoint.asVector3D();
     const Vector3D rotationAxis = initialPoint.cross(_centerPoint.asVector3D());
     const Angle rotationDelta = Angle::fromRadians( - mu->acos(_initialPoint.normalized().dot(_centerPoint.normalized())) );
-    if (rotationDelta.isNan()) return MutableMatrix44D::invalid();
-    //tempCamera.rotateWithAxis(rotationAxis, rotationDelta);
-    
+    if (rotationDelta.isNan()) return MutableMatrix44D::invalid();    
     MutableMatrix44D rotation = MutableMatrix44D::createRotationMatrix(rotationDelta, rotationAxis);
     positionCamera = positionCamera.transformedBy(rotation, 1.0);
     viewDirection = viewDirection.transformedBy(rotation, 0.0);
-    //upDirection = upDirection.transformedBy(rotation, 0.0);
     ray0 = ray0.transformedBy(rotation, 0.0);
     ray1 = ray1.transformedBy(rotation, 0.0);
-    matrix = matrix.multiply(rotation);
-    //tempCamera.applyTransform(rotation);
-    
+    matrix = rotation.multiply(matrix);
   }
   
-  // move the camera
-  if (true) {
-    //tempCamera.moveForward(dAccum);
-    
+  // move the camera forward
+  {
     MutableMatrix44D translation = MutableMatrix44D::createTranslationMatrix(viewDirection.asVector3D().normalized().times(dAccum));
     positionCamera = positionCamera.transformedBy(translation, 1.0);
     matrix = translation.multiply(matrix);
-    //tempCamera.applyTransform(translation);
-    
   }
   
   // compute 3D point of view center
-  //tempCamera.updateModelMatrix();
-  //Vector3D centerPoint2 = tempCamera.getXYZCenterOfView();
   Vector3D centerPoint2 = closestIntersection(positionCamera.asVector3D(), viewDirection.asVector3D());
   
-  
-  // middle point in 3D
-  //Vector3D P0 = tempCamera.pixel2PlanetPoint(pixel0);
+  // compute middle point in 3D
   Vector3D P0 = closestIntersection(positionCamera.asVector3D(), ray0.asVector3D());
-  //Vector3D P1 = tempCamera.pixel2PlanetPoint(pixel1);
   Vector3D P1 = closestIntersection(positionCamera.asVector3D(), ray1.asVector3D());
-  //const Planet *planet = eventContext->getPlanet();
   Geodetic2D g = getMidPoint(toGeodetic2D(P0), toGeodetic2D(P1));
   Vector3D finalPoint = toCartesian(g);
   
   // drag globe from centerPoint to finalPoint
-  const Vector3D rotationAxis = centerPoint2.cross(finalPoint);
-  const Angle rotationDelta = Angle::fromRadians( - mu->acos(centerPoint2.normalized().dot(finalPoint.normalized())) );
-  if (rotationDelta.isNan()) return MutableMatrix44D::invalid();
-  //tempCamera.rotateWithAxis(rotationAxis, rotationDelta);
-  
-  MutableMatrix44D rotation = MutableMatrix44D::createRotationMatrix(rotationDelta, rotationAxis);
-  positionCamera = positionCamera.transformedBy(rotation, 1.0);
-  viewDirection = viewDirection.transformedBy(rotation, 0.0);
-  //upDirection = upDirection.transformedBy(rotation, 0.0);
-  ray0 = ray0.transformedBy(rotation, 0.0);
-  ray1 = ray1.transformedBy(rotation, 0.0);
-  matrix = rotation.multiply(matrix);
-  //tempCamera.applyTransform(rotation);
-  
-  
-  
-  
-  
-  // the gesture was valid. Copy data to final camera
-  //tempCamera.updateModelMatrix();
+  {
+    const Vector3D rotationAxis = centerPoint2.cross(finalPoint);
+    const Angle rotationDelta = Angle::fromRadians( - mu->acos(centerPoint2.normalized().dot(finalPoint.normalized())) );
+    if (rotationDelta.isNan()) return MutableMatrix44D::invalid();
+    MutableMatrix44D rotation = MutableMatrix44D::createRotationMatrix(rotationDelta, rotationAxis);
+    positionCamera = positionCamera.transformedBy(rotation, 1.0);
+    viewDirection = viewDirection.transformedBy(rotation, 0.0);
+    ray0 = ray0.transformedBy(rotation, 0.0);
+    ray1 = ray1.transformedBy(rotation, 0.0);
+    matrix = rotation.multiply(matrix);
+  }
   
   // camera rotation
-  if (true) {
+  {
     Vector3D normal = geodeticSurfaceNormal(centerPoint2);
     Vector3D v0     = _initialPoint0.asVector3D().sub(centerPoint2).projectionInPlane(normal);
-    //Vector3D v1     = tempCamera.pixel2PlanetPoint(pixel0).sub(centerPoint2).projectionInPlane(normal);
-    
-    Vector3D P0 = closestIntersection(positionCamera.asVector3D(), ray0.asVector3D());
-    Vector3D v1 = P0.sub(centerPoint2).projectionInPlane(normal);
-    
-    
+    Vector3D P0     = closestIntersection(positionCamera.asVector3D(), ray0.asVector3D());
+    Vector3D v1     = P0.sub(centerPoint2).projectionInPlane(normal);
     double angle    = v0.angleBetween(v1)._degrees;
     double sign     = v1.cross(v0).dot(normal);
     if (sign<0) angle = -angle;
-    //tempCamera.rotateWithAxisAndPoint(normal, centerPoint2, Angle::fromDegrees(angle));
-    
     MutableMatrix44D rotation = MutableMatrix44D::createGeneralRotationMatrix(Angle::fromDegrees(angle), normal, centerPoint2);
     matrix = rotation.multiply(matrix);
-    //tempCamera.applyTransform(rotation);
-    
   }
   
   return matrix;
