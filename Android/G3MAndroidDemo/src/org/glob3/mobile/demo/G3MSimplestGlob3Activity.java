604c71f3b52c0732c7b85a1683d0ff65e549865a
Jose Miguel SN <josemiguelsn@MacBook-Pro-de-Jose-2.local>
2013-08-08 12:51:45 +0100

    ...

--- Android/G3MAndroidDemo/src/org/glob3/mobile/demo/G3MSimplestGlob3Activity.java
+++ Android/G3MAndroidDemo/src/org/glob3/mobile/demo/G3MSimplestGlob3Activity.java
@@ -113,9 +113,7 @@ public class G3MSimplestGlob3Activity
       final CameraRenderer cameraRenderer = new CameraRenderer();
       final boolean useInertia = true;
       cameraRenderer.addHandler(new CameraSingleDragHandler(useInertia));
-      final boolean processRotation = true;
-      final boolean processZoom = true;
-      cameraRenderer.addHandler(new CameraDoubleDragHandler(processRotation, processZoom));
+      cameraRenderer.addHandler(new CameraDoubleDragHandler());
       cameraRenderer.addHandler(new CameraRotationHandler());
       cameraRenderer.addHandler(new CameraDoubleTapHandler());
 
