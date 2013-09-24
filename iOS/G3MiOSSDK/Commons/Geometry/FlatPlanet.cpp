718b00e519bded7d7a618273dea1c19086089845
Agustín Trujillo <agustin@MacBook-Pro-de-Agustin.local>
2013-09-24 11:57:52 +0100

    remove unused methods in FlatPlanet.cpp

--- iOS/G3MiOSSDK/Commons/Geometry/FlatPlanet.cpp
+++ iOS/G3MiOSSDK/Commons/Geometry/FlatPlanet.cpp
@@ -79,15 +79,6 @@ Geodetic2D FlatPlanet::getMidPoint (const Geodetic2D& P0, const Geodetic2D& P1) 
 }
 
 
-std::list<Vector3D> FlatPlanet::computeCurve(const Vector3D& start,
-                                             const Vector3D& stop,
-                                             double granularity) const {
-  int TODO_compute_plane_curve;
-  std::list<Vector3D> positions;
-  return positions;
-}
-
-
 // compute distance from two points
 double FlatPlanet::computePreciseLatLonDistance(const Geodetic2D& g1,
                                                 const Geodetic2D& g2) const {
@@ -109,12 +100,6 @@ Vector3D FlatPlanet::closestIntersection(const Vector3D& pos,
     return Vector3D::nan();
   }
   return pos.add(ray.times(distances[0]));
-}
-
-
-Vector3D FlatPlanet::closestPointToSphere(const Vector3D& pos, const Vector3D& ray) const {
-  int TODO_agustin;
-  return Vector3D::zero;
 }
 
 
