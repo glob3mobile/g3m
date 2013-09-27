8a0f38d0324ec4ae13590c616a15523fafa0bb86
aldeanueva <aldeanueva@igosoftware.es>
2013-09-27 22:50:21 +0200

    Generated

---
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/CanvasImageFactory.java
@@ -0,0 +1,46 @@
+package org.glob3.mobile.generated; 
+//
+//  CanvasImageFactory.cpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 9/27/13.
+//
+//
+
+//
+//  CanvasImageFactory.hpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 9/27/13.
+//
+//
+
+
+
+//class ICanvas;
+
+public abstract class CanvasImageFactory extends ImageFactory
+{
+
+  protected abstract void drawOn(ICanvas canvas, int width, int height);
+
+  public CanvasImageFactory()
+  {
+
+  }
+
+  public final void create(G3MRenderContext rc, int width, int height, IImageListener listener, boolean deleteListener)
+  {
+  
+    ICanvas canvas = rc.getFactory().createCanvas();
+    canvas.initialize(width, height);
+  
+    drawOn(canvas, width, height);
+  
+    canvas.createImage(listener, deleteListener);
+  
+    if (canvas != null)
+       canvas.dispose();
+  }
+
+}
---
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/ImageFactory.java
@@ -0,0 +1,29 @@
+package org.glob3.mobile.generated; 
+//
+//  ImageFactory.cpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 9/27/13.
+//
+//
+
+//
+//  ImageFactory.hpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 9/27/13.
+//
+//
+
+
+//class G3MRenderContext;
+//class IImageListener;
+
+public abstract class ImageFactory
+{
+//C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
+//  void dispose();
+
+  public abstract void create(G3MRenderContext rc, int width, int height, IImageListener listener, boolean deleteListener);
+
+}
