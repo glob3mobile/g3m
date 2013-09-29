82867a72277707d2515a7c7f5eb410f4ecd9445f
aldeanueva <aldeanueva@igosoftware.es>
2013-09-29 20:52:59 +0200

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/HUDErrorRenderer.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/HUDErrorRenderer.java
@@ -17,7 +17,13 @@ package org.glob3.mobile.generated;
 
 
 
-public abstract class HUDErrorRenderer extends ErrorRenderer
-{
 
+//C++ TO JAVA CONVERTER TODO TASK: Multiple inheritance is not available in Java:
+public class HUDErrorRenderer extends HUDImageRenderer, ErrorRenderer implements ImageFactory
+{
+  public HUDErrorRenderer()
+  {
+     super(this);
+
+  }
 }
