6d14a20bd44b3e6c348f1a172757e477f29962a7
Diego Gomez Deck <dgd@consultar.com>
2013-10-05 17:31:12 -0400

    -

--- Android/G3MAndroidDemo/src/org/glob3/mobile/demo/PointCloudActivity.java
+++ Android/G3MAndroidDemo/src/org/glob3/mobile/demo/PointCloudActivity.java
@@ -2,19 +2,21 @@
 
 package org.glob3.mobile.demo;
 
+import org.glob3.mobile.generated.Angle;
 import org.glob3.mobile.generated.BSONParser;
 import org.glob3.mobile.generated.Color;
 import org.glob3.mobile.generated.DirectMesh;
 import org.glob3.mobile.generated.FloatBufferBuilderFromColor;
 import org.glob3.mobile.generated.FloatBufferBuilderFromGeodetic;
 import org.glob3.mobile.generated.G3MContext;
+import org.glob3.mobile.generated.GAsyncTask;
 import org.glob3.mobile.generated.GInitializationTask;
 import org.glob3.mobile.generated.GLPrimitive;
 import org.glob3.mobile.generated.Geodetic3D;
 import org.glob3.mobile.generated.IBufferDownloadListener;
 import org.glob3.mobile.generated.IByteBuffer;
 import org.glob3.mobile.generated.IDownloader;
-import org.glob3.mobile.generated.ILogger;
+import org.glob3.mobile.generated.IThreadUtils;
 import org.glob3.mobile.generated.JSONArray;
 import org.glob3.mobile.generated.JSONBaseObject;
 import org.glob3.mobile.generated.JSONObject;
@@ -36,24 +38,146 @@ public class PointCloudActivity
          extends
             Activity {
 
+   private final class PointsCloudParser
+            extends
+               GAsyncTask {
+
+      private final IByteBuffer _buffer;
+      private DirectMesh        _mesh;
+
+
+      private PointsCloudParser(final IByteBuffer buffer) {
+         _buffer = buffer;
+      }
+
+
+      @Override
+      public void runInBackground(final G3MContext context) {
+         final JSONBaseObject objectBase = BSONParser.parse(_buffer);
+         final JSONObject object = objectBase.asObject();
+         final JSONArray pointsJson = object.getAsArray("points");
+
+         final FloatBufferBuilderFromGeodetic vertices = FloatBufferBuilderFromGeodetic.builderWithFirstVertexAsCenter(_builder.getPlanet());
+
+         final FloatBufferBuilderFromColor colors = new FloatBufferBuilderFromColor();
+
+
+         final int size = pointsJson.size();
+         //         ILogger.instance().logInfo("Painting:" + size + " points");
+
+         double minHeight = Double.MAX_VALUE;
+         double maxHeight = Double.MIN_VALUE;
+         double totalHeight = 0;
+         for (int i = 0; i < size; i = i + 3) {
+            final double height = pointsJson.get(i + 2).asNumber().value();
+            totalHeight += height;
+            if (height < minHeight) {
+               minHeight = height;
+            }
+            if (height > maxHeight) {
+               maxHeight = height;
+            }
+         }
+         final double averageHeight = totalHeight / (size / 3.0);
+
+         final Color fromColor = Color.red();
+         final Color middleColor = Color.green();
+         final Color toColor = Color.blue();
+
+         for (int i = 0; i < size; i = i + 3) {
+            final double latDegrees = pointsJson.getAsNumber(i + 1, 0);
+            final double lonDegrees = pointsJson.getAsNumber(i, 0);
+            final double height = pointsJson.get(i + 2).asNumber().value();
+
+            vertices.add(Angle.fromDegrees(latDegrees), Angle.fromDegrees(lonDegrees), height);
+
+            final Color interpolatedColor = interpolateColor(fromColor, middleColor, toColor,
+                     normalize((float) height, (float) minHeight, (float) (averageHeight * 1.5), 1, 0));
+            colors.add(interpolatedColor);
+         }
+
+         final float lineWidth = 1;
+         final float pointSize = 4F;
+         final Color flatColor = null;
+         _mesh = new DirectMesh( //
+                  GLPrimitive.points(), //
+                  true, //
+                  vertices.getCenter(), //
+                  vertices.create(), //
+                  lineWidth, //
+                  pointSize, //
+                  flatColor, //
+                  colors.create(), //
+                  1, //
+                  false);
+      }
+
+
+      @Override
+      public void onPostExecute(final G3MContext context) {
+         if (_mesh != null) {
+            setPointsCloudMesh(_mesh);
+         }
+      }
+   }
+
+   private final class PointsCloudDownloader
+            extends
+               IBufferDownloadListener {
+
+      private final IThreadUtils _threadUtils;
+
+
+      public PointsCloudDownloader(final IThreadUtils threadUtils) {
+         _threadUtils = threadUtils;
+      }
+
+
+      @Override
+      public void onDownload(final URL url,
+                             final IByteBuffer buffer,
+                             final boolean expired) {
+
+         _threadUtils.invokeAsyncTask(new PointsCloudParser(buffer), true);
+      }
+
+
+      @Override
+      public void onError(final URL url) {
+      }
+
+
+      @Override
+      public void onCancel(final URL url) {
+      }
+
+
+      @Override
+      public void onCanceledDownload(final URL url,
+                                     final IByteBuffer buffer,
+                                     final boolean expired) {
+      }
+   }
+
+
    private G3MWidget_Android  _g3mWidget;
    private RelativeLayout     _placeHolder;
    private G3MBuilder_Android _builder;
-   final MeshRenderer         _meshRenderer = new MeshRenderer();
-   boolean                    _isDone       = false;
+   private final MeshRenderer _meshRenderer        = new MeshRenderer();
+   private boolean            _isPointsCloudLoader = false;
+
+
+   private void setPointsCloudMesh(final DirectMesh mesh) {
+      _meshRenderer.addMesh(mesh);
+      _isPointsCloudLoader = true;
+   }
 
 
    @Override
    protected void onCreate(final Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_point_cloud);
 
-      /*
-      39.1353919693813
-      -77.6089543461732
-      39.1215022335016
-      -77.591121367969
-      */
+      setContentView(R.layout.activity_point_cloud);
 
       final LayerSet layerSet = SimpleRasterLayerBuilder.createLayerset();
       layerSet.disableAllLayers();
@@ -64,7 +188,6 @@ public class PointCloudActivity
       _builder.setPlanet(Planet.createEarth());
       _builder.getPlanetRendererBuilder().setLayerSet(layerSet);
 
-
       _builder.setInitializationTask(pointCloudInitializationTask());
       _builder.addRenderer(_meshRenderer);
 
@@ -73,10 +196,8 @@ public class PointCloudActivity
       _g3mWidget.setAnimatedCameraPosition(Geodetic3D.fromDegrees(39.12787093899339d, -77.59965772558118d, 2000),
                TimeInterval.fromSeconds(5));
 
-
       _placeHolder = (RelativeLayout) findViewById(R.id.g3mWidgetHolder);
       _placeHolder.addView(_g3mWidget);
-
    }
 
 
@@ -84,118 +205,26 @@ public class PointCloudActivity
 
 
       final GInitializationTask initTask = new GInitializationTask() {
-
          @Override
          public void run(final G3MContext context) {
 
             final IDownloader downloader = context.getDownloader();
 
-            final IBufferDownloadListener listener = new IBufferDownloadListener() {
+            final IBufferDownloadListener listener = new PointsCloudDownloader(context.getThreadUtils());
 
-               private JSONArray _pointsJson;
-
-
-               @Override
-               public void onDownload(final URL url,
-                                      final IByteBuffer buffer,
-                                      final boolean expired) {
-
-                  ILogger.instance().logInfo("Before parsing");
-
-                  final JSONBaseObject objectBase = BSONParser.parse(buffer);
-                  final JSONObject object = objectBase.asObject();
-                  _pointsJson = object.getAsArray("points");
-
-                  final FloatBufferBuilderFromGeodetic vertices = FloatBufferBuilderFromGeodetic.builderWithFirstVertexAsCenter(_builder.getPlanet());
-
-                  final FloatBufferBuilderFromColor colors = new FloatBufferBuilderFromColor();
-
-
-                  final int size = _pointsJson.size();
-                  ILogger.instance().logInfo("Painting:" + size + " points");
-
-
-                  //                  ILogger.instance().logInfo("Painting:" + _pointsJson.size() + " points");
-
-                  double minHeight = Double.MAX_VALUE;
-                  double maxHeight = Double.MIN_VALUE;
-                  double totalHeight = 0;
-                  for (int i = 0; i < size; i = i + 3) {
-                     final double height = _pointsJson.get(i + 2).asNumber().value();
-                     totalHeight += height;
-                     if (height < minHeight) {
-                        minHeight = height;
-                     }
-                     if (height > maxHeight) {
-                        maxHeight = height;
-                     }
-                  }
-                  final double averageHeight = totalHeight / (size / 3.0);
-
-                  for (int i = 0; i < size; i = i + 3) {
-                     final double latDegrees = _pointsJson.getAsNumber(i + 1, 0);
-                     final double lotDegrees = _pointsJson.getAsNumber(i, 0);
-                     final double height = _pointsJson.get(i + 2).asNumber().value();
-
-                     final Geodetic3D position = Geodetic3D.fromDegrees(latDegrees, lotDegrees, height - 180);
-
-                     //ILogger.instance().logInfo("Point::" + position.description());
-
-                     vertices.add(position);
-
-
-                     //        final Color interpolatedColor = fromColor.mixedWith(toColor, normalize((float) height, 744, 775, 1, 0));
-
-                     final Color interpolatedColor = interpolateColor(Color.red(), Color.green(), Color.blue(),
-                              normalize((float) height, (float) minHeight, (float) (averageHeight * 1.5), 1, 0));
-
-                     colors.add(interpolatedColor);
-
-
-                  }
-
-                  final float lineWidth = 1;
-                  final Color flatColor = null;
-
-                  _meshRenderer.addMesh(new DirectMesh(GLPrimitive.points(), true, vertices.getCenter(), vertices.create(),
-                           lineWidth, 4F, flatColor, colors.create(), 1, false));
-                  _isDone = true;
-               }
-
-
-               @Override
-               public void onError(final URL url) {
-               }
-
-
-               @Override
-               public void onCancel(final URL url) {
-               }
-
-
-               @Override
-               public void onCanceledDownload(final URL url,
-                                              final IByteBuffer buffer,
-                                              final boolean expired) {
-               }
-
-            };
-
-            downloader.requestBuffer( //-
+            downloader.requestBuffer( //
                      new URL("file:///18STJ7435_2000_4326.bson", false), //
                      0, //
                      TimeInterval.forever(), //
                      true, //
                      listener, //
                      false);
-
          }
 
 
          @Override
          public boolean isDone(final G3MContext context) {
-
-            return _isDone;
+            return _isPointsCloudLoader;
          }
       };
 
@@ -229,95 +258,6 @@ public class PointCloudActivity
       return middle.mixedWith(to, (d - 0.5f) * 2);
    }
 
-
-   /* 
-    private void loadPointsDownloader(final Planet planet1,
-                                      final G3MContext context) {
-       final IDownloader downloader = context.getDownloader();
-
-       final IBufferDownloadListener listener = new IBufferDownloadListener() {
-
-
-          @Override
-          public void onDownload(final URL url,
-                                 final IByteBuffer buffer,
-                                 final boolean expired) {
-
-
-             ILogger.instance().logInfo("Before parsing");
-
-             final JSONBaseObject objectBase = BSONParser.parse(buffer);
-             final JSONObject object = objectBase.asObject();
-             _pointsJson = object.getAsArray("points");
-
-             final FloatBufferBuilderFromGeodetic vertices = FloatBufferBuilderFromGeodetic.builderWithFirstVertexAsCenter(planet1);
-
-             final FloatBufferBuilderFromColor colors = new FloatBufferBuilderFromColor();
-
-
-             ILogger.instance().logInfo("Painting:" + _pointsJson.size() + " points");
-
-             for (int i = 0; i < _pointsJson.size(); i = i + 3) {
-
-                final double height = _pointsJson.get(i + 2).asNumber().value();
-                vertices.add(new Geodetic3D(Angle.fromDegrees(_pointsJson.getAsNumber(i + 1, 0)),
-                         Angle.fromDegrees(_pointsJson.getAsNumber(i, 0)), ((height + DELTA_HEIGHT) * _VerticalExaggeration)));
-
-
-                //        final Color interpolatedColor = fromColor.mixedWith(toColor, normalize((float) height, 744, 775, 1, 0));
-
-                final Color interpolatedColor = interpolateColor(Color.red(), Color.green(), Color.blue(),
-                         normalize((float) height, 744, 775, 1, 0));
-
-                colors.add(interpolatedColor);
-
-
-             }
-
-             final float lineWidth = 1;
-             final Color flatColor = null;
-
-             _meshRenderer.addMesh(new DirectMesh(GLPrimitive.points(), true, vertices.getCenter(), vertices.create(),
-                      lineWidth, 4F, flatColor, colors.create(), 1, false));
-
-
-          }
-
-
-          @Override
-          public void onError(final URL url) {
-             // TODO Auto-generated method stub
-
-          }
-
-
-          @Override
-          public void onCancel(final URL url) {
-             // TODO Auto-generated method stub
-
-          }
-
-
-          @Override
-          public void onCanceledDownload(final URL url,
-                                         final IByteBuffer buffer,
-                                         final boolean expired) {
-             // TODO Auto-generated method stub
-
-          }
-
-       };
-
-
-       downloader.requestBuffer( //-
-                new URL("file:///338000_8092000_1K_class4326.bson", false), //
-                0, //
-                TimeInterval.forever(), //
-                true, //
-                listener, //
-                false);
-    }
-   */
 
    @Override
    public boolean onCreateOptionsMenu(final Menu menu) {
