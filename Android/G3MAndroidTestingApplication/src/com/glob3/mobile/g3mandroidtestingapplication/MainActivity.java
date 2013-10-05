f647be24c3e53d12112e7a63d34347c65c352700
Manuel <mdelacalle@gmail.com>
2013-10-06 01:55:12 +0200

    testing app

---
+++ Android/G3MAndroidTestingApplication/src/com/glob3/mobile/g3mandroidtestingapplication/MainActivity.java
@@ -0,0 +1,40 @@
+
+
+package com.glob3.mobile.g3mandroidtestingapplication;
+
+import org.glob3.mobile.specific.G3MBuilder_Android;
+import org.glob3.mobile.specific.G3MWidget_Android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity
+         extends
+            Activity {
+
+   private G3MWidget_Android _g3mWidget;
+   private RelativeLayout    _placeHolder;
+
+
+   @Override
+   protected void onCreate(final Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      final G3MBuilder_Android builder = new G3MBuilder_Android(this);
+      _g3mWidget = builder.createWidget();
+      _placeHolder = (RelativeLayout) findViewById(R.id.g3mWidgetHolder);
+      _placeHolder.addView(_g3mWidget);
+   }
+
+
+   @Override
+   public boolean onCreateOptionsMenu(final Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+   }
+
+}
