5f64de39cf787f22d6cf398c6833472325acd2c2
Manuel <mdelacalle@gmail.com>
2013-10-05 17:54:56 +0200

    conversion tools package created

--- Android/G3MAndroidDemo/src/org/glob3/mobile/demo/ScenarioTerrainActivity.java
+++ Android/G3MAndroidDemo/src/org/glob3/mobile/demo/ScenarioTerrainActivity.java
@@ -5,9 +5,8 @@ package org.glob3.mobile.demo;
 import org.glob3.mobile.generated.Angle;
 import org.glob3.mobile.generated.ElevationDataProvider;
 import org.glob3.mobile.generated.Geodetic2D;
-import org.glob3.mobile.generated.Geodetic3D;
 import org.glob3.mobile.generated.LayerSet;
-import org.glob3.mobile.generated.Planet;
+import org.glob3.mobile.generated.MapBoxLayer;
 import org.glob3.mobile.generated.Sector;
 import org.glob3.mobile.generated.SingleBillElevationDataProvider;
 import org.glob3.mobile.generated.TimeInterval;
@@ -40,24 +39,26 @@ public class ScenarioTerrainActivity
       // final double DELTA_HEIGHT = 0;
 
 
-      final LayerSet layerset = SimpleRasterLayerBuilder.createLayerset();
-      layerset.disableAllLayers();
-      layerset.getLayerByTitle("Map Box Terrain").setEnable(true);
+      final LayerSet layerSet = new LayerSet();
+
+      //bla
+      final MapBoxLayer mboxTerrainLayer = new MapBoxLayer("examples.map-qogxobv1", TimeInterval.fromDays(30), true, 2);
+      layerSet.addLayer(mboxTerrainLayer);
+
 
       final G3MBuilder_Android builder = new G3MBuilder_Android(this);
-      builder.setPlanet(Planet.createSphericalEarth());
-      builder.getPlanetRendererBuilder().setLayerSet(layerset);
+      //  builder.setPlanet(Planet.createSphericalEarth());
+      builder.getPlanetRendererBuilder().setLayerSet(layerSet);
 
 
       final Geodetic2D lower = new Geodetic2D( //
-               Angle.fromDegrees(40.1540143280790858), //
-               Angle.fromDegrees(-5.8664874640814313));
+               Angle.fromDegrees(40.1665739916489), //
+               Angle.fromDegrees(-5.85449532145337));
       final Geodetic2D upper = new Geodetic2D( //
-               Angle.fromDegrees(40.3423148480663158), //
+               Angle.fromDegrees(40.3320215899527), //
                Angle.fromDegrees(-5.5116079822178570));
 
       final Sector demSector = new Sector(lower, upper);
-
 
       // NROWS          1335
       // NCOLS          2516
@@ -67,11 +68,11 @@ public class ScenarioTerrainActivity
       builder.getPlanetRendererBuilder().setElevationDataProvider(dem);
       builder.getPlanetRendererBuilder().setVerticalExaggeration(_VerticalExaggeration);
 
-      builder.setShownSector(demSector);
+      builder.setShownSector(demSector.shrinkedByPercent(0.1f));
 
 
       _g3mWidget = builder.createWidget();
-      _g3mWidget.setAnimatedCameraPosition(new Geodetic3D(demSector.getCenter(), 10000), TimeInterval.fromSeconds(5));
+
       _placeHolder = (RelativeLayout) findViewById(R.id.g3mWidgetHolder);
       _placeHolder.addView(_g3mWidget);
 
