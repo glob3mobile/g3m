f73b8b85c78b124118ae4516ed88942f661e7995
Jose Miguel SN <josemiguelsn@MacBook-Pro-de-Jose-2.local>
2013-10-15 16:08:14 +0100

    ready for merge

--- Android/G3MAndroidTestingApplication/src/com/glob3/mobile/g3mandroidtestingapplication/MainActivity.java
+++ Android/G3MAndroidTestingApplication/src/com/glob3/mobile/g3mandroidtestingapplication/MainActivity.java
@@ -33,23 +33,6 @@ public class MainActivity
       setContentView(R.layout.activity_main);
       final G3MBuilder_Android builder = new G3MBuilder_Android(this);
       builder.getPlanetRendererBuilder().setRenderDebug(true);
-      
-      if (true){ //testing small shown sector
-
-          final Geodetic2D lower = new Geodetic2D( //
-                   Angle.fromDegrees(40.1665739916489), //
-                   Angle.fromDegrees(-5.85449532145337));
-          final Geodetic2D upper = new Geodetic2D( //
-                   Angle.fromDegrees(40.3320215899527), //
-                   Angle.fromDegrees(-1.5116079822178570));
-
-          final Sector demSector = new Sector(lower, upper);
-
-          //The sector is shrinked to adjust the projection of
-          builder.setShownSector(demSector.shrinkedByPercent(0.1f));
-      }
-      
-      
       _g3mWidget = builder.createWidget();
       _placeHolder = (RelativeLayout) findViewById(R.id.g3mWidgetHolder);
       _placeHolder.addView(_g3mWidget);
