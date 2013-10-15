626ea6490f10976a67a3e3aaf93cf74c3e94f78b
Jose Miguel SN <josemiguelsn@MacBook-Pro-de-Jose-2.local>
2013-10-15 13:44:14 +0100

    Using main activity to test results on android

--- Android/G3MAndroidTestingApplication/src/com/glob3/mobile/g3mandroidtestingapplication/MainActivity.java
+++ Android/G3MAndroidTestingApplication/src/com/glob3/mobile/g3mandroidtestingapplication/MainActivity.java
@@ -2,6 +2,13 @@
 
 package com.glob3.mobile.g3mandroidtestingapplication;
 
+import org.glob3.mobile.generated.Angle;
+import org.glob3.mobile.generated.ElevationDataProvider;
+import org.glob3.mobile.generated.Geodetic2D;
+import org.glob3.mobile.generated.Sector;
+import org.glob3.mobile.generated.SingleBillElevationDataProvider;
+import org.glob3.mobile.generated.URL;
+import org.glob3.mobile.generated.Vector2I;
 import org.glob3.mobile.specific.G3MBuilder_Android;
 import org.glob3.mobile.specific.G3MWidget_Android;
 
@@ -26,6 +33,23 @@ public class MainActivity
       setContentView(R.layout.activity_main);
       final G3MBuilder_Android builder = new G3MBuilder_Android(this);
       builder.getPlanetRendererBuilder().setRenderDebug(true);
+      
+      if (true){ //testing small shown sector
+
+          final Geodetic2D lower = new Geodetic2D( //
+                   Angle.fromDegrees(40.1665739916489), //
+                   Angle.fromDegrees(-5.85449532145337));
+          final Geodetic2D upper = new Geodetic2D( //
+                   Angle.fromDegrees(40.3320215899527), //
+                   Angle.fromDegrees(-5.5116079822178570));
+
+          final Sector demSector = new Sector(lower, upper);
+
+          //The sector is shrinked to adjust the projection of
+          builder.setShownSector(demSector.shrinkedByPercent(0.1f));
+      }
+      
+      
       _g3mWidget = builder.createWidget();
       _placeHolder = (RelativeLayout) findViewById(R.id.g3mWidgetHolder);
       _placeHolder.addView(_g3mWidget);
