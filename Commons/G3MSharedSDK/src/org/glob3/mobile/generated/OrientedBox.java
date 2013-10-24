eca1e425c7027f5b50f796f05d899c80d2459f0f
aldeanueva <aldeanueva@igosoftware.es>
2013-10-24 20:05:04 +0200

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/OrientedBox.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/OrientedBox.java
@@ -144,13 +144,13 @@ public class OrientedBox extends BoundingVolume
     if (_cornersD == null) {
       _cornersD = new java.util.ArrayList<Vector3D>(8);
       _cornersD.add(new Vector3D(_halfExtentX,  _halfExtentY,   _halfExtentZ).transformedBy(_transformMatrix, 1));
-      _cornersD.add(new Vector3D(-_halfExtentX, _halfExtentY,   _halfExtentZ).transformedBy(*_transformMatrix, 1));
-      _cornersD.add(new Vector3D(_halfExtentX,  -_halfExtentY,  _halfExtentZ).transformedBy(*_transformMatrix, 1));
-      _cornersD.add(new Vector3D(-_halfExtentX, -_halfExtentY,  _halfExtentZ).transformedBy(*_transformMatrix, 1));
-      _cornersD.add(new Vector3D(_halfExtentX,  _halfExtentY,   -_halfExtentZ).transformedBy(*_transformMatrix, 1));
-      _cornersD.add(new Vector3D(-_halfExtentX, _halfExtentY,   -_halfExtentZ).transformedBy(*_transformMatrix, 1));
-      _cornersD.add(new Vector3D(_halfExtentX,  -_halfExtentY,  -_halfExtentZ).transformedBy(*_transformMatrix, 1));
-      _cornersD.add(new Vector3D(-_halfExtentX, -_halfExtentY,  -_halfExtentZ).transformedBy(*_transformMatrix, 1));
+      _cornersD.add(new Vector3D(-_halfExtentX, _halfExtentY,   _halfExtentZ).transformedBy(_transformMatrix, 1));
+      _cornersD.add(new Vector3D(_halfExtentX,  -_halfExtentY,  _halfExtentZ).transformedBy(_transformMatrix, 1));
+      _cornersD.add(new Vector3D(-_halfExtentX, -_halfExtentY,  _halfExtentZ).transformedBy(_transformMatrix, 1));
+      _cornersD.add(new Vector3D(_halfExtentX,  _halfExtentY,   -_halfExtentZ).transformedBy(_transformMatrix, 1));
+      _cornersD.add(new Vector3D(-_halfExtentX, _halfExtentY,   -_halfExtentZ).transformedBy(_transformMatrix, 1));
+      _cornersD.add(new Vector3D(_halfExtentX,  -_halfExtentY,  -_halfExtentZ).transformedBy(_transformMatrix, 1));
+      _cornersD.add(new Vector3D(-_halfExtentX, -_halfExtentY,  -_halfExtentZ).transformedBy(_transformMatrix, 1));
     }
     return _cornersD;
   
