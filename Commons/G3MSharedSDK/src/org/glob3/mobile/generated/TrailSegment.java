40017c59a1c28cc4d6a14688197d218c8977d4e7
aldeanueva <aldeanueva@igosoftware.es>
2013-10-26 21:37:59 +0200

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/TrailSegment.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/TrailSegment.java
@@ -104,23 +104,21 @@ public class TrailSegment
   
     Mesh surfaceMesh = new DirectMesh(GLPrimitive.triangleStrip(), true, vertices.getCenter(), vertices.create(), 1, 1, new Color(_color), null, 0.0f, false); // depthTest -  colorsIntensity -  colors
   
-    // Debug unions
+    return surfaceMesh;
+  
+  //  // Debug unions
   //  Mesh* edgesMesh = new DirectMesh(GLPrimitive::lines(),
   //                                   false,
-  //                                   center,
-  //                                   vertices,
+  //                                   vertices.getCenter(),
+  //                                   vertices.create(),
   //                                   2,
   //                                   1,
   //                                   Color::newFromRGBA(1, 1, 1, 0.7f));
-  //
   //  CompositeMesh* cm = new CompositeMesh();
-  //
   //  cm->addMesh(surfaceMesh);
   //  cm->addMesh(edgesMesh);
-  //
   //  return cm;
   
-    return surfaceMesh;
   }
 
   private Mesh _mesh;
