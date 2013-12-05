81bf4af71002849e4712f6a9a2b10731f84e98b0
aldeanueva <aldeanueva@igosoftware.es>
2013-12-05 19:19:48 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/GEOTileRasterizer_QuadTreeVisitor.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/GEOTileRasterizer_QuadTreeVisitor.java
@@ -31,11 +31,7 @@ package org.glob3.mobile.generated;
 public class GEOTileRasterizer_QuadTreeVisitor extends QuadTreeVisitor
 {
   private final GEOTileRasterizer _geoTileRasterizer;
-<<<<<<< HEAD
-  private final IImage _originalImage;
-=======
   private IImage _originalImage;
->>>>>>> purgatory
   private final TileRasterizerContext _trc;
   private IImageListener _listener;
   private boolean _autodelete;
@@ -96,12 +92,7 @@ public class GEOTileRasterizer_QuadTreeVisitor extends QuadTreeVisitor
     {
       _canvas.createImage(_listener, _autodelete);
 
-<<<<<<< HEAD
-      if (_originalImage != null)
-         _originalImage.dispose();
-=======
       _originalImage = null;
->>>>>>> purgatory
       _originalImage = null;
 
       if (_projection != null)
