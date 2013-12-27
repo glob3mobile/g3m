dec24da469b091da6a69e5641826d9f8a2520808
Diego Gomez Deck <dgd@consultar.com>
2013-12-27 10:57:11 -0500

    -

--- iOS/G3MiOSSDK/Commons/Rendererers/HUDRelativeSize.cpp
+++ iOS/G3MiOSSDK/Commons/Rendererers/HUDRelativeSize.cpp
@@ -15,22 +15,29 @@ float HUDRelativeSize::getSize(int viewPortWidth,
   switch (_relativeTo) {
     case VIEWPORT_WIDTH:
       return _factor * viewPortWidth;
+
     case VIEWPORT_HEIGTH:
       return _factor * viewPortHeight;
+
     case VIEWPORT_MIN_AXIS:
       return _factor * ((viewPortWidth < viewPortHeight) ? viewPortWidth : viewPortHeight);
+
     case VIEWPORT_MAX_AXIS:
       return _factor * ((viewPortWidth > viewPortHeight) ? viewPortWidth : viewPortHeight);
 
+
     case BITMAP_WIDTH:
       return _factor * bitmapWidth;
+
     case BITMAP_HEIGTH:
       return _factor * bitmapHeight;
+
     case BITMAP_MIN_AXIS:
       return _factor * ((bitmapWidth < bitmapHeight) ? bitmapWidth : bitmapHeight);
+
     case BITMAP_MAX_AXIS:
       return _factor * ((bitmapWidth > bitmapHeight) ? bitmapWidth : bitmapHeight);
   }
-
+  
   return 0;
 }
