9d0fe5642d03e5b6354d24f010a0f58bef98c002
aldeanueva <aldeanueva@igosoftware.es>
2013-12-31 15:34:09 +0100

    Generated

---
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Future.java
@@ -0,0 +1,27 @@
+package org.glob3.mobile.generated; 
+//
+//  Future.cpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 12/31/13.
+//
+//
+
+//
+//  Future.hpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 12/31/13.
+//
+//
+
+
+//C++ TO JAVA CONVERTER TODO TASK: The original C++ template specifier was replaced with a Java generic specifier, which may not produce the same behavior:
+public interface Future<T>
+{
+  private T _value = new T();
+
+  T get();
+
+
+}
