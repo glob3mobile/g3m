27c166a095e71f54f403c2867a8219147e802805
aldeanueva <aldeanueva@igosoftware.es>
2013-12-31 16:07:01 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Future.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Future.java
@@ -18,25 +18,61 @@ package org.glob3.mobile.generated;
 
 
 //C++ TO JAVA CONVERTER TODO TASK: The original C++ template specifier was replaced with a Java generic specifier, which may not produce the same behavior:
-public abstract class Future<T>
+public class Future<T>
 {
   private T _value;
+  private boolean _isDone;
+
+  private boolean _isDoneWithError;
+  private String _error;
+
+//C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
+//  Future(Future<T> that);
 
   protected Future()
   {
      _value = null;
+     _isDone = false;
+     _isDoneWithError = false;
   }
 
-  protected final void set(T value)
+  protected final void done(T value)
   {
     _value = value;
+    _isDone = true;
+    _isDoneWithError = false;
   }
 
-  public abstract T get();
+  protected final void error(String error)
+  {
+    _error = error;
+    _isDone = true;
+    _isDoneWithError = true;
+  }
 
   public void dispose()
   {
     _value = null;
+  }
+
+  public final T get()
+  {
+    return _value;
+  }
+
+  public final boolean isDone()
+  {
+    return _isDone;
+  }
+
+  public final boolean isDoneWithError()
+  {
+    return _isDoneWithError;
+  }
+
+  public final String getError()
+  {
+    return _error;
   }
 
 }
---
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/FutureImage.java
@@ -0,0 +1,6 @@
+package org.glob3.mobile.generated; 
+//class IImage;
+public class FutureImage extends Future<IImage>
+{
+
+}
