4c4b1af4edd096cee2999ce62536503cd57b5663
amazingsmash <josemiguelsn@MacBook-Pro-de-Jose-2.local>
2014-01-15 09:58:02 +0000

    ...

--- iOS/G3MiOSSDK/Commons/Mesh/TexturedMesh.cpp
+++ iOS/G3MiOSSDK/Commons/Mesh/TexturedMesh.cpp
@@ -9,13 +9,11 @@
 #include "TexturedMesh.hpp"
 
 void TexturedMesh::createGLState() {
-//  _textureMapping->modifyGLState(*_glState);
-#warning Diego at work!
 }
 
 void TexturedMesh::rawRender(const G3MRenderContext* rc,
                              const GLState* parentState) const{
-#warning Diego at work!
+#warning To Diego: As a textureMapping could be used by more than 1 TexturedMesh (I think) it's necessary to check glState consistency at every frame. Otherwise you should store somehow a list of every user of the mapping in order to change their states when any parameter of the mapping is updated. Method modifyGLState() is now much lighter though.
   _textureMapping->modifyGLState(*_glState);
 
   _glState->setParent(parentState);
