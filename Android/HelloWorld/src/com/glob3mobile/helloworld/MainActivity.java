22924e45639136fcc1ac82b78d83933b7fb7b052
Manuel <mdelacalle@gmail.com>
2014-01-22 10:56:37 +0100

    logos

--- Android/HelloWorld/src/com/glob3mobile/helloworld/MainActivity.java
+++ Android/HelloWorld/src/com/glob3mobile/helloworld/MainActivity.java
@@ -1,34 +1,40 @@
+
+
 package com.glob3mobile.helloworld;
 
 import org.glob3.mobile.specific.G3MBuilder_Android;
 import org.glob3.mobile.specific.G3MWidget_Android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.widget.LinearLayout;
 
-public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		final G3MBuilder_Android builder = new G3MBuilder_Android(this);
-		
-		G3MWidget_Android g3mWidget = builder.createWidget();
+public class MainActivity
+         extends
+            Activity {
 
-		 final LinearLayout layout = (LinearLayout) findViewById(R.id.glob3);
-	     layout.addView(g3mWidget);
-		
-	}
+   @Override
+   protected void onCreate(final Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+      final G3MBuilder_Android builder = new G3MBuilder_Android(this);
+
+      final G3MWidget_Android g3mWidget = builder.createWidget();
+
+      final LinearLayout layout = (LinearLayout) findViewById(R.id.glob3);
+      layout.addView(g3mWidget);
+
+   }
+
+
+   @Override
+   public boolean onCreateOptionsMenu(final Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+   }
 
 }
