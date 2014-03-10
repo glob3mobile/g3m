bada9d35720e4aedfe11d8be9389def21a185aa5
Diego Gomez Deck <dgd@consultar.com>
2014-03-10 12:16:15 -0400

    Renamed GEOLineRasterSymbol and GEOPolygonRasterSymbol

--- WebGL/G3MWebGLDemo/src/org/glob3/mobile/client/G3MWebGLDemo.java
+++ WebGL/G3MWebGLDemo/src/org/glob3/mobile/client/G3MWebGLDemo.java
@@ -32,9 +32,9 @@ import org.glob3.mobile.generated.GEO2DPolygonData;
 import org.glob3.mobile.generated.GEO2DPolygonGeometry;
 import org.glob3.mobile.generated.GEO2DSurfaceRasterStyle;
 import org.glob3.mobile.generated.GEOGeometry;
+import org.glob3.mobile.generated.GEOLineRasterSymbol;
 import org.glob3.mobile.generated.GEOMultiLineRasterSymbol;
-import org.glob3.mobile.generated.GEORasterLineSymbol;
-import org.glob3.mobile.generated.GEORasterPolygonSymbol;
+import org.glob3.mobile.generated.GEOPolygonRasterSymbol;
 import org.glob3.mobile.generated.GEORenderer;
 import org.glob3.mobile.generated.GEOSymbol;
 import org.glob3.mobile.generated.GEOSymbolizer;
@@ -293,7 +293,7 @@ public class G3MWebGLDemo
          @Override
          public ArrayList<GEOSymbol> createSymbols(final GEO2DLineStringGeometry geometry) {
             final ArrayList<GEOSymbol> symbols = new ArrayList<GEOSymbol>();
-            symbols.add(new GEORasterLineSymbol(geometry.getCoordinates(), createLineRasterStyle(geometry)));
+            symbols.add(new GEOLineRasterSymbol(geometry.getCoordinates(), createLineRasterStyle(geometry)));
             return symbols;
          }
 
@@ -335,7 +335,7 @@ public class G3MWebGLDemo
          public ArrayList<GEOSymbol> createSymbols(final GEO2DPolygonGeometry geometry) {
             final ArrayList<GEOSymbol> symbols = new ArrayList<GEOSymbol>(0);
 
-            symbols.add(new GEORasterPolygonSymbol(geometry.getPolygonData(), createPolygonLineRasterStyle(geometry),
+            symbols.add(new GEOPolygonRasterSymbol(geometry.getPolygonData(), createPolygonLineRasterStyle(geometry),
                      createPolygonSurfaceRasterStyle(geometry)));
 
             return symbols;
@@ -354,8 +354,7 @@ public class G3MWebGLDemo
 
             for (int i = 0; i < polygonsDataSize; i++) {
                final GEO2DPolygonData polygonData = polygonsData.get(i);
-               symbols.add(new GEORasterPolygonSymbol(polygonData, lineStyle, surfaceStyle));
-
+               symbols.add(new GEOPolygonRasterSymbol(polygonData, lineStyle, surfaceStyle));
             }
 
             return symbols;
