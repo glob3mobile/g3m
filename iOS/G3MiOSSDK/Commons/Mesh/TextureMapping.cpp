2ea16b282b78633398b63059246f00c5c5f85fb8
Diego Gomez Deck <dgd@consultar.com>
2014-04-20 14:02:05 -0400

    Tile's image creation refactoring - NOT YET USABLE

---
+++ iOS/G3MiOSSDK/Commons/Mesh/TextureMapping.cpp
@@ -0,0 +1,9 @@
+//
+//  TextureMapping.cpp
+//  G3MiOSSDK
+//
+//  Created by José Miguel S N on 12/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include "TextureMapping.hpp"
