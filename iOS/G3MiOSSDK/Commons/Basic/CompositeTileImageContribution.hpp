8e441b6c097c16a62546ac9bc38edc2d20061e24
Diego Gomez Deck <dgd@consultar.com>
2014-04-28 16:46:58 -0400

    Tile's image creation refactoring - NOT YET USABLE

--- iOS/G3MiOSSDK/Commons/Basic/CompositeTileImageContribution.hpp
+++ iOS/G3MiOSSDK/Commons/Basic/CompositeTileImageContribution.hpp
@@ -51,11 +51,12 @@ public:
     for (int i = 0; i < size; i++) {
 #ifdef C_CODE
       const ChildContribution* contribution = _contributions[i];
+      delete contribution;
 #endif
 #ifdef JAVA_CODE
       final ChildContribution contribution = _contributions.get(i);
+      contribution.dispose();
 #endif
-      delete contribution;
     }
   }
 
