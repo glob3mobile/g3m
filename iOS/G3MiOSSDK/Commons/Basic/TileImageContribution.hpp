e8a4495b816005f60008aa68d1c6daaca6ee82a2
Diego Gomez Deck <dgd@consultar.com>
2014-05-06 11:19:32 -0400

    Tile's image creation refactoring - NOT YET USABLE

--- iOS/G3MiOSSDK/Commons/Basic/TileImageContribution.hpp
+++ iOS/G3MiOSSDK/Commons/Basic/TileImageContribution.hpp
@@ -54,7 +54,7 @@ protected:
   }
 
   virtual ~TileImageContribution() {
-    printf("******* deleting contribution %p\n", this);
+//    printf("******* deleting contribution %p\n", this);
 #ifdef JAVA_CODE
     super.dispose();
 #endif
