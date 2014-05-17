1caeb78b5dc63d20b75ea4570af7a2c60f41bfe0
Diego Gomez Deck <dgd@consultar.com>
2014-05-17 19:09:42 -0400

    -

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/RCObject.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/RCObject.java
@@ -1,4 +1,11 @@
-package org.glob3.mobile.generated; 
+
+
+package org.glob3.mobile.generated;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 //
 //  RCObject.cpp
 //  G3MiOSSDK
@@ -16,46 +23,112 @@ package org.glob3.mobile.generated;
 //
 
 
-
-public class RCObject
-{
-  private int _referenceCounter;
-
-  private void _suicide()
-  {
-    this.dispose();
-  }
-
-//C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
-//  RCObject(RCObject that);
-//C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
-//  RCObject operator =(RCObject that);
-
-  protected RCObject() // the object starts retained
-  {
-     _referenceCounter = 1;
-  }
-
-  public void dispose()
-  {
-    if (_referenceCounter != 0)
-    {
-      ILogger.instance().logError("DELETING RCOBJECT WITH UNRELEASED REFERENCES!");
-    }
-  }
+public class RCObject {
+   private int                             _referenceCounter;
+   private boolean                         _suicided          = false;
+   private boolean                         _disposed          = false;
 
 
-  public final void _retain()
-  {
-    _referenceCounter++;
-  }
+   private final StackTraceElement[]       _instanciationCallStack;
+   private final List<StackTraceElement[]> _retainCallstacks  = new ArrayList<StackTraceElement[]>();
+   private final List<StackTraceElement[]> _releaseCallstacks = new ArrayList<StackTraceElement[]>();
 
-  public final void _release()
-  {
-    if (--_referenceCounter == 0)
-    {
-      _suicide();
-    }
-  }
+
+   private void _suicide() {
+      _suicided = true;
+      this.dispose();
+   }
+
+
+   //C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
+   //  RCObject(RCObject that);
+
+   //C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
+   //  RCObject operator =(RCObject that);
+
+
+   public void dispose() {
+      _disposed = true;
+      if (_referenceCounter != 0) {
+         ILogger.instance().logError("DELETING RCOBJECT WITH UNRELEASED REFERENCES!");
+      }
+   }
+
+
+   protected RCObject() // the object starts retained
+   {
+      _referenceCounter = 1;
+      if (this instanceof TileImageContribution) {
+         _instanciationCallStack = new Throwable().getStackTrace();
+      }
+      else {
+         _instanciationCallStack = null;
+      }
+   }
+
+
+   public final void _retain() {
+      _referenceCounter++;
+      if (this instanceof TileImageContribution) {
+         _retainCallstacks.add(new Throwable().getStackTrace());
+      }
+   }
+
+
+   public final void _release() {
+      if (this instanceof TileImageContribution) {
+         _releaseCallstacks.add(new Throwable().getStackTrace());
+      }
+
+      if (--_referenceCounter == 0) {
+         _suicide();
+      }
+      if (_referenceCounter < 0) {
+         showLogicError("Invalid _release()");
+      }
+   }
+
+
+   @Override
+   protected void finalize() throws Throwable {
+      if (_referenceCounter != 0) {
+         showLogicError("Finalized with invalid _referenceCounter");
+      }
+      super.finalize();
+   }
+
+
+   private void showLogicError(final String string) {
+      if (this instanceof TileImageContribution) {
+         final ILogger logger = ILogger.instance();
+
+         //      logger.logError("RCObject: \"" + this + "\" " + string + ", _referenceCounter=" + _referenceCounter + ", _suicided="
+         //                      + _suicided + ", _disposed=" + _disposed);
+
+         logger.logError("=================================================");
+         logger.logError("RCObject: \"" + this + "\" " + string + ", _referenceCounter=" + _referenceCounter + ", _suicided="
+                         + _suicided + ", _disposed=" + _disposed);
+         logger.logError("Instanciated at: ");
+         for (final StackTraceElement e : _instanciationCallStack) {
+            logger.logError("  " + e.toString());
+         }
+         logger.logError("Retained at: ");
+         for (final StackTraceElement[] each : _retainCallstacks) {
+            for (final StackTraceElement e : each) {
+               logger.logError("  " + e.toString());
+            }
+            logger.logError("---");
+         }
+         logger.logError("Released at: ");
+         for (final StackTraceElement[] each : _releaseCallstacks) {
+            for (final StackTraceElement e : each) {
+               logger.logError("  " + e.toString());
+            }
+            logger.logError("---");
+         }
+         logger.logError("=================================================");
+      }
+
+   }
 
 }
