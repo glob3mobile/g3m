ec9b97e1debb249dfc122bfec8b7c70b086cabad
aldeanueva <aldeanueva@igosoftware.es>
2014-05-24 20:52:19 +0200

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/MutableMatrix44D.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/MutableMatrix44D.java
@@ -61,40 +61,7 @@ public class MutableMatrix44D
      _matrix44D = null;
   }
 
-  //MutableMatrix44D& MutableMatrix44D::operator=(const MutableMatrix44D &that) {
-  //  if (this != &that) {
-  //    _m00 = that._m00;
-  //    _m01 = that._m01;
-  //    _m02 = that._m02;
-  //    _m03 = that._m03;
-  //
-  //    _m10 = that._m10;
-  //    _m11 = that._m11;
-  //    _m12 = that._m12;
-  //    _m13 = that._m13;
-  //
-  //    _m20 = that._m20;
-  //    _m21 = that._m21;
-  //    _m22 = that._m22;
-  //    _m23 = that._m23;
-  //
-  //    _m30 = that._m30;
-  //    _m31 = that._m31;
-  //    _m32 = that._m32;
-  //    _m33 = that._m33;
-  //
-  //    _isValid = that._isValid;
-  //
-  //    if (_matrix44D != NULL) {
-  //      _matrix44D->_release();
-  //      _matrix44D = NULL;
-  //    }
-  //  }
-  //
-  //  return *this;
-  //}
-  
-  private void MutableMatrix44D.copyFrom(MutableMatrix44D that)
+  private MutableMatrix44D copyFrom(MutableMatrix44D that)
   {
     if (this != that)
     {
@@ -126,6 +93,8 @@ public class MutableMatrix44D
         _matrix44D = null;
       }
     }
+  
+    return this;
   }
 
 
@@ -242,8 +211,39 @@ public class MutableMatrix44D
 
   }
 
-//C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
-//  void copyFrom(MutableMatrix44D that);
+  public final void copyFrom(MutableMatrix44D that)
+  {
+    if (this != that)
+    {
+      _m00 = that._m00;
+      _m01 = that._m01;
+      _m02 = that._m02;
+      _m03 = that._m03;
+  
+      _m10 = that._m10;
+      _m11 = that._m11;
+      _m12 = that._m12;
+      _m13 = that._m13;
+  
+      _m20 = that._m20;
+      _m21 = that._m21;
+      _m22 = that._m22;
+      _m23 = that._m23;
+  
+      _m30 = that._m30;
+      _m31 = that._m31;
+      _m32 = that._m32;
+      _m33 = that._m33;
+  
+      _isValid = that._isValid;
+  
+      if (_matrix44D != null)
+      {
+        _matrix44D._release();
+        _matrix44D = null;
+      }
+    }
+  }
 
 
   public final Matrix44D asMatrix44D()
