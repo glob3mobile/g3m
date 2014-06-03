1bbcf62e0a60edb58afbd4b06dc9eb8348962683
Vidal Toboso <vidaltoboso@igosoftware.es>
2014-06-03 11:07:02 +0200

    06-03 11:03:35.940: E/AndroidRuntime(6183): java.lang.IndexOutOfBoundsException: Invalid index 0, size is 0

--- iOS/G3MiOSSDK/Commons/Interfaces/ICanvasUtils.cpp
+++ iOS/G3MiOSSDK/Commons/Interfaces/ICanvasUtils.cpp
@@ -26,33 +26,37 @@ Vector2F ICanvasUtils::drawStringsOn(const std::vector<std::string> &strings,
                                      const Color& shadowColor,
                                      const int padding,
                                      const int cornerRadius) {
-  int longestTextIndex = 0;
-  int maxLength = strings.at(longestTextIndex).length();
-  const int stringsSize = strings.size();
-  for (int i = 1; i < stringsSize; i++) {
-    const int itemLength = strings.at(i).length();
-    if (maxLength < itemLength) {
-      maxLength = itemLength;
-      longestTextIndex = i;
-    }
-  }
   
+  const int stringsSize = strings.size();
   int fontSize = maxFontSize;
-  const int maxWidth = width - (2 * padding);
-  bool fit = false;
-  while (!fit && fontSize > minFontSize) {
-    GFont labelFont = GFont::sansSerif(fontSize);
-    const std::string longestText = strings.at(longestTextIndex);
-    canvas->setFont(labelFont);
-    const Vector2F extent = canvas->textExtent(longestText);
-    if (extent._x <= maxWidth) {
-      fit = true;
+  if (stringsSize > 0 ) {
+    int longestTextIndex = 0;
+    int maxLength = strings.at(longestTextIndex).length();
+    for (int i = 1; i < stringsSize; i++) {
+      const int itemLength = strings.at(i).length();
+      if (maxLength < itemLength) {
+        maxLength = itemLength;
+        longestTextIndex = i;
+      }
     }
-    else {
-      fontSize--;
+    
+    
+    
+    const int maxWidth = width - (2 * padding);
+    bool fit = false;
+    while (!fit && fontSize > minFontSize) {
+      GFont labelFont = GFont::sansSerif(fontSize);
+      const std::string longestText = strings.at(longestTextIndex);
+      canvas->setFont(labelFont);
+      const Vector2F extent = canvas->textExtent(longestText);
+      if (extent._x <= maxWidth) {
+        fit = true;
+      }
+      else {
+        fontSize--;
+      }
     }
   }
-
   canvas->setShadow(shadowColor, 1.0f, 1.0f, -1.0f);
   
   ColumnCanvasElement column(backgroundColor,
