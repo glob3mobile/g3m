a3c61dbb65b561f9f5fc2fed5a92af0e99326e3c
MariLuz Mateo <mlmateo@igosoftware.es>
2014-09-05 12:03:34 +0200

    Added Hello World iOS project

---
+++ iOS/HelloWorld/HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Mari Luz Mateo on 04/09/14.
+//  Copyright (c) 2014 Igo Software SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([HWAppDelegate class]));
+  }
+}
