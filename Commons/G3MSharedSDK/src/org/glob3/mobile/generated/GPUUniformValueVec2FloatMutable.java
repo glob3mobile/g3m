b28fdd8117038889a8ab30b39ede4543b99de8f2
aldeanueva <aldeanueva@igosoftware.es>
2014-10-14 13:42:30 +0200

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/GPUUniformValueVec2FloatMutable.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/GPUUniformValueVec2FloatMutable.java
@@ -6,15 +6,37 @@ public class GPUUniformValueVec2FloatMutable extends GPUUniformValueVec2Float
     super.dispose();
   }
 
+  private boolean _hasChangedSinceLastSetUniform;
+
 
   public GPUUniformValueVec2FloatMutable(float x, float y)
   {
-     super(x,y);
+     super(x, y);
+     _hasChangedSinceLastSetUniform = true;
   }
 
   public final void changeValue(float x, float y)
   {
-    _x = x;
-    _y = y;
+    if (x != _x || y != _y)
+    {
+      _x = x;
+      _y = y;
+      _hasChangedSinceLastSetUniform = true;
+    }
+  }
+
+  public final void setUniform(GL gl, IGLUniformID id)
+  {
+    super.setUniform(gl, id);
+    _hasChangedSinceLastSetUniform = false;
+  }
+
+  public final boolean isEquals(GPUUniformValue v)
+  {
+    if (_hasChangedSinceLastSetUniform && v == this)
+    {
+      return false;
+    }
+    return super.isEquals(v);
   }
 }
--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/GPUUniformValueVec3FloatMutable.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/GPUUniformValueVec3FloatMutable.java
@@ -6,16 +6,38 @@ public class GPUUniformValueVec3FloatMutable extends GPUUniformValueVec3Float
     super.dispose();
   }
 
+  private boolean _hasChangedSinceLastSetUniform;
+
 
   public GPUUniformValueVec3FloatMutable(float x, float y, float z)
   {
-     super(x,y,z);
+     super(x, y, z);
+     _hasChangedSinceLastSetUniform = true;
   }
 
   public final void changeValue(float x, float y, float z)
   {
-    _x = x;
-    _y = y;
-    _z = z;
+    if (x != _x || y != _y || z != _z)
+    {
+      _x = x;
+      _y = y;
+      _z = z;
+      _hasChangedSinceLastSetUniform = true;
+    }
+  }
+
+  public final void setUniform(GL gl, IGLUniformID id)
+  {
+    super.setUniform(gl, id);
+    _hasChangedSinceLastSetUniform = false;
+  }
+
+  public final boolean isEquals(GPUUniformValue v)
+  {
+    if (_hasChangedSinceLastSetUniform && v == this)
+    {
+      return false;
+    }
+    return super.isEquals(v);
   }
 }
