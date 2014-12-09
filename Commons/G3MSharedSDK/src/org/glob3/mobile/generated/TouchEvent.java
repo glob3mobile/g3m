866c8cca6716dbd436c935f2f98e0c7a95de9bcb
aldeanueva <aldeanueva@igosoftware.es>
2014-12-09 12:09:24 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/TouchEvent.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/TouchEvent.java
@@ -5,15 +5,17 @@ public class TouchEvent
   private final java.util.ArrayList<Touch> _touchs;
   private final boolean _shiftPressed;
   private final boolean _ctrlPressed;
+  private final boolean _altPressed;
   private final double _wheelDelta;
 
 
-  private TouchEvent(TouchEventType type, java.util.ArrayList<Touch> touchs, boolean shift, boolean ctrl, double wheelDelta)
+  private TouchEvent(TouchEventType type, java.util.ArrayList<Touch> touchs, boolean shift, boolean ctrl, boolean alt, double wheelDelta)
   {
      _eventType = type;
      _touchs = touchs;
      _shiftPressed = shift;
      _ctrlPressed = ctrl;
+     _altPressed = alt;
      _wheelDelta = wheelDelta;
   }
 
@@ -23,17 +25,18 @@ public class TouchEvent
      _touchs = other._touchs;
      _shiftPressed = other._shiftPressed;
      _ctrlPressed = other._ctrlPressed;
+     _altPressed = other._altPressed;
      _wheelDelta = other._wheelDelta;
   }
 
   public static TouchEvent create(TouchEventType type, java.util.ArrayList<Touch> touchs)
   {
-    return new TouchEvent(type, touchs, false, false, 0.0);
+    return new TouchEvent(type, touchs, false, false, false, 0.0);
   }
 
-  public static TouchEvent create(TouchEventType type, java.util.ArrayList<Touch> touchs, boolean shift, boolean ctrl, double wheelDelta)
+  public static TouchEvent create(TouchEventType type, java.util.ArrayList<Touch> touchs, boolean shift, boolean ctrl, boolean alt, double wheelDelta)
   {
-    return new TouchEvent(type, touchs, shift, ctrl, wheelDelta);
+    return new TouchEvent(type, touchs, shift, ctrl, alt, wheelDelta);
   }
 
   public static TouchEvent create(TouchEventType type, Touch touch)
@@ -43,11 +46,11 @@ public class TouchEvent
     return create(type, touchs);
   }
 
-  public static TouchEvent create(TouchEventType type, Touch touch, boolean shift, boolean ctrl, double wheelDelta)
+  public static TouchEvent create(TouchEventType type, Touch touch, boolean shift, boolean ctrl, boolean alt, double wheelDelta)
   {
     final java.util.ArrayList<Touch> touchs = new java.util.ArrayList<Touch>(java.util.Arrays.asList(touch)); //CHANGED BY CONVERSOR RULE
 
-    return create(type, touchs, shift, ctrl, wheelDelta);
+    return create(type, touchs, shift, ctrl, alt, wheelDelta);
   }
 
   public final TouchEventType getType()
@@ -84,6 +87,11 @@ public class TouchEvent
   public final boolean isCtrlPressed()
   {
     return _ctrlPressed;
+  }
+
+  public final boolean isAltPressed()
+  {
+    return _altPressed;
   }
 
 }
