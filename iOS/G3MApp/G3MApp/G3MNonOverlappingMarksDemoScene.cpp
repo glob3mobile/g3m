2c6ad130bff99afa41c2b22a244eb498d3e78061
Diego Gomez Deck <dgd@consultar.com>
2015-01-30 19:32:51 -0500

    -

--- iOS/G3MApp/G3MApp/G3MNonOverlappingMarksDemoScene.cpp
+++ iOS/G3MApp/G3MApp/G3MNonOverlappingMarksDemoScene.cpp
@@ -14,7 +14,6 @@
 #include <G3MiOSSDK/DownloaderImageBuilder.hpp>
 #include <G3MiOSSDK/G3MWidget.hpp>
 
-
 #include "G3MDemoModel.hpp"
 
 void G3MNonOverlappingMarksDemoScene::rawActivate(const G3MContext* context) {
@@ -25,47 +24,50 @@ void G3MNonOverlappingMarksDemoScene::rawActivate(const G3MContext* context) {
   OSMLayer* layer = new OSMLayer(TimeInterval::fromDays(30));
   model->getLayerSet()->addLayer(layer);
 
-  NonOverlappingMarksRenderer* nonOverlappingMarksRenderer = model->getNonOverlappingMarksRenderer();
 
-  NonOverlappingMark* mark = new NonOverlappingMark(new DownloaderImageBuilder(URL("file:///g3m-marker.png")),
-                                                    new DownloaderImageBuilder(URL("file:///anchorWidget.png")),
-                                                    Geodetic3D::fromDegrees(28.131817, -15.440219, 0));
-  nonOverlappingMarksRenderer->addMark(mark);
+  NonOverlappingMarksRenderer* renderer = model->getNonOverlappingMarksRenderer();
 
-  NonOverlappingMark* mark2 = new NonOverlappingMark(new DownloaderImageBuilder(URL("file:///g3m-marker.png")),
-                                                     new DownloaderImageBuilder(URL("file:///anchorWidget.png")),
+  const URL markBitmapURL("file:///g3m-marker.png");
+  const URL anchorBitmapURL("file:///anchorWidget.png");
+
+  NonOverlappingMark* mark1 = new NonOverlappingMark(new DownloaderImageBuilder(markBitmapURL),
+                                                     new DownloaderImageBuilder(anchorBitmapURL),
+                                                     Geodetic3D::fromDegrees(28.131817, -15.440219, 0));
+  renderer->addMark(mark1);
+
+  NonOverlappingMark* mark2 = new NonOverlappingMark(new DownloaderImageBuilder(markBitmapURL),
+                                                     new DownloaderImageBuilder(anchorBitmapURL),
                                                      Geodetic3D::fromDegrees(28.947345, -13.523105, 0));
-  nonOverlappingMarksRenderer->addMark(mark2);
+  renderer->addMark(mark2);
 
-  NonOverlappingMark* mark3 = new NonOverlappingMark(new DownloaderImageBuilder(URL("file:///g3m-marker.png")),
-                                                     new DownloaderImageBuilder(URL("file:///anchorWidget.png")),
+  NonOverlappingMark* mark3 = new NonOverlappingMark(new DownloaderImageBuilder(markBitmapURL),
+                                                     new DownloaderImageBuilder(anchorBitmapURL),
                                                      Geodetic3D::fromDegrees(28.473802, -13.859360, 0));
-  nonOverlappingMarksRenderer->addMark(mark3);
+  renderer->addMark(mark3);
 
-  NonOverlappingMark* mark4 = new NonOverlappingMark(new DownloaderImageBuilder(URL("file:///g3m-marker.png")),
-                                                     new DownloaderImageBuilder(URL("file:///anchorWidget.png")),
+  NonOverlappingMark* mark4 = new NonOverlappingMark(new DownloaderImageBuilder(markBitmapURL),
+                                                     new DownloaderImageBuilder(anchorBitmapURL),
                                                      Geodetic3D::fromDegrees(28.467706, -16.251426, 0));
-  nonOverlappingMarksRenderer->addMark(mark4);
+  renderer->addMark(mark4);
 
-  NonOverlappingMark* mark5 = new NonOverlappingMark(new DownloaderImageBuilder(URL("file:///g3m-marker.png")),
-                                                     new DownloaderImageBuilder(URL("file:///anchorWidget.png")),
+  NonOverlappingMark* mark5 = new NonOverlappingMark(new DownloaderImageBuilder(markBitmapURL),
+                                                     new DownloaderImageBuilder(anchorBitmapURL),
                                                      Geodetic3D::fromDegrees(28.701819, -17.762003, 0));
-  nonOverlappingMarksRenderer->addMark(mark5);
+  renderer->addMark(mark5);
 
-  NonOverlappingMark* mark6 = new NonOverlappingMark(new DownloaderImageBuilder(URL("file:///g3m-marker.png")),
-                                                     new DownloaderImageBuilder(URL("file:///anchorWidget.png")),
+  NonOverlappingMark* mark6 = new NonOverlappingMark(new DownloaderImageBuilder(markBitmapURL),
+                                                     new DownloaderImageBuilder(anchorBitmapURL),
                                                      Geodetic3D::fromDegrees(28.086595, -17.105796, 0));
-  nonOverlappingMarksRenderer->addMark(mark6);
+  renderer->addMark(mark6);
 
-  NonOverlappingMark* mark7 = new NonOverlappingMark(new DownloaderImageBuilder(URL("file:///g3m-marker.png")),
-                                                     new DownloaderImageBuilder(URL("file:///anchorWidget.png")),
+  NonOverlappingMark* mark7 = new NonOverlappingMark(new DownloaderImageBuilder(markBitmapURL),
+                                                     new DownloaderImageBuilder(anchorBitmapURL),
                                                      Geodetic3D::fromDegrees(27.810709, -17.917639, 0));
-  nonOverlappingMarksRenderer->addMark(mark7);
+  renderer->addMark(mark7);
 
 
-
+  // Canarias, here we go!
   g3mWidget->setAnimatedCameraPosition( Geodetic3D::fromDegrees(28.034468668529083146,
                                                                 -15.904092315837871752,
                                                                 1634079) );
-
 }
