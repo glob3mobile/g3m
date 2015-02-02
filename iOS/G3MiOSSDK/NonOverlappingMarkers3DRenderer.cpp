027f78ac7f342217d6516813ca2a122486c6389e
Stefanie Alfonso <astef@seas.upenn.edu>
2015-02-01 18:26:46 -0800

    added empty class

---
+++ iOS/G3MiOSSDK/NonOverlappingMarkers3DRenderer.cpp
@@ -0,0 +1,9 @@
+//
+//  NonOverlappingMarkers3DRenderer.cpp
+//  G3MiOSSDK
+//
+//  Created by Stefanie Alfonso on 2/1/15.
+//
+//
+
+#include "NonOverlappingMarkers3DRenderer.h"
---
+++ iOS/G3MiOSSDK/NonOverlappingMarkers3DRenderer.h
@@ -0,0 +1,14 @@
+//
+//  NonOverlappingMarkers3DRenderer.h
+//  G3MiOSSDK
+//
+//  Created by Stefanie Alfonso on 2/1/15.
+//
+//
+
+#ifndef __G3MiOSSDK__NonOverlappingMarkers3DRenderer__
+#define __G3MiOSSDK__NonOverlappingMarkers3DRenderer__
+
+#include <iostream>
+
+#endif /* defined(__G3MiOSSDK__NonOverlappingMarkers3DRenderer__) */
