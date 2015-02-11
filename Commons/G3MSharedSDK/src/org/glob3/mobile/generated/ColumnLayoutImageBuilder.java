5c23940c01265508bb0029e7a989dd452faec991
aldeanueva <aldeanueva@igosoftware.es>
2015-02-11 23:34:19 +0100

    Generated

---
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/ColumnLayoutImageBuilder.java
@@ -0,0 +1,148 @@
+package org.glob3.mobile.generated; 
+//
+//  ColumnLayoutImageBuilder.cpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 2/11/15.
+//
+//
+
+//
+//  ColumnLayoutImageBuilder.hpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 2/11/15.
+//
+//
+
+
+
+public class ColumnLayoutImageBuilder extends LayoutImageBuilder
+{
+  protected final void doLayout(G3MContext context, IImageBuilderListener listener, boolean deleteListener, java.util.ArrayList<ChildResult> results)
+  {
+    boolean anyError = false;
+    String error;
+    String imageName;
+  
+    int maxWidth = 0;
+    int accumulatedHeight = 0;
+  
+    final int resultsSize = results.size();
+    for (int i = 0; i < resultsSize; i++)
+    {
+      ChildResult result = results.get(i);
+      final IImage image = result._image;
+  
+      if (image == null)
+      {
+        anyError = true;
+        error += result._error + " ";
+      }
+      else
+      {
+        accumulatedHeight += image.getHeight();
+        if (image.getWidth() > maxWidth)
+        {
+          maxWidth = image.getWidth();
+        }
+        imageName += result._imageName + "/";
+      }
+    }
+  
+    if (anyError)
+    {
+      if (listener != null)
+      {
+        listener.onError(error);
+        if (deleteListener)
+        {
+          if (listener != null)
+             listener.dispose();
+        }
+      }
+    }
+    else
+    {
+      ICanvas canvas = context.getFactory().createCanvas();
+  
+      final int width = maxWidth;
+      final int height = accumulatedHeight;
+      canvas.initialize(width, height);
+//C++ TO JAVA CONVERTER TODO TASK: There is no preprocessor in Java:
+//#warning remove debug code
+      canvas.setFillColor(Color.red());
+      canvas.fillRectangle(0, 0, width, height);
+  
+      float cursorTop = height;
+      for (int i = 0; i < resultsSize; i++)
+      {
+        ChildResult result = results.get(i);
+        final IImage image = result._image;
+  
+        final float destLeft = ((float)(width - image.getWidth()) / 2.0f);
+        cursorTop -= image.getHeight();
+        canvas.drawImage(image, destLeft, cursorTop);
+      }
+  
+      canvas.createImage(new ColumnLayoutImageBuilder_IImageListener(imageName, listener, deleteListener), true);
+      if (canvas != null)
+         canvas.dispose();
+    }
+  
+//C++ TO JAVA CONVERTER TODO TASK: There is no preprocessor in Java:
+//#warning delete images??
+    for (int i = 0; i < resultsSize; i++)
+    {
+      ChildResult result = results.get(i);
+      result = null;
+    }
+  
+  }
+
+
+  public ColumnLayoutImageBuilder(java.util.ArrayList<IImageBuilder> children, int margin, int borderWidth, Color borderColor)
+  {
+     this(children, margin, borderWidth, borderColor, 2);
+  }
+  public ColumnLayoutImageBuilder(java.util.ArrayList<IImageBuilder> children, int margin, int borderWidth)
+  {
+     this(children, margin, borderWidth, Color.transparent(), 2);
+  }
+  public ColumnLayoutImageBuilder(java.util.ArrayList<IImageBuilder> children, int margin)
+  {
+     this(children, margin, 0, Color.transparent(), 2);
+  }
+  public ColumnLayoutImageBuilder(java.util.ArrayList<IImageBuilder> children)
+  {
+     this(children, 4, 0, Color.transparent(), 2);
+  }
+  public ColumnLayoutImageBuilder(java.util.ArrayList<IImageBuilder> children, int margin, int borderWidth, Color borderColor, int padding)
+  {
+     super(children, margin, borderWidth, borderColor, padding);
+  
+  }
+
+  public ColumnLayoutImageBuilder(IImageBuilder child0, IImageBuilder child1, int margin, int borderWidth, Color borderColor)
+  {
+     this(child0, child1, margin, borderWidth, borderColor, 2);
+  }
+  public ColumnLayoutImageBuilder(IImageBuilder child0, IImageBuilder child1, int margin, int borderWidth)
+  {
+     this(child0, child1, margin, borderWidth, Color.transparent(), 2);
+  }
+  public ColumnLayoutImageBuilder(IImageBuilder child0, IImageBuilder child1, int margin)
+  {
+     this(child0, child1, margin, 0, Color.transparent(), 2);
+  }
+  public ColumnLayoutImageBuilder(IImageBuilder child0, IImageBuilder child1)
+  {
+     this(child0, child1, 4, 0, Color.transparent(), 2);
+  }
+  public ColumnLayoutImageBuilder(IImageBuilder child0, IImageBuilder child1, int margin, int borderWidth, Color borderColor, int padding)
+  {
+     super(child0, child1, margin, borderWidth, borderColor, padding);
+  
+  }
+
+}
---
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/LayoutImageBuilder.java
@@ -0,0 +1,192 @@
+package org.glob3.mobile.generated; 
+//
+//  LayoutImageBuilder.cpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 2/11/15.
+//
+//
+
+//
+//  LayoutImageBuilder.hpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 2/11/15.
+//
+//
+
+
+
+public abstract class LayoutImageBuilder extends AbstractImageBuilder
+{
+
+  protected static class ChildResult
+  {
+    public final IImage _image;
+    public final String _imageName;
+    public final String _error;
+
+    public ChildResult(IImage image, String imageName)
+    {
+       _image = image;
+       _imageName = imageName;
+       _error = "";
+    }
+
+    public ChildResult(String error)
+    {
+       _image = null;
+       _imageName = "";
+       _error = error;
+    }
+
+    public void dispose()
+    {
+      if (_image != null)
+         _image.dispose();
+    }
+  }
+
+  protected static class ChildrenResult
+  {
+    private LayoutImageBuilder _layoutImageBuilder;
+    private final G3MContext _context;
+    private IImageBuilderListener _listener;
+    private boolean _deleteListener;
+
+    private int _childrenResultPendingCounter;
+
+    public java.util.ArrayList<ChildResult> _childrenResult = new java.util.ArrayList<ChildResult>();
+
+    public ChildrenResult(LayoutImageBuilder layoutImageBuilder, int childrenSize, G3MContext context, IImageBuilderListener listener, boolean deleteListener)
+    {
+       _layoutImageBuilder = layoutImageBuilder;
+       _context = context;
+       _listener = listener;
+       _deleteListener = deleteListener;
+      _childrenResultPendingCounter = childrenSize;
+      for (int i = 0; i < childrenSize; i++)
+      {
+        _childrenResult.add(null); // make space for the result
+      }
+    }
+
+    public final void childImageCreated(IImage image, String imageName, int childIndex)
+    {
+      if (_childrenResult.get(childIndex) != null)
+      {
+        throw new RuntimeException("Logic error");
+      }
+    
+      _childrenResult.set(childIndex, new ChildResult(image, imageName));
+      if (--_childrenResultPendingCounter == 0)
+      {
+        _layoutImageBuilder.doLayout(_context, _listener, _deleteListener, _childrenResult);
+      }
+    }
+
+    public final void childError(String error, int childIndex)
+    {
+      if (_childrenResult.get(childIndex) != null)
+      {
+        throw new RuntimeException("Logic error");
+      }
+    
+      _childrenResult.set(childIndex, new ChildResult(error));
+      if (--_childrenResultPendingCounter == 0)
+      {
+        _layoutImageBuilder.doLayout(_context, _listener, _deleteListener, _childrenResult);
+      }
+    }
+  }
+
+  protected static class LayoutImageBuilderChildListener implements IImageBuilderListener
+  {
+    private ChildrenResult _childrenResult;
+    private final int _childIndex;
+
+    public LayoutImageBuilderChildListener(ChildrenResult childrenResult, int childIndex)
+    {
+       _childrenResult = childrenResult;
+       _childIndex = childIndex;
+    }
+
+    public final void imageCreated(IImage image, String imageName)
+    {
+      _childrenResult.childImageCreated(image, imageName, _childIndex);
+    }
+
+    public final void onError(String error)
+    {
+      _childrenResult.childError(error, _childIndex);
+    }
+  }
+
+
+  protected java.util.ArrayList<IImageBuilder> _children = new java.util.ArrayList<IImageBuilder>();
+
+  protected final int _margin;
+  protected final int _borderWidth;
+  protected final Color _borderColor ;
+  protected final int _padding;
+
+
+  /*
+   | margin
+   |   border
+   |     padding
+   |       content
+   */
+
+  protected LayoutImageBuilder(java.util.ArrayList<IImageBuilder> children, int margin, int borderWidth, Color borderColor, int padding)
+  {
+     _children = children;
+     _margin = margin;
+     _borderWidth = borderWidth;
+     _borderColor = new Color(borderColor);
+     _padding = padding;
+  }
+
+  protected LayoutImageBuilder(IImageBuilder child0, IImageBuilder child1, int margin, int borderWidth, Color borderColor, int padding)
+  {
+     _margin = margin;
+     _borderWidth = borderWidth;
+     _borderColor = new Color(borderColor);
+     _padding = padding;
+    _children.add(child0);
+    _children.add(child1);
+  }
+
+  protected abstract void doLayout(G3MContext context, IImageBuilderListener listener, boolean deleteListener, java.util.ArrayList<ChildResult> results);
+
+  public void dispose()
+  {
+    final int childrenSize = _children.size();
+    for (int i = 0; i < childrenSize; i++)
+    {
+      IImageBuilder child = _children.get(i);
+      if (child != null)
+         child.dispose();
+    }
+  }
+
+  public final boolean isMutable()
+  {
+//C++ TO JAVA CONVERTER TODO TASK: There is no preprocessor in Java:
+//#warning TODO: make mutable if any children is
+    return false;
+  }
+
+  public final void build(G3MContext context, IImageBuilderListener listener, boolean deleteListener)
+  {
+    final int childrenSize = _children.size();
+    ChildrenResult childrenResult = new ChildrenResult(this, childrenSize, context, listener, deleteListener);
+    for (int i = 0; i < childrenSize; i++)
+    {
+      IImageBuilder child = _children.get(i);
+  
+      child.build(context, new LayoutImageBuilderChildListener(childrenResult, i), true);
+    }
+  }
+
+}
