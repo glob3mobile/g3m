9abea907d9d73d469ecb3eb848dd2ee3e89688c1
Diego Gomez Deck <dgd@consultar.com>
2015-02-12 12:07:09 -0500

    -

--- Android/G3MAndroidTestingApplication/src/com/glob3/mobile/g3mandroidtestingapplication/MainActivity.java
+++ Android/G3MAndroidTestingApplication/src/com/glob3/mobile/g3mandroidtestingapplication/MainActivity.java
@@ -2,8 +2,11 @@
 
 package com.glob3.mobile.g3mandroidtestingapplication;
 
+import org.glob3.mobile.generated.ColumnLayoutImageBuilder;
 import org.glob3.mobile.generated.DownloaderImageBuilder;
+import org.glob3.mobile.generated.GFont;
 import org.glob3.mobile.generated.Geodetic3D;
+import org.glob3.mobile.generated.LabelImageBuilder;
 import org.glob3.mobile.generated.NonOverlappingMark;
 import org.glob3.mobile.generated.NonOverlappingMarksRenderer;
 import org.glob3.mobile.generated.URL;
@@ -18,8 +21,8 @@ import android.widget.RelativeLayout;
 
 
 public class MainActivity
-         extends
-            Activity {
+extends
+Activity {
 
    private G3MWidget_Android _g3mWidget;
 
@@ -52,37 +55,23 @@ public class MainActivity
                new DownloaderImageBuilder(markBitmapURL), //
                new DownloaderImageBuilder(anchorBitmapURL), //
                position);
+   }
 
 
-      //      final MarkWidgetTouchListener touchListener = null;
-      //      final float springLengthInPixels = 100.0f;
-      //      final float springK = 70.0f;
-      //      final float minSpringLength = 0.0f;
-      //      final float maxSpringLength = 0.0f;
-      //      final float electricCharge = 3000.0f;
-      //      final float anchorElectricCharge = 2000.0f;
-      //      //final float minWidgetSpeedInPixelsPerSecond = 5.0f;
-      //      final float minWidgetSpeedInPixelsPerSecond = 50.0f;
-      //      final float maxWidgetSpeedInPixelsPerSecond = 1000.0f;
-      //      final float resistanceFactor = 0.95f;
-      //
-      //
-      //      return new NonOverlappingMark( //
-      //               new DownloaderImageBuilder(markBitmapURL), //
-      //               new DownloaderImageBuilder(anchorBitmapURL), //
-      //               position, //
-      //               touchListener, //
-      //               springLengthInPixels, //
-      //               springK, //
-      //               minSpringLength, //
-      //               maxSpringLength, //
-      //               electricCharge, //
-      //               anchorElectricCharge, //
-      //               minWidgetSpeedInPixelsPerSecond, //
-      //               maxWidgetSpeedInPixelsPerSecond, //
-      //               resistanceFactor //
-      //      );
+   private static NonOverlappingMark createMark(final String label,
+                                                final Geodetic3D position) {
+      final URL markBitmapURL = new URL("file:///g3m-marker.png");
+      final URL anchorBitmapURL = new URL("file:///anchorWidget.png");
 
+      final ColumnLayoutImageBuilder imageBuilderWidget = new ColumnLayoutImageBuilder( //
+               new DownloaderImageBuilder(markBitmapURL), //
+               new LabelImageBuilder(label, GFont.monospaced(50, true)) //
+      );
+
+      return new NonOverlappingMark( //
+               imageBuilderWidget, //
+               new DownloaderImageBuilder(anchorBitmapURL), //
+               position);
    }
 
 
@@ -93,7 +82,7 @@ public class MainActivity
       final NonOverlappingMarksRenderer renderer = new NonOverlappingMarksRenderer(30);
       builder.addRenderer(renderer);
 
-      renderer.addMark(createMark(Geodetic3D.fromDegrees(28.131817, -15.440219, 0)));
+      renderer.addMark(createMark("Label #1", Geodetic3D.fromDegrees(28.131817, -15.440219, 0)));
       renderer.addMark(createMark(Geodetic3D.fromDegrees(28.947345, -13.523105, 0)));
       renderer.addMark(createMark(Geodetic3D.fromDegrees(28.473802, -13.859360, 0)));
       renderer.addMark(createMark(Geodetic3D.fromDegrees(28.467706, -16.251426, 0)));
