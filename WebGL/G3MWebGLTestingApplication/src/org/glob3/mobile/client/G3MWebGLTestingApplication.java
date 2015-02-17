eb8ef1294fa0429daa796c6e7555409f9903c15f
Diego Gomez Deck <dgd@consultar.com>
2015-02-17 13:28:09 -0500

    bugs fixed on Canvas_WebGL

--- WebGL/G3MWebGLTestingApplication/src/org/glob3/mobile/client/G3MWebGLTestingApplication.java
+++ WebGL/G3MWebGLTestingApplication/src/org/glob3/mobile/client/G3MWebGLTestingApplication.java
@@ -2,31 +2,22 @@
 
 package org.glob3.mobile.client;
 
-import java.util.ArrayList;
-
+import org.glob3.mobile.generated.AltitudeMode;
 import org.glob3.mobile.generated.Color;
-import org.glob3.mobile.generated.GEO2DLineRasterStyle;
-import org.glob3.mobile.generated.GEO2DLineStringGeometry;
-import org.glob3.mobile.generated.GEO2DMultiLineStringGeometry;
-import org.glob3.mobile.generated.GEO2DMultiPolygonGeometry;
-import org.glob3.mobile.generated.GEO2DPointGeometry;
-import org.glob3.mobile.generated.GEO2DPolygonData;
-import org.glob3.mobile.generated.GEO2DPolygonGeometry;
-import org.glob3.mobile.generated.GEO2DSurfaceRasterStyle;
-import org.glob3.mobile.generated.GEOGeometry;
-import org.glob3.mobile.generated.GEOPolygonRasterSymbol;
-import org.glob3.mobile.generated.GEORasterSymbol;
-import org.glob3.mobile.generated.GEORasterSymbolizer;
-import org.glob3.mobile.generated.JSONObject;
+import org.glob3.mobile.generated.G3MContext;
+import org.glob3.mobile.generated.GFont;
+import org.glob3.mobile.generated.GInitializationTask;
+import org.glob3.mobile.generated.Geodetic3D;
+import org.glob3.mobile.generated.ICanvas;
+import org.glob3.mobile.generated.IImage;
+import org.glob3.mobile.generated.IImageDownloadListener;
+import org.glob3.mobile.generated.IImageListener;
 import org.glob3.mobile.generated.LayerSet;
-import org.glob3.mobile.generated.MapBoxLayer;
-import org.glob3.mobile.generated.MarksRenderer;
-import org.glob3.mobile.generated.Sector;
+import org.glob3.mobile.generated.MapQuestLayer;
+import org.glob3.mobile.generated.QuadShape;
 import org.glob3.mobile.generated.ShapesRenderer;
-import org.glob3.mobile.generated.StrokeCap;
-import org.glob3.mobile.generated.StrokeJoin;
-import org.glob3.mobile.generated.TiledVectorLayer;
 import org.glob3.mobile.generated.TimeInterval;
+import org.glob3.mobile.generated.URL;
 import org.glob3.mobile.specific.Downloader_WebGL;
 import org.glob3.mobile.specific.G3MBuilder_WebGL;
 import org.glob3.mobile.specific.G3MWidget_WebGL;
@@ -37,20 +28,14 @@ import com.google.gwt.user.client.ui.RootPanel;
 
 
 public class G3MWebGLTestingApplication
-         implements
-            EntryPoint {
-
-   private final String         _g3mWidgetHolderId = "g3mWidgetHolder";
-
-   private G3MWidget_WebGL      _widget            = null;
-
-   private final boolean        _markersParsed     = false;
-   private MarksRenderer        _markersRenderer;
-   private final ShapesRenderer _shapesRenderer    = new ShapesRenderer();
+implements
+EntryPoint {
+   private static final String _g3mWidgetHolderId = "g3mWidgetHolder";
+   private G3MWidget_WebGL     _g3mWidget         = null;
 
 
    private native void runUserPlugin() /*-{
-		$wnd.onLoadG3M();
+        $wnd.onLoadG3M();
    }-*/;
 
 
@@ -60,9 +45,22 @@ public class G3MWebGLTestingApplication
 
       final Panel g3mWidgetHolder = RootPanel.get(_g3mWidgetHolderId);
 
+      _g3mWidget = createWidget();
+      g3mWidgetHolder.add(_g3mWidget);
+
+
+      // Buenos Aires, there we go!
+      _g3mWidget.setAnimatedCameraPosition(Geodetic3D.fromDegrees(-34.615047738942699596, -58.4447233540403559, 35000));
+   }
+
+
+   private static G3MWidget_WebGL createWidget() {
       final G3MBuilder_WebGL builder = new G3MBuilder_WebGL();
 
-      builder.getPlanetRendererBuilder().setLayerSet(createLayerSet());
+      final LayerSet layerSet = new LayerSet();
+      layerSet.addLayer(MapQuestLayer.newOSM(TimeInterval.fromDays(30)));
+
+      builder.getPlanetRendererBuilder().setLayerSet(layerSet);
 
       final String proxy = null; // "http://galileo.glob3mobile.com/" + "proxy.php?url="
       builder.setDownloader(new Downloader_WebGL( //
@@ -70,116 +68,131 @@ public class G3MWebGLTestingApplication
                10, // delayMillis
                proxy));
 
-      _widget = builder.createWidget();
-      g3mWidgetHolder.add(_widget);
+
+      final boolean testCanvas = true;
+      if (testCanvas) {
+         final ShapesRenderer shapesRenderer = new ShapesRenderer();
+         builder.addRenderer(shapesRenderer);
+
+
+         builder.setInitializationTask(new GInitializationTask() {
+            @Override
+            public void run(final G3MContext context) {
+
+
+               final IImageDownloadListener listener = new IImageDownloadListener() {
+                  @Override
+                  public void onError(final URL url) {
+                  }
+
+
+                  @Override
+                  public void onDownload(final URL url,
+                                         final IImage image,
+                                         final boolean expired) {
+                     final ICanvas canvas = context.getFactory().createCanvas();
+                     final int width = 1024;
+                     final int height = 1024;
+                     canvas.initialize(width, height);
+
+                     canvas.setFillColor(Color.fromRGBA(1f, 1f, 0f, 0.5f));
+                     canvas.fillRectangle(0, 0, width, height);
+                     canvas.setLineWidth(4);
+                     canvas.setLineColor(Color.black());
+                     canvas.strokeRectangle(0, 0, width, height);
+
+                     final int steps = 8;
+                     final float leftStep = (float) width / steps;
+                     final float topStep = (float) height / steps;
+
+                     canvas.setLineWidth(1);
+                     canvas.setFillColor(Color.fromRGBA(0f, 0f, 0f, 0.75f));
+                     for (int i = 1; i < steps; i++) {
+                        canvas.fillRectangle(0, topStep * i, width, 1);
+                        canvas.fillRectangle(leftStep * i, 0, 1, height);
+                     }
+
+                     canvas.setFont(GFont.monospaced());
+                     canvas.setFillColor(Color.black());
+                     //                  canvas.fillText("0,0", 0, 0);
+                     //                  canvas.fillText("w,h", width, height);
+                     for (int i = 0; i < steps; i++) {
+                        canvas.fillText("Hellow World", leftStep * i, topStep * i);
+                     }
+
+
+                     canvas.drawImage(image, width / 8, height / 8); // ok
+                     canvas.drawImage(image, (width / 8) * 3, height / 8, 0.5f); // ok
+
+                     canvas.drawImage(image, width / 8, (height / 8) * 3, image.getWidth() * 2, image.getHeight() * 2); // ok
+                     canvas.drawImage(image, (width / 8) * 3, (height / 8) * 3, image.getWidth() * 2, image.getHeight() * 2, 0.5f); //ok
+
+                     // ok
+                     canvas.drawImage(image, //
+                              0, 0, image.getWidth(), image.getHeight(), //
+                              (width / 8) * 5, (height / 8) * 5, image.getWidth() * 2, image.getHeight() * 2);
+                     // ok
+                     canvas.drawImage(image, //
+                              0, 0, image.getWidth(), image.getHeight(), //
+                              (width / 8) * 7, (height / 8) * 7, image.getWidth() * 2, image.getHeight() * 2, //
+                              0.5f);
+
+
+                     canvas.createImage(new IImageListener() {
+                        @Override
+                        public void imageCreated(final IImage canvasImage) {
+                           final QuadShape quad = new QuadShape( //
+                                    Geodetic3D.fromDegrees(-34.615047738942699596, -58.4447233540403559, 1000), //
+                                    AltitudeMode.ABSOLUTE, //
+                                    canvasImage, //
+                                    canvasImage.getWidth() * 15.0f, //
+                                    canvasImage.getHeight() * 15.0f, //
+                                    true);
+
+                           shapesRenderer.addShape(quad);
+                        }
+                     }, true);
+
+                     canvas.dispose();
+
+                     image.dispose();
+                  }
+
+
+                  @Override
+                  public void onCanceledDownload(final URL url,
+                                                 final IImage image,
+                                                 final boolean expired) {
+                  }
+
+
+                  @Override
+                  public void onCancel(final URL url) {
+                  }
+               };
+
+
+               context.getDownloader().requestImage( //
+                        // new URL("file:///g3m-marker.png"), //
+                        new URL("/g3m-marker.png"), //
+                        1, // priority, //
+                        TimeInterval.zero(), //
+                        false, //
+                        listener, //
+                        true);
+            }
+
+
+            @Override
+            public boolean isDone(final G3MContext context) {
+               return true;
+            }
+         });
+      }
+
+
+      return builder.createWidget();
    }
 
-
-   private LayerSet createLayerSet() {
-      final LayerSet layerSet = new LayerSet();
-      //      layerSet.addLayer(MapQuestLayer.newOSM(TimeInterval.fromDays(30)));
-
-      layerSet.addLayer(new MapBoxLayer("examples.map-9ijuk24y", TimeInterval.fromDays(30)));
-
-
-      final String urlTemplate = "http://192.168.1.2/ne_10m_admin_0_countries-Levels10/{level}/{y}/{x}.geojson";
-      final int firstLevel = 2;
-      final int maxLevel = 10;
-
-      final GEORasterSymbolizer symbolizer = new SampleRasterSymbolizer();
-
-      final TiledVectorLayer tiledVectorLayer = TiledVectorLayer.newMercator( //
-               symbolizer, //
-               urlTemplate, //
-               Sector.fullSphere(), // sector
-               firstLevel, //
-               maxLevel, //
-               TimeInterval.fromDays(30), // timeToCache
-               true, // readExpired
-               1, // transparency
-               null // condition
-      );
-      layerSet.addLayer(tiledVectorLayer);
-
-
-      return layerSet;
-   }
-
-   private static class SampleRasterSymbolizer
-            extends
-               GEORasterSymbolizer {
-
-      private static final Color FROM_COLOR = Color.fromRGBA(0.7f, 0, 0, 0.5f);
-
-
-      private static GEO2DLineRasterStyle createPolygonLineRasterStyle(final GEOGeometry geometry) {
-         final JSONObject properties = geometry.getFeature().getProperties();
-         final int colorIndex = (int) properties.getAsNumber("mapcolor7", 0);
-         final Color color = FROM_COLOR.wheelStep(7, colorIndex).muchLighter().muchLighter();
-         final float dashLengths[] = {};
-         final int dashCount = 0;
-         return new GEO2DLineRasterStyle(color, 2, StrokeCap.CAP_ROUND, StrokeJoin.JOIN_ROUND, 1, dashLengths, dashCount, 0);
-      }
-
-
-      private static GEO2DSurfaceRasterStyle createPolygonSurfaceRasterStyle(final GEOGeometry geometry) {
-         final JSONObject properties = geometry.getFeature().getProperties();
-         final int colorIndex = (int) properties.getAsNumber("mapcolor7", 0);
-         final Color color = FROM_COLOR.wheelStep(7, colorIndex);
-         return new GEO2DSurfaceRasterStyle(color);
-      }
-
-
-      @Override
-      public GEORasterSymbolizer copy() {
-         return new SampleRasterSymbolizer();
-      }
-
-
-      @Override
-      public ArrayList<GEORasterSymbol> createSymbols(final GEO2DPointGeometry geometry) {
-         return null;
-      }
-
-
-      @Override
-      public ArrayList<GEORasterSymbol> createSymbols(final GEO2DLineStringGeometry geometry) {
-         return null;
-      }
-
-
-      @Override
-      public ArrayList<GEORasterSymbol> createSymbols(final GEO2DMultiLineStringGeometry geometry) {
-         return null;
-      }
-
-
-      @Override
-      public ArrayList<GEORasterSymbol> createSymbols(final GEO2DPolygonGeometry geometry) {
-         final ArrayList<GEORasterSymbol> symbols = new ArrayList<GEORasterSymbol>();
-         final GEOPolygonRasterSymbol symbol = new GEOPolygonRasterSymbol( //
-                  geometry.getPolygonData(), //
-                  createPolygonLineRasterStyle(geometry), //
-                  createPolygonSurfaceRasterStyle(geometry) //
-         );
-         symbols.add(symbol);
-         return symbols;
-      }
-
-
-      @Override
-      public ArrayList<GEORasterSymbol> createSymbols(final GEO2DMultiPolygonGeometry geometry) {
-         final ArrayList<GEORasterSymbol> symbols = new ArrayList<GEORasterSymbol>();
-
-         final GEO2DLineRasterStyle lineStyle = createPolygonLineRasterStyle(geometry);
-         final GEO2DSurfaceRasterStyle surfaceStyle = createPolygonSurfaceRasterStyle(geometry);
-
-         for (final GEO2DPolygonData polygonData : geometry.getPolygonsData()) {
-            symbols.add(new GEOPolygonRasterSymbol(polygonData, lineStyle, surfaceStyle));
-         }
-
-         return symbols;
-      }
-   }
 
 }
