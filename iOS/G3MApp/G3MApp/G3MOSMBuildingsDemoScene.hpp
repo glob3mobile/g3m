7dc265e14999be50df9a11492dc7cd8ca8803968
Pratik Prakash <pratikprakash1@yahoo.com>
2015-04-02 17:30:39 -0400

    Generalized url code to take in the general tile information

--- iOS/G3MApp/G3MApp/G3MOSMBuildingsDemoScene.hpp
+++ iOS/G3MApp/G3MApp/G3MOSMBuildingsDemoScene.hpp
@@ -11,12 +11,28 @@
 
 #include <stdio.h>
 #include "G3MDemoScene.hpp"
+#include "G3MiOSSDK/Tile.hpp"
 
 class Mark;
 
 class G3MOSMBuildingsDemoScene : public G3MDemoScene {
 private:
     long long _requestId;
+    std::string url = "http://data.osmbuildings.org/0.2/%s/tile/%d/%d/%d.json";
+    const std::string dataKey = "rkc8ywdl";
+    
+    //Hardcoding level, row and col. **TODO Later we will get this from the tile
+    int level = 14;
+    int row = 4825;
+    int col = 6156;
+    
+    //Geodetic2D* get2DCoordsFromTile(int xIndex, int yIndex, int zoom); **TODO: Won't implement this until we find a use for it
+    std::string getURLFromTile(int xIndex, int yIndex, int zoom);
+    
+    //Gets the Tile's row and column given a latitude or longitude in degrees as well as a zoom level.
+    int getTileRowFrom2DCoords(double lon, int zoom);
+    int getTileColFrom2DCoords(double lat, int zoom);
+    
     
 protected:
     void rawActivate(const G3MContext* context);
@@ -33,8 +49,6 @@ public:
     }
     
     void deactivate(const G3MContext* context);
-    
-    
     void addMark(Mark* mark);
     
 };
