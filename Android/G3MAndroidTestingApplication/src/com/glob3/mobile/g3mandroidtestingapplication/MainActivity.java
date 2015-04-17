aa03d20be806dabb69bc520a03acefbc4ced128e
amazingsmash <josemiguelsn@MacBook-Pro-de-Jose-4.local>
2015-04-17 13:26:50 +0100

    ...

--- Android/G3MAndroidTestingApplication/src/com/glob3/mobile/g3mandroidtestingapplication/MainActivity.java
+++ Android/G3MAndroidTestingApplication/src/com/glob3/mobile/g3mandroidtestingapplication/MainActivity.java
@@ -3,17 +3,20 @@
 package com.glob3.mobile.g3mandroidtestingapplication;
 
 import org.glob3.mobile.generated.AltitudeMode;
+import org.glob3.mobile.generated.Angle;
 import org.glob3.mobile.generated.Color;
 import org.glob3.mobile.generated.ColumnLayoutImageBuilder;
 import org.glob3.mobile.generated.DownloaderImageBuilder;
 import org.glob3.mobile.generated.G3MContext;
 import org.glob3.mobile.generated.GFont;
 import org.glob3.mobile.generated.GInitializationTask;
+import org.glob3.mobile.generated.Geodetic2D;
 import org.glob3.mobile.generated.Geodetic3D;
 import org.glob3.mobile.generated.ICanvas;
 import org.glob3.mobile.generated.IImage;
 import org.glob3.mobile.generated.IImageDownloadListener;
 import org.glob3.mobile.generated.IImageListener;
+import org.glob3.mobile.generated.ILogger;
 import org.glob3.mobile.generated.LabelImageBuilder;
 import org.glob3.mobile.generated.LayerSet;
 import org.glob3.mobile.generated.NonOverlappingMark;
@@ -21,6 +24,8 @@ import org.glob3.mobile.generated.NonOverlappingMarkTouchListener;
 import org.glob3.mobile.generated.NonOverlappingMarksRenderer;
 import org.glob3.mobile.generated.OSMLayer;
 import org.glob3.mobile.generated.QuadShape;
+import org.glob3.mobile.generated.SGShape;
+import org.glob3.mobile.generated.ShapeLoadListener;
 import org.glob3.mobile.generated.ShapesRenderer;
 import org.glob3.mobile.generated.TimeInterval;
 import org.glob3.mobile.generated.URL;
@@ -42,6 +47,10 @@ public class MainActivity
    private G3MWidget_Android _g3mWidget;
 
 
+   Geodetic2D _merida = Geodetic2D.fromDegrees(38.915736, -6.3571647);
+   Geodetic2D _museoMerida = Geodetic2D.fromDegrees(38.917146, -6.339565);
+   
+
    @Override
    protected void onCreate(final Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
@@ -57,193 +66,72 @@ public class MainActivity
 
       placeHolder.addView(_g3mWidget);
 
-      _g3mWidget.setAnimatedCameraPosition(Geodetic3D.fromDegrees(28.034468668529083146, -15.904092315837871752, 1634079));
-
-      //      // Buenos Aires, there we go!
-      //      _g3mWidget.setAnimatedCameraPosition(Geodetic3D.fromDegrees(-34.615047738942699596, -58.4447233540403559, 35000));
+      
+      _g3mWidget.setAnimatedCameraPosition(new Geodetic3D(_merida, 6000.0), 
+      										TimeInterval.fromSeconds(15.0));
+      
+      // Camera position=(lat=38.908400780766385d, lon=-6.345933272891653d, height=664.4377440058383)
+      
    }
-
-
-   private static NonOverlappingMark createMark(final Geodetic3D position) {
-      final URL markBitmapURL = new URL("file:///g3m-marker.png");
-      final URL anchorBitmapURL = new URL("file:///anchorWidget.png");
-
-      return new NonOverlappingMark( //
-               new DownloaderImageBuilder(markBitmapURL), //
-               new DownloaderImageBuilder(anchorBitmapURL), //
-               position);
-   }
-
-
-   private static NonOverlappingMark createMark(final String label,
-                                                final Geodetic3D position) {
-      final URL markBitmapURL = new URL("file:///g3m-marker.png");
-      final URL anchorBitmapURL = new URL("file:///anchorWidget.png");
-
-      final ColumnLayoutImageBuilder imageBuilderWidget = new ColumnLayoutImageBuilder( //
-               new DownloaderImageBuilder(markBitmapURL), //
-               new LabelImageBuilder(label, GFont.monospaced()) //
-      );
-
-      return new NonOverlappingMark( //
-               imageBuilderWidget, //
-               new DownloaderImageBuilder(anchorBitmapURL), //
-               position);
-   }
-
 
    private G3MWidget_Android createWidget() {
       final G3MBuilder_Android builder = new G3MBuilder_Android(this);
+      
 
       final LayerSet layerSet = new LayerSet();
       layerSet.addLayer(new OSMLayer(TimeInterval.fromDays(30)));
       builder.getPlanetRendererBuilder().setLayerSet(layerSet);
+      
+      ShapesRenderer sr = new ShapesRenderer();
+      
+      sr.loadJSONSceneJS(new URL(URL.FILE_PROTOCOL + "models/1/1.json"),
+              1000,
+              TimeInterval.forever(),
+              true,
+              URL.FILE_PROTOCOL + "models/1/",
+              false,
+              new Geodetic3D(_museoMerida, 100.0),
+              AltitudeMode.ABSOLUTE,
+              new ShapeLoadListener(){
 
+				@Override
+				public void dispose() {
+					// TODO Auto-generated method stub
+					
+				}
 
-      final NonOverlappingMarksRenderer renderer = new NonOverlappingMarksRenderer(30);
-      builder.addRenderer(renderer);
+				@Override
+				public void onBeforeAddShape(SGShape shape) {
+					// TODO Auto-generated method stub
+					shape.setScale(10);
+					shape.setPitch(Angle.fromDegrees(90));
+					ILogger.instance().logError("MODEL ADDED");
+				}
 
-      renderer.setTouchListener(new NonOverlappingMarkTouchListener() {
-         @Override
-         public boolean touchedMark(final NonOverlappingMark mark,
-                                    final Vector2F touchedPixel) {
-            System.out.println("Touched on pixel=" + touchedPixel + ", mark=" + mark);
-            return true;
-         }
-      });
-
-      renderer.addMark(createMark("Label #1", Geodetic3D.fromDegrees(28.131817, -15.440219, 0)));
-      renderer.addMark(createMark(Geodetic3D.fromDegrees(28.947345, -13.523105, 0)));
-      renderer.addMark(createMark(Geodetic3D.fromDegrees(28.473802, -13.859360, 0)));
-      renderer.addMark(createMark(Geodetic3D.fromDegrees(28.467706, -16.251426, 0)));
-      renderer.addMark(createMark(Geodetic3D.fromDegrees(28.701819, -17.762003, 0)));
-      renderer.addMark(createMark(Geodetic3D.fromDegrees(28.086595, -17.105796, 0)));
-      renderer.addMark(createMark(Geodetic3D.fromDegrees(27.810709, -17.917639, 0)));
-
-
-      final boolean testCanvas = false;
-      if (testCanvas) {
-         final ShapesRenderer shapesRenderer = new ShapesRenderer();
-         builder.addRenderer(shapesRenderer);
-
-
-         builder.setInitializationTask(new GInitializationTask() {
-            @Override
-            public void run(final G3MContext context) {
-
-
-               final IImageDownloadListener listener = new IImageDownloadListener() {
-                  @Override
-                  public void onError(final URL url) {
-                  }
-
-
-                  @Override
-                  public void onDownload(final URL url,
-                                         final IImage image,
-                                         final boolean expired) {
-
-                     final ICanvas canvas = context.getFactory().createCanvas();
-                     final int width = 1024;
-                     final int height = 1024;
-                     canvas.initialize(width, height);
-
-                     canvas.setFillColor(Color.fromRGBA(1f, 1f, 0f, 0.5f));
-                     canvas.fillRectangle(0, 0, width, height);
-                     canvas.setLineWidth(4);
-                     canvas.setLineColor(Color.black());
-                     canvas.strokeRectangle(0, 0, width, height);
-
-                     final int steps = 8;
-                     final float leftStep = (float) width / steps;
-                     final float topStep = (float) height / steps;
-
-                     canvas.setLineWidth(1);
-                     canvas.setFillColor(Color.fromRGBA(0f, 0f, 0f, 0.75f));
-                     for (int i = 1; i < steps; i++) {
-                        canvas.fillRectangle(0, topStep * i, width, 1);
-                        canvas.fillRectangle(leftStep * i, 0, 1, height);
-                     }
-
-                     canvas.setFont(GFont.monospaced());
-                     canvas.setFillColor(Color.black());
-                     //                  canvas.fillText("0,0", 0, 0);
-                     //                  canvas.fillText("w,h", width, height);
-                     for (int i = 0; i < steps; i++) {
-                        canvas.fillText("Hellow World", leftStep * i, topStep * i);
-                     }
-
-                     //                  canvas.drawImage(image, width / 4, height / 4); // ok
-
-                     canvas.drawImage(image, width / 8, height / 8); // ok
-                     canvas.drawImage(image, (width / 8) * 3, height / 8, 0.5f); // ok
-
-                     canvas.drawImage(image, width / 8, (height / 8) * 3, image.getWidth() * 2, image.getHeight() * 2); // ok
-                     canvas.drawImage(image, (width / 8) * 3, (height / 8) * 3, image.getWidth() * 2, image.getHeight() * 2, 0.5f); //ok
-
-                     // ok
-                     canvas.drawImage(image, //
-                              0, 0, image.getWidth(), image.getHeight(), //
-                              (width / 8) * 5, (height / 8) * 5, image.getWidth() * 2, image.getHeight() * 2);
-                     // ok
-                     canvas.drawImage(image, //
-                              0, 0, image.getWidth(), image.getHeight(), //
-                              (width / 8) * 7, (height / 8) * 7, image.getWidth() * 2, image.getHeight() * 2, //
-                              0.5f);
-
-
-                     canvas.createImage(new IImageListener() {
-                        @Override
-                        public void imageCreated(final IImage canvasImage) {
-                           final QuadShape quad = new QuadShape( //
-                                    Geodetic3D.fromDegrees(-34.615047738942699596, -58.4447233540403559, 1000), //
-                                    AltitudeMode.ABSOLUTE, //
-                                    canvasImage, //
-                                    canvasImage.getWidth() * 15.0f, //
-                                    canvasImage.getHeight() * 15.0f, //
-                                    true);
-
-                           shapesRenderer.addShape(quad);
-                        }
-                     }, true);
-
-                     canvas.dispose();
-
-                     image.dispose();
-                  }
-
-
-                  @Override
-                  public void onCanceledDownload(final URL url,
-                                                 final IImage image,
-                                                 final boolean expired) {
-                  }
-
-
-                  @Override
-                  public void onCancel(final URL url) {
-                  }
-               };
-
-
-               context.getDownloader().requestImage( //
-                        new URL("file:///g3m-marker.png"), //
-                        1, // priority, //
-                        TimeInterval.zero(), //
-                        false, //
-                        listener, //
-                        true);
-            }
-
-
-            @Override
-            public boolean isDone(final G3MContext context) {
-               return true;
-            }
-         });
-      }
-
-
+				@Override
+				public void onAfterAddShape(SGShape shape) {
+				}
+    	  
+      },
+              true);
+      
+      builder.addRenderer(sr);
+      
+//      Shape plane = SceneJSShapesParser.parseFromJSON(planeJSON,
+//                                                        URL::FILE_PROTOCOL + "/" ,
+//                                                        false,
+//                                                        new Geodetic3D(Angle::fromDegrees(28.127222),
+//                                                                       Angle::fromDegrees(-15.431389),
+//                                                                       10000),
+//                                                        ABSOLUTE);
+//      
+//      // Washington, DC
+//      const double scale = 1000;
+//      plane->setScale(scale, scale, scale);
+//      plane->setPitch(Angle::fromDegrees(90));
+//      plane->setHeading(Angle::fromDegrees(0));
+//      _shapesRenderer->addShape(plane);
+      
       return builder.createWidget();
    }
 }
