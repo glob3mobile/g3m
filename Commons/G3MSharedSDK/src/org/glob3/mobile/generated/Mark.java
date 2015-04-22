1eef64616e30c2add817e29dc9c2ddce4d00347f
amazingsmash <josemiguelsn@MacBook-Pro-de-Jose-4.local>
2015-04-22 13:23:21 +0100

    ....

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Mark.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/Mark.java
@@ -111,17 +111,7 @@ public class Mark implements SurfaceElevationListener
       }
       else
       {
-  //      _textureGLF = new TextureGLFeature(_textureId->getID(),
-  //                                         billboardTexCoords,
-  //                                         2,
-  //                                         0,
-  //                                         false,
-  //                                         0,
-  //                                         true,
-  //                                         GLBlendFactor::srcAlpha(),
-  //                                         GLBlendFactor::oneMinusSrcAlpha());
-  
-        _textureGLF = new TextureGLFeature(_textureId.getID(), billboardTexCoords, 2, 0, false, 0, true, GLBlendFactor.srcAlpha(), GLBlendFactor.oneMinusSrcAlpha(), 0.0, 0.0, 1.0, 1.0, 0.0f, 0.0f, 0.0f);
+        _textureGLF = new TextureGLFeature(_textureId.getID(), billboardTexCoords, 2, 0, false, 0, true, GLBlendFactor.srcAlpha(), GLBlendFactor.oneMinusSrcAlpha(), 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f);
       }
   
       _glState.addGLFeature(_textureGLF, false);
