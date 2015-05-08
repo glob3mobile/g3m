ce8cc112e3722c493ebea0f76fc76ed96f64e309
aldeanueva <aldeanueva@igosoftware.es>
2015-05-08 11:05:22 +0200

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/SphericalPlanet.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/SphericalPlanet.java
@@ -618,7 +618,7 @@ public class SphericalPlanet extends Planet
         minFactor = factor;
     }
   
-    //printf("Converge doble drag %d iteraciones con factor=%.3f\n", iter, factor);
+    System.out.printf("Converge doble drag %d iteraciones con factor=%.3f\n", iter, factor);
   
     _prevFactor = factor;
     return matrix;
@@ -863,7 +863,7 @@ public class SphericalPlanet extends Planet
   
         //angleInRadians = a0.distanceTo(last)._radians < a1.distanceTo(last)._radians? a0._radians : a1._radians;
   
-        if (ao.distanceTo(last)._radians < a1.distanceTo(last)._radians)
+        if (a0.distanceTo(last)._radians < a1.distanceTo(last)._radians)
           angleInRadians = a0._radians;
         else
           angleInRadians = a1._radians;
