2b364fdbb7aa68796b33d78ca2dc011c90065892
amazingsmash <josemiguelsn@MacBook-Pro-de-Jose-4.local>
2015-05-09 13:28:46 +0100

    ...

---
+++ WebGL/G3MWebGLSDK/src/org/glob3/mobile/specific/DeviceAttitude_WebGL.java
@@ -0,0 +1,39 @@
+package org.glob3.mobile.specific;
+
+import org.glob3.mobile.generated.IDeviceAttitude;
+import org.glob3.mobile.generated.InterfaceOrientation;
+import org.glob3.mobile.generated.MutableMatrix44D;
+
+public class DeviceAttitude_WebGL extends IDeviceAttitude {
+
+	@Override
+	public void startTrackingDeviceOrientation() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopTrackingDeviceOrientation() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isTracking() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void copyValueOfRotationMatrix(MutableMatrix44D rotationMatrix) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public InterfaceOrientation getCurrentInterfaceOrientation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
