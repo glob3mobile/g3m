47c1dc4e0a57fb01ad7c753f732aa45e8f5ec17c
aldeanueva <aldeanueva@igosoftware.es>
2015-10-28 12:32:18 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/DeviceAttitudeCameraHandler.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/DeviceAttitudeCameraHandler.java
@@ -7,8 +7,6 @@ public class DeviceAttitudeCameraHandler extends CameraEventHandler
   private MutableMatrix44D _camRM = new MutableMatrix44D();
 
   private boolean _updateLocation;
-  private long _lastLocationUpdateTimeInMS;
-  private ITimer _timer;
 
   private ILocationModifier _locationModifier;
 
@@ -21,8 +19,6 @@ public class DeviceAttitudeCameraHandler extends CameraEventHandler
   {
      _updateLocation = updateLocation;
      _locationModifier = locationModifier;
-     _lastLocationUpdateTimeInMS = 0;
-     _timer = null;
   
   }
 
@@ -31,8 +27,6 @@ public class DeviceAttitudeCameraHandler extends CameraEventHandler
     IDeviceAttitude.instance().stopTrackingDeviceOrientation();
     IDeviceLocation.instance().stopTrackingLocation();
   
-    if (_timer != null)
-       _timer.dispose();
     if (_locationModifier != null)
        _locationModifier.dispose();
   }
@@ -86,54 +80,40 @@ public class DeviceAttitudeCameraHandler extends CameraEventHandler
     if (_updateLocation)
     {
   
-      if (_timer == null)
+      IDeviceLocation loc = IDeviceLocation.instance();
+      if (!loc.isTrackingLocation())
       {
-        _timer = IFactory.instance().createTimer();
+        loc.startTrackingLocation();
       }
   
-      long t = _timer.nowInMilliseconds();
-  
-      if ((t - _lastLocationUpdateTimeInMS > 5000) || (_lastLocationUpdateTimeInMS == 0))
+      Geodetic3D g = loc.getLocation();
+      if (!g.isNan())
       {
-  
-        IDeviceLocation loc = IDeviceLocation.instance();
-        if (!loc.isTrackingLocation())
+        //Changing current location
+        double lat = g._latitude._degrees;
+        double lon = g._longitude._degrees;
+        double height = g._height;
+        if (_locationModifier != null)
         {
-          loc.startTrackingLocation();
+          Geodetic3D g2 = _locationModifier.modify(g);
+          lat = g2._latitude._degrees;
+          lon = g2._latitude._degrees;
+          height = g2._height;
         }
   
-        Geodetic3D g = loc.getLocation();
-        if (!g.isNan())
+        Geodetic3D modG = Geodetic3D.fromDegrees(lat, lon, height);
+  
+        if (nextCamera.hasValidViewDirection())
         {
-          _lastLocationUpdateTimeInMS = t;
-  
-  
-          //Changing current location
-          double lat = g._latitude._degrees;
-          double lon = g._longitude._degrees;
-          double height = g._height;
-          if (_locationModifier != null)
-          {
-            Geodetic3D g2 = _locationModifier.modify(g);
-            lat = g2._latitude._degrees;
-            lon = g2._latitude._degrees;
-            height = g2._height;
-          }
-  
-          Geodetic3D modG = Geodetic3D.fromDegrees(lat, lon, height);
-  
-          if (nextCamera.hasValidViewDirection())
-          {
-            nextCamera.setGeodeticPosition(Geodetic3D.fromDegrees(modG._latitude._degrees, modG._longitude._degrees, modG._height));
-          }
-          else
-          {
-            ILogger.instance().logWarning("Trying to set position of unvalid camera. ViewDirection: %s", nextCamera.getViewDirection().description());
-          }
+          nextCamera.setGeodeticPosition(Geodetic3D.fromDegrees(modG._latitude._degrees, modG._longitude._degrees, modG._height));
+        }
+        else
+        {
+          ILogger.instance().logWarning("Trying to set position of unvalid camera. ViewDirection: %s", nextCamera.getViewDirection().description());
         }
       }
-  
     }
+  
   }
 
   public boolean onTouchEvent(G3MEventContext eventContext, TouchEvent touchEvent, CameraContext cameraContext)
