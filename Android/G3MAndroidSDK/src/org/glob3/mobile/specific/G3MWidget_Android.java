5289689c9907b4076cd8c3925ffb96cb8e41c3fc
amazingsmash <josemiguelsn@MacBook-Pro-de-Jose-3.local>
2015-10-28 10:43:43 +0000

    DeviceLocation_Android adjusted poll time

--- Android/G3MAndroidSDK/src/org/glob3/mobile/specific/G3MWidget_Android.java
+++ Android/G3MAndroidSDK/src/org/glob3/mobile/specific/G3MWidget_Android.java
@@ -170,7 +170,7 @@ OnGestureListener {
       final IJSONParser jsonParser = new JSONParser_Android();
       final ITextUtils textUtils = new TextUtils_Android();
       final IDeviceAttitude devAttitude = new DeviceAttitude_Android(getContext());
-      final IDeviceLocation devLoc = new DeviceLocation_Android(getContext(), (long) 10000.0, 1.0f);
+      final IDeviceLocation devLoc = new DeviceLocation_Android(getContext(), (long) 500.0, 0.5f);
 
       G3MWidget.initSingletons(logger, factory, stringUtils, stringBuilder, mathUtils, jsonParser, textUtils, devAttitude, devLoc);
 
