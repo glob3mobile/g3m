46ee79ff7b6060bdcf9da80d3c2d24ddb5071794
amazingsmash <josemiguelsn@MacBook-Pro-de-Jose-3.local>
2015-11-13 14:15:31 +0000

    starting

---
+++ iOS/G3MiOSSDK/IDeviceAttitude.hpp
@@ -0,0 +1,72 @@
+//
+//  IDeviceAttitude.h
+//  G3MiOSDemo
+//
+//  Created by Jose Miguel SN on 30/4/15.
+//
+//
+
+#ifndef __G3MiOSDemo__IDeviceAttitude__
+#define __G3MiOSDemo__IDeviceAttitude__
+
+#include <stdio.h>
+
+#include "MutableMatrix44D.hpp"
+#include "ICameraConstrainer.hpp"
+#include "G3MWidget.hpp"
+#include "CoordinateSystem.hpp"
+
+enum InterfaceOrientation{
+  PORTRAIT,
+  PORTRAIT_UPSIDEDOWN,
+  LANDSCAPE_RIGHT,
+  LANDSCAPE_LEFT
+};
+
+class IDeviceAttitude{
+private:
+  static IDeviceAttitude* _instance;
+protected:
+  
+  mutable CoordinateSystem* _camCSPortrait;
+  
+  mutable CoordinateSystem* _camCSPortraitUD;
+  
+  mutable CoordinateSystem* _camCSLL;
+  
+  mutable CoordinateSystem* _camCSLR;
+public:
+  
+  IDeviceAttitude();
+  
+  static void setInstance(IDeviceAttitude* deviceAttitude);
+  
+  //Singleton
+  static IDeviceAttitude* instance();
+  
+  virtual ~IDeviceAttitude();
+  
+  /**
+   Must be called before any other operation
+   **/
+  
+  virtual void startTrackingDeviceOrientation() const = 0;
+  
+  /**
+   Must be called to stop operations
+   **/
+  
+  virtual void stopTrackingDeviceOrientation() const = 0;
+  
+  virtual bool isTracking() const = 0;
+
+  virtual void copyValueOfRotationMatrix(MutableMatrix44D& rotationMatrix) const = 0;
+  
+  virtual InterfaceOrientation getCurrentInterfaceOrientation() const = 0;
+  
+  CoordinateSystem getCameraCoordinateSystemForInterfaceOrientation(InterfaceOrientation orientation) const;
+  
+};
+
+
+#endif /* defined(__G3MiOSDemo__IDeviceAttitude__) */
