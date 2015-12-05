4d26b8b27542217dc989f04429199ac2a0f1e118
Diego Gomez Deck <dgd@consultar.com>
2015-12-05 11:00:11 -0500

    -

---
+++ tools/tiling-tools/src/com/glob3mobile/tools/tiling/Tiler.java
@@ -0,0 +1,18 @@
+
+
+package com.glob3mobile.tools.tiling;
+
+
+public class Tiler {
+
+
+   public static void main(final String[] args) {
+      System.out.println("Tiler 0.1");
+      System.out.println("---------\n");
+
+
+      System.out.println("\n- done !");
+   }
+
+
+}
