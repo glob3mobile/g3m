fb5d1621aabfff0f61612b771fcdf6414c312a8b
Diego Gomez Deck <dgd@consultar.com>
2016-02-04 15:43:55 -0500

    -

---
+++ iOS/G3MiOSSDK/Commons/Rendererers/PlanetRenderContext.hpp
@@ -0,0 +1,16 @@
+//
+//  PlanetRenderContext.hpp
+//  G3MiOSSDK
+//
+//  Created by Diego Gomez Deck on 2/4/16.
+//
+//
+
+#ifndef PlanetRenderContext_hpp
+#define PlanetRenderContext_hpp
+
+class PlanetRenderContext {
+
+};
+
+#endif
