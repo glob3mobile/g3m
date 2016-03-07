06dcf7cd58ce4b2579d088273c337080768856d6
aldeanueva <aldeanueva@igosoftware.es>
2016-03-07 10:51:16 +0100

    Generated

--- Commons/G3MSharedSDK/src/org/glob3/mobile/generated/PyramidElevationDataProvider_BufferDownloadListener.java
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/PyramidElevationDataProvider_BufferDownloadListener.java
@@ -29,13 +29,13 @@ public class PyramidElevationDataProvider_BufferDownloadListener extends IBuffer
 //    PyramidElevationDataProvider_BufferDownloadListener(Sector sector, Vector2I extent, IElevationDataListener listener, boolean autodeleteListener, double deltaHeight);
 
 //C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
-//    void onDownload(URL url, IByteBuffer buffer, boolean expired);
+//    virtual void onDownload(URL url, IByteBuffer buffer, boolean expired);
 //C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
-//    void onError(URL url);
+//    virtual void onError(URL url);
 //C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
-//    void onCancel(URL url);
+//    virtual void onCancel(URL url);
 
 //C++ TO JAVA CONVERTER TODO TASK: The implementation of the following method could not be found:
-//    void onCanceledDownload(URL url, IByteBuffer data, boolean expired);
+//    virtual void onCanceledDownload(URL url, IByteBuffer data, boolean expired);
 
 }
