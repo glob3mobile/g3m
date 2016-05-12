eaeb098d667535e6b957363606c443b7bbdf2507
premeczmatyas <matyas@glass.aero>
2016-05-12 14:33:57 +0200

    aeroglass-pre-render-hook: modified renderer to handle pre and post render tasks through IPrePostTasks interface

---
+++ iOS/G3MiOSSDK/Commons/Basic/IPrePostTasks.cpp
@@ -0,0 +1,9 @@
+//
+//  IPrePostTasks.cpp
+//  G3MiOSSDK
+//
+//  Created by Premecz Matyas on 12/05/16.
+//
+//
+
+#include "IPrePostTasks.hpp"
---
+++ iOS/G3MiOSSDK/Commons/Basic/IPrePostTasks.hpp
@@ -0,0 +1,14 @@
+//
+//  IPrePostTasks.hpp
+//  G3MiOSSDK
+//
+//  Created by Premecz Matyas on 12/05/16.
+//
+//
+
+#ifndef IPrePostTasks_hpp
+#define IPrePostTasks_hpp
+
+#include <stdio.h>
+
+#endif /* IPrePostTasks_hpp */
