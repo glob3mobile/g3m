0f338a67816ab5dbce3f3b897c39b9f93dd6d9c7
premeczmatyas <matyas@glass.aero>
2016-05-28 22:16:50 +0200

    aeroglass-pre-render-hook: webGL rename fix

--- WebGL/G3MWebGLSDK/src/org/glob3/mobile/specific/G3MWidget_WebGL.java
+++ WebGL/G3MWebGLSDK/src/org/glob3/mobile/specific/G3MWidget_WebGL.java
@@ -25,7 +25,7 @@ import org.glob3.mobile.generated.IJSONParser;
 import org.glob3.mobile.generated.ILogger;
 import org.glob3.mobile.generated.IMathUtils;
 import org.glob3.mobile.generated.INativeGL;
-import org.glob3.mobile.generated.IPrePostTasks;
+import org.glob3.mobile.generated.IPrePostRenderTasks;
 import org.glob3.mobile.generated.IStorage;
 import org.glob3.mobile.generated.IStringBuilder;
 import org.glob3.mobile.generated.IStringUtils;
@@ -367,7 +367,7 @@ public class G3MWidget_WebGL
                           final java.util.ArrayList<ICameraConstrainer> cameraConstrainers,
                           final CameraRenderer cameraRenderer,
                           final Renderer mainRenderer,
-                          final IPrePostTasks prePostTasks,
+                          final IPrePostRenderTasks prePostTasks,
                           final ProtoRenderer busyRenderer,
                           final ErrorRenderer errorRenderer,
                           final Renderer hudRenderer,
