aae60929082d947af3ad58ad3e90caa044f4c2a6
Diego Gomez Deck <dgd@consultar.com>
2016-07-26 14:40:35 +0200

    minor cleanups in Android DeviceLocation implementation

--- Android/G3MAndroidSDK/src/org/glob3/mobile/specific/DeviceLocation_Android.java
+++ Android/G3MAndroidSDK/src/org/glob3/mobile/specific/DeviceLocation_Android.java
@@ -1,3 +1,5 @@
+
+
 package org.glob3.mobile.specific;
 
 import org.glob3.mobile.generated.Geodetic3D;
@@ -11,103 +13,122 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
 
-public class DeviceLocation_Android extends IDeviceLocation{
-	
-	private class DeviceLocation_AndroidListener implements LocationListener{
-		public Location _lastLocation = null;
 
-		@Override
-		public void onLocationChanged(Location location) {
-			_lastLocation = location;
-		}
+public class DeviceLocation_Android
+   extends
+      IDeviceLocation {
 
-		@Override
-		public void onProviderDisabled(String provider) {
-			_lastLocation = null;
-			ILogger.instance().logInfo("Location Provider " + provider + " has been disabled.");
-		}
+   private class DeviceLocation_AndroidListener
+      implements
+         LocationListener {
+      public Location _lastLocation = null;
 
-		@Override
-		public void onProviderEnabled(String provider) {
-			ILogger.instance().logInfo("Location Provider " + provider + " has been enabled.");
-		}
 
-		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
-			ILogger.instance().logInfo("Location Provider " + provider + " set status " + status);
-		}
-		
-	}
-	
-	DeviceLocation_AndroidListener _netListener = new DeviceLocation_AndroidListener();
-	DeviceLocation_AndroidListener _gpsListener = new DeviceLocation_AndroidListener();
-	
-	LocationManager _locationManager = null;
-	Context _ctx = null;
-	
-	long _minTime = (long) 500.0; //Seconds between updates (ms.)
-	float _minDistance = (float) 0.5; //Min meters between updates
-	
-	boolean _isTracking = false;
-	
-	DeviceLocation_Android(Context ctx, long minTime, float minDistance){
-		_ctx = ctx;
-		_minTime = minTime;
-		_minDistance = minDistance;
-	}
+      @Override
+      public void onLocationChanged(final Location location) {
+         _lastLocation = location;
+      }
 
-	@Override
-	public boolean startTrackingLocation() {
-		
-		_locationManager = (LocationManager) _ctx.getSystemService(Context.LOCATION_SERVICE);
-		
-		boolean gpsActive = _locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-		boolean netActive = _locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-		
-		if (gpsActive) {
-			_locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, _minTime, 0, _gpsListener, Looper.getMainLooper());
-			_isTracking = true;
-		}
-		
-		if (netActive){
-			_locationManager.requestLocationUpdates( LocationManager.NETWORK_PROVIDER, _minTime, 0, _netListener, Looper.getMainLooper());
-			_isTracking = true;	
-		}
-		
-		if (!netActive && !gpsActive){
-			_isTracking = false;
-		}
-		return _isTracking;
-	}
 
-	@Override
-	public void stopTrackingLocation() {
-		if (_isTracking){
-			_locationManager.removeUpdates(_gpsListener);
-			_locationManager.removeUpdates(_netListener);
-			_isTracking = false;
-		}
-	}
+      @Override
+      public void onProviderDisabled(final String provider) {
+         _lastLocation = null;
+         ILogger.instance().logInfo("Location Provider " + provider + " has been disabled.");
+      }
 
-	@Override
-	public boolean isTrackingLocation() {
-		return _isTracking;// && (_gpsListener._lastLocation != null || _netListener._lastLocation != null);
-	}
 
-	@Override
-	public Geodetic3D getLocation() {
-		
-		Location l = _gpsListener._lastLocation;
-		if (l == null){
-			l = _netListener._lastLocation;
-		}
-		
-		if (_isTracking && l != null){
-			return Geodetic3D.fromDegrees(l.getLatitude(),
-										  l.getLongitude(),
-										  l.getAltitude());
-		}
-		return Geodetic3D.nan();
-	}
+      @Override
+      public void onProviderEnabled(final String provider) {
+         ILogger.instance().logInfo("Location Provider " + provider + " has been enabled.");
+      }
+
+
+      @Override
+      public void onStatusChanged(final String provider,
+                                  final int status,
+                                  final Bundle extras) {
+         ILogger.instance().logInfo("Location Provider " + provider + " set status " + status);
+      }
+
+   }
+
+   private final DeviceLocation_AndroidListener _netListener = new DeviceLocation_AndroidListener();
+   private final DeviceLocation_AndroidListener _gpsListener = new DeviceLocation_AndroidListener();
+
+   private LocationManager                      _locationManager;
+   private final Context                        _ctx;
+
+   private final long                           _minTime;                                           //Seconds between updates (ms.)
+   private final float                          _minDistance;                                       //Min meters between updates
+
+   private boolean                              _isTracking  = false;
+
+
+   DeviceLocation_Android(final Context ctx,
+                          final long minTime,
+                          final float minDistance) {
+      _ctx = ctx;
+      _minTime = minTime;
+      _minDistance = minDistance;
+   }
+
+
+   @Override
+   public boolean startTrackingLocation() {
+      _locationManager = (LocationManager) _ctx.getSystemService(Context.LOCATION_SERVICE);
+
+      final boolean gpsActive = _locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+      final boolean netActive = _locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+      if (gpsActive) {
+         _locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, _minTime, _minDistance, _gpsListener,
+                  Looper.getMainLooper());
+         _isTracking = true;
+      }
+
+      if (netActive) {
+         _locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, _minTime, _minDistance, _netListener,
+                  Looper.getMainLooper());
+         _isTracking = true;
+      }
+
+      if (!netActive && !gpsActive) {
+         _isTracking = false;
+      }
+
+      return _isTracking;
+   }
+
+
+   @Override
+   public void stopTrackingLocation() {
+      if (_isTracking) {
+         _locationManager.removeUpdates(_gpsListener);
+         _locationManager.removeUpdates(_netListener);
+         _isTracking = false;
+      }
+   }
+
+
+   @Override
+   public boolean isTrackingLocation() {
+      return _isTracking;// && (_gpsListener._lastLocation != null || _netListener._lastLocation != null);
+   }
+
+
+   @Override
+   public Geodetic3D getLocation() {
+
+      Location l = _gpsListener._lastLocation;
+      if (l == null) {
+         l = _netListener._lastLocation;
+      }
+
+      if (_isTracking && (l != null)) {
+         return Geodetic3D.fromDegrees(l.getLatitude(), l.getLongitude(), l.getAltitude());
+      }
+      return Geodetic3D.nan();
+   }
+
 
 }
--- Android/G3MAndroidSDK/src/org/glob3/mobile/specific/G3MWidget_Android.java
+++ Android/G3MAndroidSDK/src/org/glob3/mobile/specific/G3MWidget_Android.java
@@ -168,7 +168,7 @@ public final class G3MWidget_Android
       final IJSONParser jsonParser = new JSONParser_Android();
       final ITextUtils textUtils = new TextUtils_Android();
       final IDeviceAttitude devAttitude = new DeviceAttitude_Android(getContext());
-      final IDeviceLocation devLoc = new DeviceLocation_Android(getContext(), (long) 500.0, 0.5f);
+      final IDeviceLocation devLoc = new DeviceLocation_Android(getContext(), (long) 500.0, 0.0f);
 
       G3MWidget.initSingletons(logger, factory, stringUtils, stringBuilder, mathUtils, jsonParser, textUtils, devAttitude, devLoc);
    }
