66c7c842973c7cfc64e21fe615a5b1a6b5a27326
Diego Gomez Deck <dgd@consultar.com>
2016-10-19 12:14:02 +0200

    -

--- iOS/G3MiOSSDK/Commons/DEM/TerrainElevationProvider.hpp
+++ iOS/G3MiOSSDK/Commons/DEM/TerrainElevationProvider.hpp
@@ -13,6 +13,9 @@
 
 class RenderState;
 class G3MContext;
+class Sector;
+class Vector2I;
+class TerrainElevationGrid;
 
 
 class TerrainElevationProvider : public RCObject {
@@ -24,11 +27,33 @@ protected:
   }
 
 public:
+
+  class Listener {
+  public:
+#ifdef C_CODE
+    virtual ~Listener() { }
+#endif
+#ifdef JAVA_CODE
+    void dispose();
+#endif
+
+    virtual void onGrid(const TerrainElevationGrid* grid) = 0;
+
+  };
+
+
   virtual RenderState getRenderState() = 0;
 
   virtual void initialize(const G3MContext* context) = 0;
 
   virtual void cancel() = 0;
+
+  virtual long long subscribe(const Sector&   sector,
+                              const Vector2I& resolution,
+                              TerrainElevationProvider::Listener* listener,
+                              bool deleteListener) = 0;
+
+  virtual void unsubscribe(long long subscriptionID) = 0;
 
 };
 
