f41d426045194e551b9f74537d0316e7a74f2bd5
Diego Gomez Deck <dgd@consultar.com>
2016-11-23 12:44:11 +0100

    -

--- cpp2java.sh
+++ cpp2java.sh
@@ -3,9 +3,8 @@
 export PROJECT_NAME="G3M"
 export SOURCE_DIRECTORY="iOS/G3MiOSSDK/Commons/"
 export TARGET_DIRECTORY="Commons/G3MSharedSDK/src/org/glob3/mobile/generated"
-export SETTINGS="converter/C++ to Java Converter.dat"
+export SETTINGS="converter/settings.dat"
 export JAVA_PREPEND="package org.glob3.mobile.generated;\\
 "
 
 ../converter/cpp2java.sh
-
