f90aeb847acffe3aaa2d9792f1fc447627e0710f
Diego Gomez Deck <DiegoGomezDeck@users.noreply.github.com>
2016-11-23 12:31:44 +0100

    Update cpp2java.sh

--- cpp2java.sh
+++ cpp2java.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 
-echo - Converting C++ sources to Java...
-echo
+export PROJECT_NAME="G3M"
+export SOURCE_DIRECTORY="iOS/G3MiOSSDK/Commons/"
+export TARGET_DIRECTORY="Commons/G3MSharedSDK/src/org/glob3/mobile/generated"
+export SETTINGS="converter/C++ to Java Converter.dat"
+export JAVA_PREPEND="package org.glob3.mobile.generated;\\
+"
 
-rm -rf TEMP
-mkdir TEMP
-mkdir TEMP/CPP
-mkdir TEMP/JAVA
-
-echo - Gathering C++ sources...
-find iOS/G3MiOSSDK/Commons -iname "*.h"   -exec cp "{}" TEMP/CPP \;
-find iOS/G3MiOSSDK/Commons -iname "*.hpp" -exec cp "{}" TEMP/CPP \;
-find iOS/G3MiOSSDK/Commons -iname "*.cpp" -exec cp "{}" TEMP/CPP \;
-
-echo - Running converter...
-cp converter/C++\ to\ Java\ Converter.dat ~/.wine/drive_c/users/${USER}/Application\ Data/Tangible\ Software\ Solutions\ Inc/C++\ to\ Java\ Converter/
-wine ../converter/converter.exe TEMP\\CPP TEMP\\JAVA
-
-echo - Adding package to java sources...
-find TEMP/JAVA -iname "*.java" -exec sed -i.temp '1s/^/package org.glob3.mobile.generated;\
-/' "{}" \;
-rm TEMP/JAVA/*.temp
-
-echo - Merging generated Java sources...
-rm Commons/G3MSharedSDK/src/org/glob3/mobile/generated/*.java
-mv TEMP/JAVA/*.java Commons/G3MSharedSDK/src/org/glob3/mobile/generated/
-
-echo - Cleaning up...
-rm -rf TEMP
-
-echo - done!
-echo
+../converter/cpp2java.sh
