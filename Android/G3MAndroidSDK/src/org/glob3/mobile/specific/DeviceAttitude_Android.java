157933c3db7846bce310df680c9c4d2e73e788f9
Diego Gomez Deck <dgd@consultar.com>
2017-01-19 18:57:43 +0100

    -

--- Android/G3MAndroidSDK/src/org/glob3/mobile/specific/DeviceAttitude_Android.java
+++ Android/G3MAndroidSDK/src/org/glob3/mobile/specific/DeviceAttitude_Android.java
@@ -28,29 +28,32 @@ public class DeviceAttitude_Android
    implements
       SensorEventListener {
 
-   private final SensorManager _sensorManager;
-   private final Display       _defaultDisplay;
-   //   private final float         _lowPassFilterRatio;
 
-   //   private float[]             _magnetometerReading  = null;
-   //   private float[]             _accelerometerReading = null;
-
-   //   private final float[]       _rotationVectorReading = new float[3];
-   private final float[]       _rotationMatrix = new float[16];
-
-   private boolean             _tracking       = false;
+   private static final float      NS2S                          = 1.0f / 1000000000.0f;
+   private static final double     EPSILON                       = 0.1;
+   private static final float      OUTLIER_THRESHOLD             = 0.85f;
+   private static final float      OUTLIER_PANIC_THRESHOLD       = 0.75f;
+   private static final float      INDIRECT_INTERPOLATION_WEIGHT = 0.01f;
+   private static final int        PANIC_THRESHOLD               = 60;
 
 
-   //   public DeviceAttitude_Android(final Context context,
-   //                                 final float lowPassFilterRatio) {
-   //      _sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
-   //      _defaultDisplay = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-   //      _lowPassFilterRatio = lowPassFilterRatio;
-   //   }
-   //   public DeviceAttitude_Android(final Context context) {
-   //      // this(context, 0.09f); // for shitty devices
-   //      this(context, 0.1f);
-   //   }
+   private final SensorManager     _sensorManager;
+   private final Display           _defaultDisplay;
+   private boolean                 _tracking                     = false;
+   private final float[]           _rotationMatrix               = new float[16];
+
+   private final float[]           _tmpQuaternion                = new float[4];
+   private final MutableQuaternion _quaternionRotationVector     = new MutableQuaternion();
+   private boolean                 _positionInitialised          = false;
+   private final MutableQuaternion _quaternionGyroscope          = new MutableQuaternion();
+   private long                    _timestamp;
+   private final MutableQuaternion _deltaQuaternion              = new MutableQuaternion();
+   private int                     _panicCounter;
+   private final MutableQuaternion _interpolatedQuaternion       = new MutableQuaternion();
+   private final MutableQuaternion _correctedQuaternion          = new MutableQuaternion();
+   private final MutableQuaternion _currentOrientationQuaternion = new MutableQuaternion();
+   private final float[]           _tmpArray                     = new float[4];
+
 
    public DeviceAttitude_Android(final Context context) {
       _sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
@@ -64,18 +67,6 @@ public class DeviceAttitude_Android
          _tracking = true;
 
          _sensorManager.unregisterListener(this);
-
-         //         final Sensor accelerometerSensor = _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-         //         if (!_sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST)) {
-         //            ILogger.instance().logError("TYPE_ACCELEROMETER sensor not supported.");
-         //            _tracking = false;
-         //         }
-         //
-         //         final Sensor magneticSensor = _sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-         //         if (!_sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_FASTEST)) {
-         //            ILogger.instance().logError("TYPE_MAGNETIC_FIELD sensor not supported.");
-         //            // _tracking = false;
-         //         }
 
          final Sensor rotationVectorSensor = _sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
          if (!_sensorManager.registerListener(this, rotationVectorSensor, SensorManager.SENSOR_DELAY_FASTEST)) {
@@ -109,43 +100,16 @@ public class DeviceAttitude_Android
 
    @Override
    public void copyValueOfRotationMatrix(final MutableMatrix44D rotationMatrix) {
-      //      rotationMatrix.setValid(false);
-      //      if (_accelerometerReading != null) {
-      //         if (_magnetometerReading != null) {
-      //            if (SensorManager.getRotationMatrix(_rotationMatrix, null, _accelerometerReading, _magnetometerReading)) {
-      //               rotationMatrix.setValue( //
-      //                        _rotationMatrix[0], _rotationMatrix[4], _rotationMatrix[8], _rotationMatrix[12], //
-      //                        _rotationMatrix[1], _rotationMatrix[5], _rotationMatrix[9], _rotationMatrix[13], //
-      //                        _rotationMatrix[2], _rotationMatrix[6], _rotationMatrix[10], _rotationMatrix[14], //
-      //                        _rotationMatrix[3], _rotationMatrix[7], _rotationMatrix[11], _rotationMatrix[15]);
-      //            }
-      //         }
-      //         else {
-      //            final double gx = _accelerometerReading[0] / 9.81f;
-      //            final double gy = _accelerometerReading[1] / 9.81f;
-      //            final double gz = _accelerometerReading[2] / 9.81f;
-      //
-      //            // http://theccontinuum.com/2012/09/24/arduino-imu-pitch-roll-from-accelerometer/
-      //            final float pitch = (float) -Math.atan(gy / Math.sqrt((gx * gx) + (gz * gz)));
-      //            final float roll = (float) -Math.atan(gx / Math.sqrt((gy * gy) + (gz * gz)));
-      //            final float azimuth = 0; // Impossible to guess
-      //
-      //            final float[] mAccMagOrientation = { azimuth, pitch, roll };
-      //            if (SensorManager.getRotationMatrix(_rotationMatrix, null, _accelerometerReading, mAccMagOrientation)) {
-      //               rotationMatrix.setValue( //
-      //                        _rotationMatrix[0], _rotationMatrix[4], _rotationMatrix[8], _rotationMatrix[12], //
-      //                        _rotationMatrix[1], _rotationMatrix[5], _rotationMatrix[9], _rotationMatrix[13], //
-      //                        _rotationMatrix[2], _rotationMatrix[6], _rotationMatrix[10], _rotationMatrix[14], //
-      //                        _rotationMatrix[3], _rotationMatrix[7], _rotationMatrix[11], _rotationMatrix[15]);
-      //            }
-      //         }
-      //      }
-
-      rotationMatrix.setValue( //
-               _rotationMatrix[0], _rotationMatrix[4], _rotationMatrix[8], _rotationMatrix[12], //
-               _rotationMatrix[1], _rotationMatrix[5], _rotationMatrix[9], _rotationMatrix[13], //
-               _rotationMatrix[2], _rotationMatrix[6], _rotationMatrix[10], _rotationMatrix[14], //
-               _rotationMatrix[3], _rotationMatrix[7], _rotationMatrix[11], _rotationMatrix[15]);
+      if (_tracking) {
+         rotationMatrix.setValue( //
+                  _rotationMatrix[0], _rotationMatrix[4], _rotationMatrix[8], _rotationMatrix[12], //
+                  _rotationMatrix[1], _rotationMatrix[5], _rotationMatrix[9], _rotationMatrix[13], //
+                  _rotationMatrix[2], _rotationMatrix[6], _rotationMatrix[10], _rotationMatrix[14], //
+                  _rotationMatrix[3], _rotationMatrix[7], _rotationMatrix[11], _rotationMatrix[15]);
+      }
+      else {
+         rotationMatrix.setValid(false);
+      }
    }
 
 
@@ -173,82 +137,25 @@ public class DeviceAttitude_Android
    }
 
 
-   //   private static float[] lowPass(final float[] current,
-   //                                  final float[] previous,
-   //                                  final float lowPassFilterRatio) {
-   //      if ((previous == null) || (lowPassFilterRatio >= 1.0)) {
-   //         return current.clone();
-   //      }
-   //      final int length = current.length;
-   //      for (int i = 0; i < length; i++) {
-   //         previous[i] = previous[i] + (lowPassFilterRatio * (current[i] - previous[i]));
-   //         // previous[i] = (current[i] * lowPassFilterRatio) + (previous[i] * (1.0f - lowPassFilterRatio));
-   //      }
-   //      return previous;
-   //   }
-
-
    @Override
    public void onSensorChanged(final SensorEvent event) {
       switch (event.sensor.getType()) {
-      //         case Sensor.TYPE_ACCELEROMETER:
-      //            _accelerometerReading = lowPass(event.values, _accelerometerReading, _lowPassFilterRatio);
-      //            break;
-      //         case Sensor.TYPE_MAGNETIC_FIELD:
-      //            _magnetometerReading = lowPass(event.values, _magnetometerReading, _lowPassFilterRatio);
-      //            break;
-      //         case Sensor.TYPE_ROTATION_VECTOR:
-      //            SensorManager.getRotationMatrixFromVector(_rotationMatrix, event.values);
-      //            break;
          case Sensor.TYPE_ROTATION_VECTOR:
             processRotationVector(event);
             break;
          case Sensor.TYPE_GYROSCOPE:
             processGyroscope(event);
             break;
-      //         default:
-      //            return;
       }
 
 
    }
 
 
-   private static final float      NS2S                          = 1.0f / 1000000000.0f;
-   private static final double     EPSILON                       = 0.1f;
-   private static final float      OUTLIER_THRESHOLD             = 0.85f;
-   private static final float      OUTLIER_PANIC_THRESHOLD       = 0.75f;
-   private static final float      INDIRECT_INTERPOLATION_WEIGHT = 0.01f;
-   private static final int        PANIC_THRESHOLD               = 60;
-
-
-   private final float[]           _temporaryQuaternion          = new float[4];
-   private final MutableQuaternion _quaternionRotationVector     = new MutableQuaternion();
-   private final MutableQuaternion _quaternionGyroscope          = new MutableQuaternion();
-   private boolean                 _positionInitialised          = false;
-   private long                    _timestamp;
-   //private double              _gyroscopeRotationVelocity     = 0;
-   private final MutableQuaternion _deltaQuaternion              = new MutableQuaternion();
-   private int                     _panicCounter;
-   private final MutableQuaternion _interpolatedQuaternion       = new MutableQuaternion();
-
-   private final MutableQuaternion _correctedQuaternion          = new MutableQuaternion();
-
-   //   protected final float[]     _currentOrientationRotationMatrix = new float[16];
-   private final MutableQuaternion _currentOrientationQuaternion = new MutableQuaternion();
-   private final float[]           _tmpArray                     = new float[4];
-
-
    private void processRotationVector(final SensorEvent event) {
-      // Process rotation vector (just safe it)
-      // Calculate angle. Starting with API_18, Android will provide this value as event.values[3], but if not, we have to calculate it manually.
-      SensorManager.getQuaternionFromVector(_temporaryQuaternion, event.values);
-
-      // Store in quaternion
-      _quaternionRotationVector.setXYZW(_temporaryQuaternion[1], _temporaryQuaternion[2], _temporaryQuaternion[3],
-               -_temporaryQuaternion[0]);
+      SensorManager.getQuaternionFromVector(_tmpQuaternion, event.values);
+      _quaternionRotationVector.setXYZW(_tmpQuaternion[1], _tmpQuaternion[2], _tmpQuaternion[3], -_tmpQuaternion[0]);
       if (!_positionInitialised) {
-         // Override
          _quaternionGyroscope.copyFrom(_quaternionRotationVector);
          _positionInitialised = true;
       }
@@ -294,18 +201,17 @@ public class DeviceAttitude_Android
 
          // Calculate dot-product to calculate whether the two orientation sensors have diverged
          // (if the dot-product is closer to 0 than to 1), because it should be close to 1 if both are the same.
-         final float dotProd = _quaternionGyroscope.dot(_quaternionRotationVector);
+         final float dot = _quaternionGyroscope.dot(_quaternionRotationVector);
 
          // If they have diverged, rely on gyroscope only (this happens on some devices when the rotation vector "jumps").
-         if (Math.abs(dotProd) < OUTLIER_THRESHOLD) {
+         if (Math.abs(dot) < OUTLIER_THRESHOLD) {
             // Increase panic counter
-            if (Math.abs(dotProd) < OUTLIER_PANIC_THRESHOLD) {
+            if (Math.abs(dot) < OUTLIER_PANIC_THRESHOLD) {
                _panicCounter++;
             }
 
             // Directly use Gyro
             setOrientationQuaternionAndMatrix(_quaternionGyroscope);
-
          }
          else {
             // Both are nearly saying the same. Perform normal fusion.
@@ -367,5 +273,6 @@ public class DeviceAttitude_Android
       _tmpArray[3] = _correctedQuaternion.getW();
       SensorManager.getRotationMatrixFromVector(_rotationMatrix, _tmpArray);
    }
+
 
 }
