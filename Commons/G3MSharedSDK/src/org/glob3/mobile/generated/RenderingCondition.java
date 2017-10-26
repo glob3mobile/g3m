2af72208878e270be9906f27154db7858ae9aa35
amazingsmash <josemiguelsn@macbook-pro-de-jose-4.local>
2017-10-26 14:08:13 +0100

    ImageManager_Android

---
+++ Commons/G3MSharedSDK/src/org/glob3/mobile/generated/RenderingCondition.java
@@ -0,0 +1,26 @@
+package org.glob3.mobile.generated;
+//
+//  RenderingCondition.cpp
+//  CaceresCommon
+//
+//  Created by Jose Miguel SN on 25/10/2017.
+//  Copyright © 2017 IGO SOFTWARE S.L. All rights reserved.
+//
+
+//
+//  RenderingCondition.hpp
+//  CaceresCommon
+//
+//  Created by Jose Miguel SN on 25/10/2017.
+//  Copyright © 2017 IGO SOFTWARE S.L. All rights reserved.
+//
+
+
+
+public abstract class RenderingCondition
+{
+  public void dispose()
+  {
+  }
+  public abstract boolean mustRender(G3MRenderContext context);
+}
