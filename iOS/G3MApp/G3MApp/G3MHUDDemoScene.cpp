c218ad17b322bdff751da0a3b3d24aa498dbb778
Diego Gomez Deck <diego@consultar.com>
2018-08-09 15:10:55 +0200

    -

--- iOS/G3MApp/G3MApp/G3MHUDDemoScene.cpp
+++ iOS/G3MApp/G3MApp/G3MHUDDemoScene.cpp
@@ -78,7 +78,7 @@ protected:
   const std::string getImageName(const G3MContext* context) const {
     const IStringUtils* su = context->getStringUtils();
 
-    return "_AltimeterCanvasImage_" + su->toString(_width) + "_" + su->toString(_height) + "_" + su->toString(_altitude);
+    return "_AltimeterCanvasImage_" + getResolutionID(context) + "_" + su->toString(_altitude);
   }
 
 
@@ -188,7 +188,7 @@ void G3MHUDDemoScene::rawActivate(const G3MContext *context) {
   HUDQuadWidget* test = new HUDQuadWidget(altimeterCanvasImageBuilder,
                                           new HUDRelativePosition(0,
                                                                   HUDRelativePosition::VIEWPORT_WIDTH,
-                                                                  HUDRelativePosition::RIGHT,
+                                                                  HUDRelativePosition::LEFT,
                                                                   10),
                                           new HUDRelativePosition(0.5,
                                                                   HUDRelativePosition::VIEWPORT_HEIGHT,
@@ -241,7 +241,7 @@ void G3MHUDDemoScene::rawActivate(const G3MContext *context) {
   HUDQuadWidget* ruler = new HUDQuadWidget(new DownloaderImageBuilder(URL("file:///altimeter-ruler-1536x113.png")),
                                            new HUDRelativePosition(1,
                                                                    HUDRelativePosition::VIEWPORT_WIDTH,
-                                                                   HUDRelativePosition::LEFT,
+                                                                   HUDRelativePosition::RIGHT,
                                                                    10),
                                            new HUDRelativePosition(0.5,
                                                                    HUDRelativePosition::VIEWPORT_HEIGHT,
