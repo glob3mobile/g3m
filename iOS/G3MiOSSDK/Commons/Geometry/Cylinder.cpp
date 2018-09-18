07c9547626867c81cc03c2d29628b9e12277f081
amazingsmash <amazingsmash@gmail.com>
2018-09-18 11:37:11 +0100

    UtilityNetworkRenderer

--- iOS/G3MiOSSDK/Commons/Geometry/Cylinder.cpp
+++ iOS/G3MiOSSDK/Commons/Geometry/Cylinder.cpp
@@ -17,49 +17,30 @@
 #include "ShortBufferBuilder.hpp"
 #include "CompositeMesh.hpp"
 #include "DirectMesh.hpp"
-
-#warning Chano adding stuff
 #include "MeshRenderer.hpp"
 #include "Camera.hpp"
 #include "FloatBufferBuilderFromColor.hpp"
-
-#define PROXIMITY_VALUE 25
-
-int Cylinder::DISTANCE_METHOD = 2;
-bool Cylinder::DEPTH_ENABLED = false;
-
-void Cylinder::setDistanceMethod(int method){
-    Cylinder::DISTANCE_METHOD = method;
-}
-
-int Cylinder::getDistanceMethod() {
-    return Cylinder::DISTANCE_METHOD;
-}
-
-void Cylinder::setDepthEnabled(bool enabled){
-    Cylinder::DEPTH_ENABLED = enabled;
-}
-bool Cylinder::getDepthEnabled(){
-    return Cylinder::DEPTH_ENABLED;
-}
 
 Mesh* Cylinder::createMesh(const Color& color,
                            const int nSegments,
                            bool depthTest,
                            const Planet *planet){
     
-    Vector3D d = _end.sub(_start);
-    Vector3D r = d._z == 0? Vector3D(0.0,0.0,1.0) : Vector3D(1.0, 1.0, (-d._x -d._y) / d._z);
+    Vector3D cylinderAxis = _end.sub(_start);
+    Vector3D r = cylinderAxis._z == 0? Vector3D(0.0,0.0,1.0) : Vector3D(1.0, 1.0, (-cylinderAxis._x -cylinderAxis._y) / cylinderAxis._z);
     
-    Vector3D p = _start.add(r.times(_radius / r.length()));
+    Vector3D pointAtStart = _start.add(r.times(_startRadius / r.length()));
+    Vector3D pointAtEnd = _end.add(r.times(_endRadius / r.length()));
     
     const double totalAngle = _endAngle - _startAngle;
+    Angle stepAngle = Angle::fromDegrees(totalAngle / nSegments);
     
-    
-    MutableMatrix44D m = MutableMatrix44D::createGeneralRotationMatrix(
-                                                                       Angle::fromDegrees(totalAngle / nSegments),
-                                                                       d,
+    MutableMatrix44D startTransf = MutableMatrix44D::createGeneralRotationMatrix(
+                                                                       stepAngle,
+                                                                       cylinderAxis,
                                                                        _start);
+    MutableMatrix44D endTransf = MutableMatrix44D::createGeneralRotationMatrix(stepAngle,
+                                                                  cylinderAxis, _end);
     
     FloatBufferBuilderFromCartesian3D* fbb = FloatBufferBuilderFromCartesian3D::builderWithFirstVertexAsCenter();
     FloatBufferBuilderFromCartesian3D* fbbC1 = FloatBufferBuilderFromCartesian3D::builderWithFirstVertexAsCenter();
@@ -69,42 +50,48 @@ Mesh* Cylinder::createMesh(const Color& color,
     
     FloatBufferBuilderFromCartesian3D* normals = FloatBufferBuilderFromCartesian3D::builderWithoutCenter();
     FloatBufferBuilderFromCartesian3D* normalsC1 = FloatBufferBuilderFromCartesian3D::builderWithoutCenter();
-    normalsC1->add(d.times(-1.0));
+    normalsC1->add(cylinderAxis.times(-1.0));
     FloatBufferBuilderFromCartesian3D* normalsC2 = FloatBufferBuilderFromCartesian3D::builderWithoutCenter();
-    normalsC2->add(d);
+    normalsC2->add(cylinderAxis);
     
     FloatBufferBuilderFromColor colors;
     
-    MutableVector3D x(p);
-    x = x.transformedBy(
+    MutableVector3D currentPointAtStart(pointAtStart);
+    currentPointAtStart = currentPointAtStart.transformedBy(
     MutableMatrix44D::createGeneralRotationMatrix(Angle::fromDegrees(_startAngle),
-                                                  d, _start), 1.0);
+                                                  cylinderAxis, _start), 1.0);
+    
+    MutableVector3D currentPointAtEnd(pointAtEnd);
+    currentPointAtEnd = currentPointAtEnd.transformedBy(
+    MutableMatrix44D::createGeneralRotationMatrix(Angle::fromDegrees(_startAngle), cylinderAxis, _end), 1.0);
+    
     std::vector<Vector3D *> vs;
     
     for (int i = 0; i < nSegments; ++i){
         
         //Tube
-        Vector3D newStartPoint = x.asVector3D().transformedBy(m, 1.0);
-        Vector3D newEndPoint = newStartPoint.add(d);
-        x.set(newStartPoint._x, newStartPoint._y, newStartPoint._z);
+        Vector3D newStartPoint = currentPointAtStart.asVector3D().transformedBy(startTransf, 1.0);
+        //Vector3D newEndPoint = newStartPoint.add(cylinderAxis);
+        Vector3D newEndPoint = currentPointAtEnd.asVector3D().transformedBy(endTransf, 1.0);
+        
+        currentPointAtStart.set(newStartPoint._x, newStartPoint._y, newStartPoint._z);
+        currentPointAtEnd.set(newEndPoint._x, newEndPoint._y, newEndPoint._z);
         
         fbb->add(newStartPoint); vs.push_back(new Vector3D(newStartPoint));
         fbb->add(newEndPoint); vs.push_back(new Vector3D(newEndPoint));
         
         Geodetic3D stPoint = planet->toGeodetic3D(newStartPoint);
         Geodetic3D endPoint = planet->toGeodetic3D(newEndPoint);
-        _info.addLatLng(stPoint._latitude._degrees, stPoint._longitude._degrees, stPoint._height);
-        _info.addLatLng(endPoint._latitude._degrees, endPoint._longitude._degrees, endPoint._height);
         
         normals->add(newStartPoint.sub(_start));
         normals->add(newEndPoint.sub(_end));
         
         //Cover1
         fbbC1->add(newStartPoint);
-        normalsC1->add(d.times(-1.0));
+        normalsC1->add(cylinderAxis.times(-1.0));
         //Cover2
         fbbC2->add(newEndPoint);
-        normalsC2->add(d);
+        normalsC2->add(cylinderAxis);
         
         colors.add(color);
         colors.add(color);
@@ -168,219 +155,4 @@ void Cylinder::createSphere(std::vector<Vector3D*> &vs){
         delete vs.at(i);
     }
     vs.clear();
-    
-    
-    
-}
-
-#warning Chano adding stuff
-
-
-std::vector<Mesh *> Cylinder::visibleMeshes(MeshRenderer *mr, const Camera *camera,
-                                            const Planet *planet,
-                                            std::vector<CylinderMeshInfo> *cylInfo,
-                                            std::vector<CylinderMeshInfo> &visibleInfo){
-    std::vector<Mesh *> theMeshes = mr->getMeshes();
-    std::vector<Mesh *> theVisibleMeshes;
-    for (size_t i=0;i<theMeshes.size();i++){
-        //        CompositeMesh *cm = (CompositeMesh *) theMeshes[i];
-        IndexedMesh *im = (IndexedMesh *) theMeshes[i]; // cm->getChildAtIndex(0);
-        // Pregunta: ¿el cash devuelve un puntero diferente a la misma dirección de memoria o otra dirección de memoria?
-        cylInfo->at(i)._cylId = (int) i;
-        CylinderMeshInfo info = cylInfo->at(i);
-        // IFloatBuffer *vertices = im->getVerticesFloatBuffer();
-        //const size_t numberVertices = vertices->size();
-        bool visible = false;
-        int vpWidth = camera->getViewPortWidth();
-        int vpHeight = camera->getViewPortHeight();
-        for (size_t j=0; j<info._latlng.size(); j+=3){
-            //¿Cómo se definen los vértices? //
-            Vector3D vPos = planet->toCartesian(Geodetic3D::fromDegrees(info._latlng[j], info._latlng[j+1], info._latlng[j+2]));
-            Vector2F pixel = camera->point2Pixel(vPos);
-            if (pixel._x >= 0 and pixel._x < vpWidth and pixel._y >=0 and pixel._y <= vpHeight){
-                //            if (pixel._x >= 0 and pixel._x < vpHeight and pixel._y >=0 and pixel._y <= vpWidth){
-                visible = true;
-                Vector3D ray = camera->pixel2Ray(pixel).normalized();
-                break;
-            }
-        }
-        if (visible){
-            theVisibleMeshes.push_back(im);
-            visibleInfo.push_back(CylinderMeshInfo(cylInfo->at(i)));
-        }
-    }
-    return theVisibleMeshes;
-}
-
-std::vector<double> Cylinder::distances(CylinderMeshInfo info,
-                                        const Camera *camera,
-                                        const Planet *planet){
-    std::vector<double> dt;
-    
-    for (size_t i=0; i<info._latlng.size(); i=i+3){
-        Geodetic2D userPosition = camera->getGeodeticPosition().asGeodetic2D();
-        Geodetic2D vertexPosition = Geodetic2D::fromDegrees(info._latlng[i],info._latlng[i+1]);
-        dt.push_back(planet->computeFastLatLonDistance(userPosition, vertexPosition));
-    }
-    return dt;
-}
-
-
-std::string Cylinder::adaptMeshes(MeshRenderer *mr,
-                                  std::vector<CylinderMeshInfo> * cylInfo,
-                                  const Camera *camera,
-                                  const Planet *planet){
-    
-    std::vector<CylinderMeshInfo> visibleInfo;
-    std::vector<Mesh*> theMeshes = visibleMeshes(mr,camera,planet,cylInfo,visibleInfo);
-    
-    double maxDt = 100;
-    std::string text = "";
-    
-    for (size_t i=0;i<theMeshes.size();i++){
-        std::vector<double> dt = distances(visibleInfo.at(i),camera,planet);
-        //bool textWritten = false;
-        for (size_t j=0;j< dt.size(); j++) {
-            //maxDt = fmax(maxDt,dt[j]);
-            if ((dt[j] < PROXIMITY_VALUE)){// && (!textWritten)){
-                char buffer [75];
-                sprintf(buffer, "You are close to a visible pipe with id %d \n",visibleInfo.at(i)._cylId);
-                text.append(buffer);
-                // textWritten = true;
-                break;
-            }
-        }
-    }
-    
-    for (size_t i=0;i<theMeshes.size();i++){
-        Mesh *im = (Mesh *) theMeshes[i];
-        std::vector<double> dt = distances(visibleInfo.at(i),camera,planet);
-        for (size_t i = 0; i < dt.size(); ++i){
-            dt[i] = 1.0; //getAlpha(dt[i], maxDt, true);
-        }
-        im->setColorTransparency(dt);
-    }
-    return text;
-}
-
-double Cylinder::getAlpha(double distance, double proximityThreshold, bool divide){
-    if (getDepthEnabled() == true){
-        return rawAlpha(distance,10000,false); //This should cover a whole city without problems.
-    }
-    else switch (getDistanceMethod()) {
-        case 1:
-            return rawAlpha(distance,proximityThreshold,divide);
-        case 2:
-            return linearAlpha(distance,proximityThreshold,divide);
-        case 3:
-            return smoothstepAlpha(distance,proximityThreshold,divide);
-        case 4:
-            return perlinSmootherstepAlpha(distance,proximityThreshold,divide);
-        case 5:
-            return mcDonaldSmootheststepAlpha(distance,proximityThreshold,divide);
-        case 6:
-            return sigmoidAlpha(distance,proximityThreshold,divide);
-        case 7:
-            return tanhAlpha(distance,proximityThreshold,divide);
-        case 8:
-            return arctanAlpha(distance,proximityThreshold,divide);
-        case 9:
-            return softsignAlpha(distance,proximityThreshold,divide);
-    }
-    
-    return NAND; // NAN
-}
-
-double Cylinder::rawAlpha(double distance, double proximityThreshold, bool divide){
-    double ndt = (distance > proximityThreshold)? 0: 1;
-    if (divide) ndt = ndt / 2;
-    return ndt;
-}
-
-double Cylinder::linearAlpha(double distance, double proximityThreshold, bool divide){
-    double ndt = (distance > proximityThreshold)? 0: 1 - ((distance / proximityThreshold));
-    //    ¿Dividir entre 2 para alpha inicial 0.5 ? //
-    if (divide) ndt = ndt / 2;
-    
-    return ndt;
-}
-
-double Cylinder::smoothstepAlpha(double distance, double proximityThreshold, bool divide){
-    double softDistance = distance / proximityThreshold;
-    double ndt = (distance > proximityThreshold)? 0: 1 - ( 3*pow(softDistance,2) - 2*pow(softDistance,3));
-    //    ¿Dividir entre 2 para alpha inicial 0.5 ? //
-    if (divide) ndt = ndt / 2;
-    
-    return ndt;
-}
-double Cylinder::perlinSmootherstepAlpha (double distance, double proximityThreshold, bool divide){
-    double softDistance = distance / proximityThreshold;
-    double ndt = (distance > proximityThreshold)? 0: 1 - ( 6*pow(softDistance,5) - 15*pow(softDistance,4) + 10*pow(softDistance,3));
-    //    ¿Dividir entre 2 para alpha inicial 0.5 ? //
-    if (divide) ndt = ndt / 2;
-    
-    return ndt;
-    
-}
-double Cylinder::mcDonaldSmootheststepAlpha (double distance, double proximityThreshold, bool divide){
-    double softDistance = distance / proximityThreshold;
-    double ndt = (distance > proximityThreshold)? 0: 1 - ( -20*pow(softDistance,7) + 70*pow(softDistance,6) - 84*pow(softDistance,5) + 35*pow(softDistance,4));
-    //    ¿Dividir entre 2 para alpha inicial 0.5 ? //
-    if (divide) ndt = ndt / 2;
-    
-    return ndt;
-}
-double Cylinder::sigmoidAlpha (double distance, double proximityThreshold, bool divide){
-    double softDistance = distance / proximityThreshold;
-    softDistance = (softDistance * 10) - 5;
-    
-    double ndt = (distance > proximityThreshold)? 0: 1 - ( 1 /(1 + exp(-softDistance)));
-    //    ¿Dividir entre 2 para alpha inicial 0.5 ? //
-    if (divide) ndt = ndt / 2;
-    
-    return ndt;
-    
-}
-double Cylinder::tanhAlpha (double distance, double proximityThreshold, bool divide){
-    double softDistance = distance / proximityThreshold;
-    softDistance = (softDistance * 10) - 5;
-    
-    double factor = 2 /(1 + exp(-2*softDistance));
-    factor = factor / 2; // Para convertir de 0-2 a 0-1
-    
-    double ndt = (distance > proximityThreshold)? 0: 1 - factor;
-    //    ¿Dividir entre 2 para alpha inicial 0.5 ? //
-    if (divide) ndt = ndt / 2;
-    
-    return ndt;
-}
-
-double Cylinder::arctanAlpha (double distance, double proximityThreshold, bool divide){
-    double softDistance = distance / proximityThreshold;
-    softDistance = (softDistance * 20) - 10;
-    
-    double factor = atan(softDistance);
-    factor = factor + 1.5;
-    factor = factor / 3; // Para convertir de -1.5 / 1.5 a 0 / 1
-    
-    double ndt = (distance > proximityThreshold)? 0: 1 - factor;
-    //    ¿Dividir entre 2 para alpha inicial 0.5 ? //
-    if (divide) ndt = ndt / 2;
-    
-    return ndt;
-}
-
-double Cylinder::softsignAlpha (double distance, double proximityThreshold, bool divide){
-    double softDistance = distance / proximityThreshold;
-    softDistance = (softDistance * 100) - 50;
-    
-    double factor = softDistance /(1 + fabs(softDistance));
-    factor = factor + 1;
-    factor = factor / 2; // Para convertir de -1 / 1 a 0 / 1
-    
-    double ndt = (distance > proximityThreshold)? 0: 1 - factor;
-    //    ¿Dividir entre 2 para alpha inicial 0.5 ? //
-    if (divide) ndt = ndt / 2;
-    
-    return ndt;
 }
--- iOS/G3MiOSSDK/Commons/Geometry/Cylinder.hpp
+++ iOS/G3MiOSSDK/Commons/Geometry/Cylinder.hpp
@@ -24,108 +24,16 @@ class FloatBufferBuilderFromCartesian3D;
 #include <sstream>
 
 class Cylinder{
-private:
-    static int DISTANCE_METHOD;
-    static bool DEPTH_ENABLED;
 public:
-    
-    
-    class CylinderMeshInfo {
-    public:
-        std::vector<double> _latlng;
-        int _cylId;
-        std::string internalMat;
-        std::string externalMat;
-        std::string internalWidth;
-        std::string externalWidth;
-        std::string cylinderClass;
-        std::string cylinderType;
-        bool isTransport;
-        bool isCommunication;
-        int officialId;
-        
-        CylinderMeshInfo() {_cylId = 0; isTransport = false; isCommunication = false; officialId = -1;}
-        CylinderMeshInfo(int id) {_cylId = id;isTransport = false; isCommunication = false; officialId = -1;};
-        CylinderMeshInfo(const CylinderMeshInfo &cylInfo){
-            _latlng.reserve(cylInfo._latlng.size());
-            copy(cylInfo._latlng.begin(),cylInfo._latlng.end(),back_inserter(_latlng));
-            _cylId = cylInfo._cylId;
-            internalMat = cylInfo.internalMat;
-            externalMat = cylInfo.externalMat;
-            internalWidth = cylInfo.internalWidth;
-            externalWidth = cylInfo.externalWidth;
-            cylinderClass = cylInfo.cylinderClass;
-            cylinderType = cylInfo.cylinderType;
-            isTransport = cylInfo.isTransport;
-            isCommunication = cylInfo.isCommunication;
-            officialId = cylInfo.officialId;
-        }
-        void addLatLng(double lat, double lng, double hgt) {
-            _latlng.push_back(lat);
-            _latlng.push_back(lng);
-            _latlng.push_back(hgt);
-        }
-        
-        void setID (int theId){
-            officialId = theId;
-        }
-        
-        void setMaterials(std::string extMat, std::string intMat){
-            //Ad hoc
-            externalMat = extMat;
-            internalMat = intMat;
-        }
-        
-        void setWidths(double intWidth, double extWidth){
-            //Ad hoc
-            std::stringstream sstm,sstm2;
-            sstm << extWidth << " cm.";
-            externalWidth = sstm.str();
-            sstm2 << intWidth << " cm.";
-            internalWidth = sstm2.str();
-        }
-        
-        void setClassAndType (std::string cClass, std::string cType){
-            //Ad hoc
-            cylinderClass = cClass;
-            cylinderType = cType;
-        }
-        
-        void setTransportComm (bool transport, bool communication){
-            //Ad hoc;
-            isTransport = transport;
-            isCommunication = communication;
-        }
-        
-        std::string getMessage(){
-            std::stringstream sstm;
-            sstm << "ID: " << officialId << "\n";
-            sstm << "Class: " << cylinderClass << "\n";
-            sstm << "Type: " << cylinderType + "\n";
-            if (cylinderClass == "Cable"){
-                sstm << "Internal Material: " << internalMat << "\n";
-                sstm << "External Material: " << externalMat << "\n";
-                sstm <<  "Cross section: " << externalWidth << "\n";
-                sstm << "Is Transmission: " << isTransport << "\n";
-                sstm <<  "Is Communication: " << isCommunication;
-            }
-            else {
-                sstm << "Material: " << externalMat << "\n";
-                sstm << "Internal Width: " << internalWidth << "\n";
-                sstm << "External Width: " << externalWidth;
-            }
-            return sstm.str();
-        }
-        
-    } _info;
-    
     Sphere *s;
     
     Cylinder(const Vector3D& start, const Vector3D& end,
-             const double radius,
+             const double startRadius,
+             const double endRadius,
              const double startAngle = 0.0,
              const double endAngle = 360.0):
-    _start(start), _end(end), _radius(radius), s(NULL),
+    _start(start), _end(end), _startRadius(startRadius),
+    _endRadius(endRadius), s(NULL),
     _startAngle(startAngle), _endAngle(endAngle){}
     
     Mesh* createMesh(const Color& color,
@@ -133,44 +41,16 @@ public:
                      bool depthTest,
                      const Planet *planet);
     
-    static std::string adaptMeshes(MeshRenderer *meshRenderer,
-                                   std::vector<CylinderMeshInfo> *cylInfo,
-                                   const Camera *camera,
-                                   const Planet *planet);
-    
-    static void setDistanceMethod(int method);
-    static int getDistanceMethod();
-    static void setDepthEnabled(bool enabled);
-    static bool getDepthEnabled();
-    
 private:
     
     const Vector3D _start;
     const Vector3D _end;
-    const double _radius;
+    const double _startRadius;
+    const double _endRadius;
     const double _startAngle;
     const double _endAngle;
     
-    static std::vector<Mesh *> visibleMeshes(MeshRenderer *mr, const Camera *camera,
-                                             const Planet *planet,
-                                             std::vector<CylinderMeshInfo> *cylInfo,
-                                             std::vector<CylinderMeshInfo> &visibleInfo);
-    static std::vector<double> distances(CylinderMeshInfo info,
-                                         const Camera *camera,
-                                         const Planet *planet);
-    
     void createSphere(std::vector<Vector3D*> &vs);
-    
-    static double getAlpha(double distance, double proximityThreshold, bool divide);
-    static double rawAlpha(double distance, double proximityThreshold, bool divide);
-    static double linearAlpha(double distance, double proximityThreshold, bool divide);
-    static double smoothstepAlpha(double distance, double proximityThreshold, bool divide);
-    static double perlinSmootherstepAlpha (double distance, double proximityThreshold, bool divide);
-    static double mcDonaldSmootheststepAlpha (double distance, double proximityThreshold, bool divide);
-    static double sigmoidAlpha (double distance, double proximityThreshold, bool divide);
-    static double tanhAlpha (double distance, double proximityThreshold, bool divide);
-    static double arctanAlpha (double distance, double proximityThreshold, bool divide);
-    static double softsignAlpha (double distance, double proximityThreshold, bool divide);
     
 };
 
