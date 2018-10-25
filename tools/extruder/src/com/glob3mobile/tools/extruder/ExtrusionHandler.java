ae9d13f0da55aa930d2c6a5be982af2558dff27a
Diego Gomez Deck <diego@consultar.com>
2018-10-25 10:15:00 +0200

    -

--- tools/extruder/src/com/glob3mobile/tools/extruder/ExtrusionHandler.java
+++ tools/extruder/src/com/glob3mobile/tools/extruder/ExtrusionHandler.java
@@ -11,33 +11,33 @@ import com.glob3mobile.tools.mesh.G3MeshMaterial;
 public interface ExtrusionHandler {
 
 
-   public void onRootGEOObject(GEOObject geoObject);
+   void onRootGEOObject(GEOObject geoObject);
 
 
    /**
     * @param geoFeature
     * @return if the geoFeature's geometry has to be extruded or not
     */
-   public boolean extrudes(GEOFeature geoFeature);
+   boolean extrudes(GEOFeature geoFeature);
 
 
    /**
     * @param geoFeature
     * @return if the extrusion mesh generated for the given geoFeature need to depthTest (in OpenGL meaning) while rendering
     */
-   public boolean getDepthTestFor(GEOFeature geoFeature);
+   boolean getDepthTestFor(GEOFeature geoFeature);
 
 
-   public G3MeshMaterial getMaterialFor(GEOFeature geoFeature);
+   G3MeshMaterial getMaterialFor(GEOFeature geoFeature);
 
 
-   public Heigths getHeightsFor(GEOFeature geoFeature);
+   Heigths getHeightsFor(GEOFeature geoFeature);
 
 
-   public void processTriangulationError(GEOFeature geoFeature);
+   void processTriangulationError(GEOFeature geoFeature);
 
 
-   public void onFinish();
+   void onFinish();
 
 
 }
