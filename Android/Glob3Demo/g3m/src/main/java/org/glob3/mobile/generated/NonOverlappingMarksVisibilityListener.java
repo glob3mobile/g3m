43c0f1313b9380769f2f49246a599415adf627ec
mdelacalle <mdelacalle@gmail.com>
2018-11-20 16:00:13 +0100

    deleting experiments

--- Android/Glob3Demo/g3m/src/main/java/org/glob3/mobile/generated/NonOverlappingMarksVisibilityListener.java
+++
@@ -1,9 +0,0 @@
-package org.glob3.mobile.generated;
-public abstract class NonOverlappingMarksVisibilityListener
-{
-  public void dispose()
-  {
-  }
-
-  public abstract void onVisibilityChange(java.util.ArrayList<NonOverlappingMark> visible);
-}
--- Android/Glob3Demo/g3m/src/main/java/org/glob3/mobile/generated/QuadTree_Content.java
+++
@@ -1,9 +0,0 @@
-package org.glob3.mobile.generated;
-public interface QuadTree_Content
-{
-  // useless, it's here only to make the C++ => Java translator creates an interface intead of an empty class
-  
-  void dispose();
-
-
-}
