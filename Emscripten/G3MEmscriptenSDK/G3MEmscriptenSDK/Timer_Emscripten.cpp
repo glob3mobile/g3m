ccf695e7f801e0f25af66428c876b39bf1b4779d
Diego Gomez Deck <diego@consultar.com>
2020-01-09 12:24:30 +0100

    -

--- Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/Timer_Emscripten.cpp
+++ Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/Timer_Emscripten.cpp
@@ -1,3 +1,27 @@
 
 
 #include "Timer_Emscripten.hpp"
+
+TimeInterval Timer_Emscripten::now() const {
+
+}
+
+TimeInterval Timer_Emscripten::fromDaysFromNow(const double days) const {
+
+}
+
+long long Timer_Emscripten::nowInMilliseconds() const {
+
+}
+
+void Timer_Emscripten::start() {
+
+}
+
+TimeInterval Timer_Emscripten::elapsedTime() const {
+
+}
+
+long long Timer_Emscripten::elapsedTimeInMilliseconds() const {
+
+}
--- Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/Timer_Emscripten.hpp
+++ Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/Timer_Emscripten.hpp
@@ -5,6 +5,19 @@
 #include "ITimer.hpp"
 
 class Timer_Emscripten : public ITimer {
+public:
+  TimeInterval now() const;
+
+  TimeInterval fromDaysFromNow(const double days) const;
+
+  long long nowInMilliseconds() const;
+
+  void start();
+
+  TimeInterval elapsedTime() const;
+
+  long long elapsedTimeInMilliseconds() const;
+
 };
 
 #endif
