4181e6cc04702754b1d6e8fc6414016b074e2eba
Diego Gomez Deck <diego@consultar.com>
2020-01-09 10:22:25 +0100

    -

--- Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/StringBuilder_Emscripten.hpp
+++ Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/StringBuilder_Emscripten.hpp
@@ -4,7 +4,38 @@
 
 #include "IStringBuilder.hpp"
 
+#include <sstream>
+
+
 class StringBuilder_Emscripten : public IStringBuilder {
+private:
+  std::ostringstream _oss;
+
+protected:
+
+  IStringBuilder* clone(const int floatPrecision) const;
+
+public:
+
+  StringBuilder_Emscripten(const int floatPrecision);
+
+  IStringBuilder* addBool(bool b);
+
+  IStringBuilder* addDouble(double d);
+
+  IStringBuilder* addFloat(float f);
+
+  IStringBuilder* addInt(int i);
+
+  IStringBuilder* addLong(long long l);
+
+  IStringBuilder* addString(const std::string& s);
+
+  const std::string getString() const;
+
+  IStringBuilder* clear(const int floatPrecision);
+
+  bool contentEqualsTo(const std::string& that) const;
 };
 
 #endif
