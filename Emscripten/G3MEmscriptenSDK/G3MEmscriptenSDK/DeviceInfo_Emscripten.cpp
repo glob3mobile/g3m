6fc14e7b5ef0ac9664056d2cbfd5b1dfb8aa1640
Diego Gomez Deck <diego@consultar.com>
2020-01-09 10:54:58 +0100

    -

---
+++ Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/DeviceInfo_Emscripten.cpp
@@ -0,0 +1,3 @@
+
+
+#include "Image_Emscripten.hpp"
---
+++ Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/DeviceInfo_Emscripten.hpp
@@ -0,0 +1,10 @@
+
+#ifndef DeviceInfo_Emscripten_hpp
+#define DeviceInfo_Emscripten_hpp
+
+#include "IDeviceInfo.hpp"
+
+class DeviceInfo_Emscripten : public IDeviceInfo {
+};
+
+#endif
