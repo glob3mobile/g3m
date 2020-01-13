2a711d1097434e52471c65de0e7b84701063dbf6
Diego Gomez Deck <diego@consultar.com>
2020-01-13 10:53:05 +0100

    -

--- Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/JSONParser_Emscripten.cpp
+++ Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/JSONParser_Emscripten.cpp
@@ -1,3 +1,20 @@
 
 
 #include "JSONParser_Emscripten.hpp"
+
+#include "IByteBuffer.hpp"
+
+
+const JSONBaseObject* JSONParser_Emscripten::parse(const IByteBuffer* buffer,
+                                                   bool nullAsObject)
+{
+  return parse(buffer->getAsString(), nullAsObject);
+}
+
+
+const JSONBaseObject* JSONParser_Emscripten::parse(const std::string& json,
+                                                   bool nullAsObject)
+{
+
+}
+
--- Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/JSONParser_Emscripten.hpp
+++ Emscripten/G3MEmscriptenSDK/G3MEmscriptenSDK/JSONParser_Emscripten.hpp
@@ -5,6 +5,14 @@
 #include "IJSONParser.hpp"
 
 class JSONParser_Emscripten : public IJSONParser {
+
+public:
+  const JSONBaseObject* parse(const std::string& json,
+                              bool nullAsObject);
+
+  const JSONBaseObject* parse(const IByteBuffer* buffer,
+                              bool nullAsObject);
+
 };
 
 #endif
