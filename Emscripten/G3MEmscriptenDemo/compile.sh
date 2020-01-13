3c18475059eac5f58097165685962928752fc228
Diego Gomez Deck <diego@consultar.com>
2020-01-13 11:13:02 +0100

    -

--- Emscripten/G3MEmscriptenDemo/compile.sh
+++ Emscripten/G3MEmscriptenDemo/compile.sh
@@ -8,15 +8,13 @@ export G3M_COMMONS_SOURCE_DIRECTORY="../../iOS/G3MiOSSDK/Commons/"
 export G3M_EMSCRIPTEN_SOURCE_DIRECTORY="../G3MEmscriptenSDK/"
 
 
-if [[ -z $EMSDK_DIRECTORY                 ]];  then echo - Missing mandatory variable EMSDK_DIRECTORY;                 exit 10; fi
-if [[ -z $G3M_COMMONS_SOURCE_DIRECTORY    ]];  then echo - Missing mandatory variable G3M_COMMONS_SOURCE_DIRECTORY;    exit 10; fi
-if [[ -z $G3M_EMSCRIPTEN_SOURCE_DIRECTORY ]];  then echo - Missing mandatory variable G3M_EMSCRIPTEN_SOURCE_DIRECTORY; exit 10; fi
+if [[ -z $EMSDK_DIRECTORY                 ]]; then echo - Missing mandatory variable EMSDK_DIRECTORY;                 exit 1; fi
+if [[ -z $G3M_COMMONS_SOURCE_DIRECTORY    ]]; then echo - Missing mandatory variable G3M_COMMONS_SOURCE_DIRECTORY;    exit 1; fi
+if [[ -z $G3M_EMSCRIPTEN_SOURCE_DIRECTORY ]]; then echo - Missing mandatory variable G3M_EMSCRIPTEN_SOURCE_DIRECTORY; exit 1; fi
 
 
 source ${EMSDK_DIRECTORY}/emsdk_env.sh
-
 emcc -v
-
 echo
 
 rm -rf deploy
@@ -27,24 +25,25 @@ mkdir deploy
 #emcc --show-ports
     
 em++ \
-    -O0 \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Basic       \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Cameras     \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/DEM         \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Downloader  \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/GEO         \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/GL          \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Geometry    \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Interfaces  \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/JSON        \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Layers      \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Math        \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Mesh        \
-    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Rendererers \
-    -I ${G3M_EMSCRIPTEN_SOURCE_DIRECTORY}          \
-    ${G3M_EMSCRIPTEN_SOURCE_DIRECTORY}/LIB/libG3M.bc \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Basic                    \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Cameras                  \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/DEM                      \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Downloader               \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/GEO                      \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/GL                       \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Geometry                 \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Interfaces               \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/JSON                     \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Layers                   \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Math                     \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Mesh                     \
+    -I ${G3M_COMMONS_SOURCE_DIRECTORY}/Rendererers              \
+    -I ${G3M_EMSCRIPTEN_SOURCE_DIRECTORY}                       \
+    ${G3M_EMSCRIPTEN_SOURCE_DIRECTORY}/LIB/libG3M.bc            \
+    ${G3M_EMSCRIPTEN_SOURCE_DIRECTORY}/LIB/libG3MEmscripten.bc  \
     *.cpp \
     -DC_CODE \
+    -O0 \
     -g \
     -std=c++11 \
     -o deploy/G3MEmscriptenDemo.js \
@@ -54,6 +53,4 @@ cp -rv html/ deploy/
 
 cd deploy
 #emrun --no_browser --port 8080 .
-#emrun --port 8080 .
-
 python -m SimpleHTTPServer 8080
