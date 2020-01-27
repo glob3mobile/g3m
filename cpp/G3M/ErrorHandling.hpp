c3c504c0996eaf1a76325d0dada397448134fbf0
Diego Gomez Deck <diego@consultar.com>
2020-01-27 17:59:12 +0100

    -

--- cpp/G3M/ErrorHandling.hpp
+++ cpp/G3M/ErrorHandling.hpp
@@ -9,20 +9,22 @@
 #ifndef __G3M__ErrorHandling__
 #define __G3M__ErrorHandling__
 
-#ifdef C_CODE
-  #include <stdexcept>
 
-  #ifdef __EMSCRIPTEN__
-    #include <emscripten/emscripten.h>
-    #define THROW_EXCEPTION(x)                                            \
-    {                                                                     \
-      emscripten_log(EM_LOG_CONSOLE | EM_LOG_ERROR | EM_LOG_C_STACK | EM_LOG_JS_STACK | EM_LOG_DEMANGLE, x);  \
-      throw( std::logic_error(x) );                                       \
-    }
-  #else
+#if defined(C_CODE) && !defined(__EMSCRIPTEN__)
+    #include <stdexcept>
     #define THROW_EXCEPTION(x) throw(std::logic_error(x));
-  #endif
-
 #endif
+
+#if defined(C_CODE) && defined(__EMSCRIPTEN__)
+  #include <stdexcept>
+  #include <emscripten/emscripten.h>
+
+  #define THROW_EXCEPTION(x)                                                                               \
+  {                                                                                                        \
+    emscripten_log(EM_LOG_CONSOLE | EM_LOG_ERROR | EM_LOG_C_STACK | EM_LOG_JS_STACK | EM_LOG_DEMANGLE, x); \
+    throw( std::logic_error(x) );                                                                          \
+  }
+#endif
+
 
 #endif
