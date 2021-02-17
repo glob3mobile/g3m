bb9963cac1534612206f0323731beb37f45c5a3c
Diego Gomez Deck <diego@consultar.com>
2021-02-17 19:21:06 +0100

    -

--- WebGL/HelloWorld/src/com/g3m/hello/world/client/HelloWorldEntryPoint.java
+++ WebGL/HelloWorld/src/com/g3m/hello/world/client/HelloWorldEntryPoint.java
@@ -2,17 +2,16 @@
 
 package com.g3m.hello.world.client;
 
-import org.glob3.mobile.specific.G3MBuilder_WebGL;
-import org.glob3.mobile.specific.G3MWidget_WebGL;
 
-import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.RootPanel;
+import org.glob3.mobile.specific.*;
+
+import com.google.gwt.core.client.*;
+import com.google.gwt.user.client.ui.*;
 
 
 public class HelloWorldEntryPoint
-         implements
-            EntryPoint {
+                                  implements
+                                     EntryPoint {
 
    private static final String _g3mWidgetHolderId = "g3mWidgetHolder";
    private G3MWidget_WebGL     _widget            = null;
@@ -20,7 +19,6 @@ public class HelloWorldEntryPoint
 
    @Override
    public void onModuleLoad() {
-      // TODO Auto-generated method stub
       final G3MBuilder_WebGL builder = new G3MBuilder_WebGL();
       _widget = builder.createWidget();
 
